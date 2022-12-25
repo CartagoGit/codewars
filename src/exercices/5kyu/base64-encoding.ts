47254a4f7c0d7d6dc96f285f5e935569b89a0198
Cartago <cartago.relaxingcup@gmail.com>
2022-12-25 02:02:11 +0100

    base64-encoding

---
+++ src/exercices/5kyu/base64-encoding.ts
@@ -0,0 +1,9 @@
+export function toBase64(str: string): string {
+    // Encode this string to Base64
+    return '';
+  }
+  
+  export function fromBase64(str: string): string {
+    // Decode this string from Base64
+    return '';
+  }
