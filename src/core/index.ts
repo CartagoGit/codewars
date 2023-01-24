926a5625fafa4c852c5795719df9dd556513c371
Cartago <cartago.relaxingcup@gmail.com>
2023-01-24 21:13:37 +0100

    Realizamos el ejercicio de nthFibo

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,9 +1,10 @@
 import { initZozonacci } from '../exercices/6kyu/zozonacci';
 import { initMixbonacci } from '../exercices/5kyu/mixbonacci';
 import { getFibonacciSequence } from '../helpers/sequences';
+import { initNthFibo } from '../exercices/6kyu/nthFibo';
 
 
-const result = initMixbonacci();
+const result = initNthFibo();
 // const result = getFibonacciSequence(0);
 // const result2 = getFibonacciIndex(9);
 console.log("❗FINAL result ➽ ⏩", result);
---
+++ src/exercices/6kyu/nthFibo.ts
@@ -0,0 +1,9 @@
+import { getFibonacciSequence } from "../../helpers/sequences";
+export const initNthFibo = (): number => {
+	return nthFibo(4);
+};
+
+export function nthFibo(n: number): number {
+	const sequence = getFibonacciSequence(n - 1);
+	return sequence[sequence.length - 1];
+}
