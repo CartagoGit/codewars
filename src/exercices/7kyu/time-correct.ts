b50d2d925a50e1502a444b4f90299dc4413658e3
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-02-18 12:57:46 +0100

    Terminamos el ejerccio de comprobar el tiempo correcto en string

--- src/exercices/7kyu/time-correct.ts
+++ src/exercices/7kyu/time-correct.ts
@@ -1,18 +1,23 @@
 export const initTimeCorrect = (): string | null => {
-	return timeCorrect("10:30:3a");
+	return timeCorrect("24:30:3");
 };
 
 export function timeCorrect(timestring: string | null): string | null {
-	if (!timestring) return null;
+	if (!timestring) return timestring;
 	const arrayTimers = timestring.split(":").map((timer) => Number(timer));
-    console.log(arrayTimers);
-	if (
-        arrayTimers.length !== 3 ||
-		arrayTimers.some((timer) => typeof timer !== "number")
-        ) {
-            return null;
-        }
-	let [hours, minutes, seconds] = arrayTimers;
+	if (arrayTimers.length !== 3 || arrayTimers.some((timer) => isNaN(timer))) {
+		return null;
+	}
+	const [hours, minutes, seconds] = arrayTimers;
 	const totalSeconds = hours * 60 * 60 + minutes * 60 + seconds;
-	return "?";
+	const finalHours = Math.floor((totalSeconds / (60 * 60)) % 24);
+	const finalMinutes = Math.floor((totalSeconds / 60) % 60);
+	const finalSeconds = totalSeconds % 60;
+	return `${getFinalNumberString(finalHours)}:${getFinalNumberString(
+		finalMinutes
+	)}:${getFinalNumberString(finalSeconds)}`;
+}
+
+export function getFinalNumberString(num: number): string {
+	return num > 9 ? num.toString() : "0" + num;
 }
