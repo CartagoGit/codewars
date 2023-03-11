fa4c69f196779b506919b92da27e5dce479461fb
Cartago <cartago.relaxingcup@gmail.com>
2023-03-11 18:17:54 +0100

    Resolvemos el ejercicio de josephus-survivor

--- src/exercices/5kyu/josephus-survivor.ts
+++ src/exercices/5kyu/josephus-survivor.ts
@@ -1,9 +1,24 @@
 export const initJosephusSurvivor = () => {
-	return josephusSurvivor(7, 3);
+	// return josephusSurvivor(11, 19);
+	return josephusSurvivor(7, 300);
 };
 
 export const josephusSurvivor = (n: number, k: number): number => {
-	const survivors = Array.from({ length: n }, (_, i) => i + 1);
-    console.log(survivors)
-    return 0;
+	if (n < 1 || k < 1) throw new Error("Invalid input");
+	let survivors = Array.from({ length: n }, (_, i) => i + 1);
+	k = k - 1;
+	let position = k;
+    while (position >= survivors.length) {
+        position = position - survivors.length;
+    }
+	while (survivors.length > 1) {
+		survivors.splice(position, 1);
+		position = position + k;
+		while (position >= survivors.length) {
+			position = position - survivors.length;
+		}
+	}
+	return survivors[0];
 };
+
+
