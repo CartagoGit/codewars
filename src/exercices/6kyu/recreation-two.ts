8a240c0384d0cbeb31f98abef20a4bc74faf5d7c
Cartago <cartago.relaxingcup@gmail.com>
2023-03-11 13:05:48 +0100

    Terminamos el ejercicio de recreation-two

--- src/exercices/6kyu/recreation-two.ts
+++ src/exercices/6kyu/recreation-two.ts
@@ -1,6 +1,6 @@
 export const initRecreationTwo = () => {
-	// return prod2sum(100, 100, 100, 100);
-	return prod2sum(143, 43, 32, 236);
+	return prod2sum(100, 100, 100, 100);
+	// return prod2sum(143, 43, 32, 236);
 	// return prod2sum(78, 41, 52, 103);
 };
 
@@ -13,25 +13,41 @@ export const prod2sum = (
 	const ab = Math.pow(a, 2) + Math.pow(b, 2);
 	const cd = Math.pow(c, 2) + Math.pow(d, 2);
 	const n = Math.abs(ab) * Math.abs(cd);
-	console.log({ a, b, c, d, n, ab, cd });
-	const result = getCombinations(n);
+	const posibilities: number[] = getPosibilities({ a, b, c, d });
+	const result = getCombinations(n, posibilities);
 
 	return result;
 };
 
-const getCombinations = (n: number): number[][] => {
+const getCombinations = (n: number, posibilities: number[]): number[][] => {
 	const combinations: number[][] = [];
-	for (let i = 1; i < n; i++) {
-		const iPow = Math.pow(i, 2);
-		if (combinations.length == 2 || iPow > n) break;
-		for (let j = i; j < n; j++) {
-			if (combinations.length == 2) break;
-			const jPow = Math.pow(j, 2);
-			const posibleResult = iPow + jPow;
-			if (posibleResult > n) break;
-			if (posibleResult !== n) continue;
-			combinations.push([i, j]);
+	for (let posibility1 of posibilities) {
+		if (combinations.flat().includes(posibility1)) continue;
+		for (let posibility2 of posibilities) {
+			if (
+				combinations.flat().includes(posibility2) ||
+				Math.pow(posibility1, 2) + Math.pow(posibility2, 2) !== n
+			)
+				continue;
+			combinations.push([posibility1, posibility2].sort((a, b) => a - b));
 		}
 	}
-	return combinations;
+	return combinations.sort((a, b) => a[0] - b[0]);
+};
+
+const getPosibilities = (abcd: {
+	a: number;
+	b: number;
+	c: number;
+	d: number;
+}): number[] => {
+	const { a, b, c, d } = abcd;
+	return [
+		a * b + c * d,
+		a * c + b * d,
+		a * d + b * c,
+		a * b - c * d,
+		a * c - b * d,
+		a * d - b * c,
+	].map((n) => Math.abs(n));
 };
