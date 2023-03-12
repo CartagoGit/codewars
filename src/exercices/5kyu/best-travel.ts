47aa3ef1beee8fb7fb9e57831e15d284d412e229
Cartago <cartago.relaxingcup@gmail.com>
2023-03-12 11:38:25 +0100

    preparamos el ejercicio de best travel

---
+++ src/exercices/5kyu/best-travel.ts
@@ -0,0 +1,12 @@
+export const initBestTravel = () => {
+	chooseBestSum(163, 3, [50, 55, 56, 57, 58]);
+};
+
+export function chooseBestSum(
+	t: number,
+	k: number,
+	ls: number[]
+): number | null {
+	// your code
+    return null
+}
---
+++ src/exercices/5kyu/tests/best-travel.test.ts
@@ -0,0 +1,22 @@
+import { assert } from "chai";
+import { chooseBestSum } from "../best-travel";
+
+describe("Fixed Tests", function() {
+  it("chooseBestSum", function() {
+    assert.strictEqual(
+      chooseBestSum(163, 3, [50, 55, 56, 57, 58]),
+      163
+    );
+
+    assert.strictEqual(
+      chooseBestSum(163, 3, [50]),
+      null
+    );
+
+    assert.strictEqual(
+      chooseBestSum(230, 3, [91, 74, 73, 85, 73, 81, 87]),
+      228
+    );
+  });
+});
+
