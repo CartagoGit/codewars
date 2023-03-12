87f02078b76cb26acd5dd2e9efa559b3d91fdddd
Cartago <cartago.relaxingcup@gmail.com>
2023-03-12 19:34:54 +0100

    Terminamos de resolver el ejercicio de best travels

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,9 +1,10 @@
 import { initCardsDrawOrder2 } from '../exercices/6kyu/cards-draw-order-2';
 import { initCardsDrawOrder1 } from '../exercices/7kyu/cards-draw-order-1';
+import { initBestTravel } from '../exercices/5kyu/best-travel';
 
 
 
-const result =initCardsDrawOrder2();
+const result =initBestTravel();
 
 console.log("❗FINAL result ➽ ⏩", result);
 
--- src/exercices/5kyu/best-travel.ts
+++ src/exercices/5kyu/best-travel.ts
@@ -1,12 +1,61 @@
 export const initBestTravel = () => {
-	chooseBestSum(163, 3, [50, 55, 56, 57, 58]);
+	return chooseBestSum(230, 3, [91, 74, 73, 85, 73, 81, 87]);
 };
 
-export function chooseBestSum(
-	t: number,
-	k: number,
-	ls: number[]
-): number | null {
-	// your code
-    return null
-}
+export const chooseBestSum = (
+	t: number, // maximum distance
+	k: number, //number visitis
+	ls: number[] // distances
+): number | null => {
+	if (ls.length < k) return null;
+
+	const possibleSums = roamDistances({
+		possibleSums: [],
+		index: 0,
+		distances: ls,
+		numVisits: k,
+		maxDistance: t,
+		listDistances: [],
+	});
+
+	return possibleSums.length === 0 ? null : Math.max(...possibleSums);
+};
+
+export const roamDistances = (data: {
+	possibleSums: number[];
+	index: number;
+	distances: number[];
+	numVisits: number;
+	maxDistance: number;
+	listDistances: number[];
+}) => {
+	let {
+		possibleSums,
+		index,
+		distances,
+		numVisits,
+		maxDistance,
+		listDistances,
+	} = data;
+	for (let i = index; i < distances.length; i++) {
+		if (listDistances?.length === numVisits - 1) {
+			listDistances.push(distances[i]);
+			const sum = listDistances.reduce((a, b) => a + b, 0);
+			if (sum <= maxDistance) possibleSums.push(sum);
+			listDistances.pop();
+			continue;
+		} else {
+			listDistances.push(distances[i]);
+			roamDistances({
+				possibleSums,
+				index: i + 1,
+				distances,
+				numVisits,
+				maxDistance,
+				listDistances,
+			});
+		}
+		listDistances.pop();
+	}
+	return possibleSums;
+};
