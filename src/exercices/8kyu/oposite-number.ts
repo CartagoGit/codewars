a6009674b88f170c5245c5567d22827e4b3b680f
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-04-22 21:23:01 +0200

    Resolvemos el ejercicio de numero opuesto

---
+++ src/exercices/8kyu/oposite-number.ts
@@ -0,0 +1,10 @@
+export const initOpositeNumber = () => {
+    const result = Kata.opposite(0);
+    return result;
+}
+
+export class Kata {
+    static opposite(n: number) {
+      return -n;
+    }
+  }
