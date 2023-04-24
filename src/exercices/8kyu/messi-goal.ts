3dadbacf312e7f7d8924c12a90d9e3089f580ab1
Cartago <cartago.relaxingcup@gmail.com>
2023-04-24 23:29:31 +0200

    Solucionamos la kata de los goles de messi

---
+++ src/exercices/8kyu/messi-goal.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/typescript
+
+export const goals = (
+	laLigaGoals: number,
+	copaDelReyGoals: number,
+	championsLeagueGoals: number
+): number => laLigaGoals + copaDelReyGoals + championsLeagueGoals;
