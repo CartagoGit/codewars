929433f442a0a19b22e04f4c45a18a3c5e13d4b0
Cartago <cartago.relaxingcup@gmail.com>
2023-04-25 20:53:37 +0200

    Encontramos otra solucion a la kata de elimianr las vocales

--- src/exercices/7kyu/disemvowel-trolls.ts
+++ src/exercices/7kyu/disemvowel-trolls.ts
@@ -1,11 +1,6 @@
 //* https://www.codewars.com/kata/52fba66badcd10859f00097e/train/typescript
 
 export class Kata {
-	public static disemvowel = (str: string): string => {
-		const vowels = ["a", "e", "i", "o", "u"];
-		return str
-			.split("")
-			.filter((letter) => !vowels.includes(letter.toLowerCase()))
-			.join("");
-	};
+	public static disemvowel = (str: string): string =>
+		str.replace(/[aeiou]/gi, "");
 }
