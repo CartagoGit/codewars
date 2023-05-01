9e64afd139635d17044a30966697b8d18619b648
Cartago <cartago.relaxingcup@gmail.com>
2023-05-01 14:55:01 +0200

    Solucionamos la kata de hacer la ola con un string

--- src/exercices/6kyu/mexican-wave.ts
+++ src/exercices/6kyu/mexican-wave.ts
@@ -3,7 +3,14 @@ export const initMexicanWave = () => {
 	return wave(prompt);
 };
 
-export function wave(str: string): Array<string> {
-	// Good luck
-	return [];
-}
+export const wave = (str: string): Array<string> => {
+	const text = str.trim().toLowerCase();
+	const result: string[] = [];
+	for (let i = 0; i < text.length; i++) {
+		if (text[i] === " ") continue;
+		result.push(
+			text.slice(0, i) + text[i].toUpperCase() + text.slice(i + 1)
+		);
+	}
+	return result;
+};
