5214607c8516e43d23491982b7ca23e669545f2d
Cartago <cartago.relaxingcup@gmail.com>
2023-05-04 20:19:39 +0200

    Encontramos otra solucion al problema del cuarto del mes del año

--- src/exercices/8kyu/quarted-of.ts
+++ src/exercices/8kyu/quarted-of.ts
@@ -1,4 +1,3 @@
 //* https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/typescript
 
-export const quarterOf = (month: number): number =>
-	Math.floor((month - 1) / 3) + 1;
+export const quarterOf = (month: number): number => Math.ceil(month / 3);
