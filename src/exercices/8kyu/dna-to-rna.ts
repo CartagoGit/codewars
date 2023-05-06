26d3cd8f1c47ac84a74494315ea8c6e32a483ebd
Cartago <cartago.relaxingcup@gmail.com>
2023-05-06 14:51:54 +0200

    Encontramos otra solucion a la kata dna to rna

--- src/exercices/8kyu/dna-to-rna.ts
+++ src/exercices/8kyu/dna-to-rna.ts
@@ -1,3 +1,7 @@
 //* https://www.codewars.com/kata/5556282156230d0e5e000089/train/typescript
 
-export const DNAtoRNA = (dna: string): string => dna.replace(/T/g, "U");
+// export const DNAtoRNA = (dna: string): string => dna.replace(/T/g, "U");
+
+export const DNAtoRNA = (dna: string): string => dna.split("T").join("U");
+
+
