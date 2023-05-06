928f4f7e4baed11e4921402043d4fd3c67ad39ce
Cartago <cartago.relaxingcup@gmail.com>
2023-05-06 14:47:42 +0200

    Solucionamos la kata de cuantos años atras el padre tenia doble de años que su hijo

---
+++ src/exercices/8kyu/twice-as-old.ts
@@ -0,0 +1,4 @@
+//* https://www.codewars.com/kata/5b853229cfde412a470000d0/train/typescript
+
+export const twiceAsOld = (dadYearsOld: number, sonYearsOld: number): number =>
+	Math.abs(dadYearsOld - sonYearsOld * 2);
