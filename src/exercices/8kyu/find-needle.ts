82b5fbe8b5583e70d511c98e366772e3d60a9fe5
Cartago <cartago.relaxingcup@gmail.com>
2023-05-07 15:58:40 +0200

    Encontramos otra solucion a la kata de encontrar needle

--- src/exercices/8kyu/find-needle.ts
+++ src/exercices/8kyu/find-needle.ts
@@ -1,6 +1,10 @@
 //* https://www.codewars.com/kata/56676e8fabd2d1ff3000000c/train/typescript
 
+// export function findNeedle(haystack: any[]): string {
+// 	const index = haystack.findIndex((item) => item === "needle");
+// 	return `found the needle at position ${index}`;
+// }
+
 export function findNeedle(haystack: any[]): string {
-	const index = haystack.findIndex((item) => item === "needle");
-	return `found the needle at position ${index}`;
+	return `found the needle at position ${haystack.indexOf("needle")}`;
 }
