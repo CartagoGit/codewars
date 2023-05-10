9d8d9d68ada697136eef3f92d16940c4f61a5406
Cartago <cartago.relaxingcup@gmail.com>
2023-05-10 18:23:37 +0200

    Hacemos otra solucion a la kata de vender hotdogs

--- src/exercices/8kyu/sale-hotdogs.ts
+++ src/exercices/8kyu/sale-hotdogs.ts
@@ -1,4 +1,4 @@
 //* https://www.codewars.com/kata/57202aefe8d6c514300001fd/train/typescript
 
 export const saleHotdogs = (n: number): number =>
-	n < 5 ? n * 100 : n < 10 ? n * 95 : n * 90;
+	n * (n < 5 ? 100 : n < 10 ? 95 : 90);
