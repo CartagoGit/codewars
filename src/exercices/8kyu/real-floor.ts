8f8c02b69d17dcbde6b8f756cbb347bddbe82c8e
Cartago <cartago.relaxingcup@gmail.com>
2023-05-19 20:40:36 +0200

    Damos otra solucion mas corta a la kata de la planta 13

--- src/exercices/8kyu/real-floor.ts
+++ src/exercices/8kyu/real-floor.ts
@@ -1,7 +1,4 @@
 //* https://www.codewars.com/kata/574b3b1599d8f897470018f6/typescript
 
-export function getRealFloor(n: number): number {
-    if(n<=0) return n
-    if(n<13) return n-1
-    return n-2
-}
+export const getRealFloor = (n: number): number =>
+	n > 13 ? n - 2 : n > 0 ? n - 1 : n;
