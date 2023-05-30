a1a5d5201e2aa84efe211548b409b0bdbc7e1af8
Cartago <cartago.relaxingcup@gmail.com>
2023-05-30 20:53:40 +0200

    Solucionamos la kata de contar los petalos de dos flores pares e impares

---
+++ src/exercices/8kyu/opposites-attract.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/555086d53eac039a2a000083/train/typescript
+
+export function lovefunc(flower1: number, flower2: number): boolean {
+    return (flower1 + flower2) % 2 === 1;
+}
