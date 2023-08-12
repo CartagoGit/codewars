ee398a16a8a787360e0fbb1b32ba143cd5398d8c
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-08-12 13:13:05 +0200

    Solucionamos el problema de recuperar elementos distintos positivos de un array

--- src/exercices/8kyu/remove-duplicates.ts
+++ src/exercices/8kyu/remove-duplicates.ts
@@ -1,6 +1,6 @@
-
 //* https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/typescript
-export const distinct = (a: number[]): number[] => {
 
-    return a;
-  }
+// Recupera un array con los numeros positivos de un array de numeros sin repetir
+export const distinct = (a: number[]): number[] => {
+	return a.filter((item, index) => a.indexOf(item) === index && item >= 0);
+};
