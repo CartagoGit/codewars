158ed842c9d8a3e925674387c43f03bb206b7b7e
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-08-13 14:05:19 +0200

    Terminamos la solucion para el conde de parentesis

--- src/exercices/6kyu/duplicate-encode.ts
+++ src/exercices/6kyu/duplicate-encode.ts
@@ -1,7 +1,11 @@
 //* https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/typescript
 
 export function duplicateEncode(word: string) {
-	word.split('').map((value, index, array) =>
-		array.some((v, i) => v === value && i !== index) ? ')' : '('
-	);
-}
+    return word
+      .toLowerCase()
+      .split('')
+      .map((value, index, array) =>
+        array.some((v, i) => v === value && i !== index) ? ')' : '('
+      )
+      .join('');
+  }
