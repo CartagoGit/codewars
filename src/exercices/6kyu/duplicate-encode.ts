85cbf82fc1f29e58e8938cdd76c1094e3a4e1ee3
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-08-13 13:36:19 +0200

    Comenzamos el ejercicio de codificar en parentesis

---
+++ src/exercices/6kyu/duplicate-encode.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/54b42f9314d9229fd6000d9c/typescript
+
+export function duplicateEncode(word: string) {
+	word.split('').map((value, index, array) =>
+		array.some((v, i) => v === value && i !== index) ? ')' : '('
+	);
+}
