9589164fdf776ea244f700f47ceee492ec2711c2
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-08-14 11:14:28 +0200

    Solucionamos la kata de ocmprobacion de factores de numeros base

---
+++ src/exercices/8kyu/check-factor.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/55cbc3586671f6aa070000fb/typescript
+
+export function checkForFactor(base: number, factor: number) {
+    // your code here
+    return base % factor === 0;
+  }
