7ad78bc987b1dbb094ed7748435775c346a4ef99
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-08-16 12:07:19 +0200

    Solucionamos la kata de calcular el volumen de un cubo

---
+++ src/exercices/8kyu/vol-cuboid.ts
@@ -0,0 +1,5 @@
+// * https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/typescript
+
+export function getVolumeOfCuboid(length: number, width:number, height:number): number {
+   return length * width * height;
+ }
