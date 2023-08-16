00b6ef1d5e041c1cb94b3190d000613ba86031f6
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-08-16 12:46:52 +0200

    Solucionamos la kata de la tabla de multiplicar

---
+++ src/exercices/6kyu/multiplation-table.ts
@@ -0,0 +1,13 @@
+//* https://www.codewars.com/kata/534d2f5b5371ecf8d2000a08/train/typescript
+
+export function multiplicationTable (size: number): number[][] {
+    const table: number[][] = [];
+    for (let i = 1; i <= size; i++) {
+        const row: number[] = [];
+        for (let j = 1; j <= size; j++) {
+            row.push(i * j);
+        }
+        table.push(row);
+    }
+    return table;
+  }
