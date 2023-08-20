1fc04147830bdc3fae688188f79af5645e2f601a
Cartago <cartago.relaxingcup@gmail.com>
2023-08-20 15:42:03 +0200

    Solucionamos la kata de sumar los numeros de un array mix de string y numeros

---
+++ src/exercices/8kyu/sum-mixed-array.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/57eaeb9578748ff92a000009/train/typescript
+
+export function sumMix(x: any[]): number {
+    return x.reduce((acc, curr) => acc + Number(curr), 0);
+}
