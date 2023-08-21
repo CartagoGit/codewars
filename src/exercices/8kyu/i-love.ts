3f36e78c4f738420cb16c97171b93595c0e42d59
Cartago <cartago.relaxingcup@gmail.com>
2023-08-21 20:12:00 +0200

    Solucionamos la kata de i love, con petalos

--- src/exercices/8kyu/i-love.ts
+++ src/exercices/8kyu/i-love.ts
@@ -2,4 +2,13 @@
 //* https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/typescript
 export function howMuchILoveYou(petals: number): string{
     //Let's pick a flower...
+    const phrases = [
+        'I love you',
+        'a little',
+        'a lot',
+        'passionately',
+        'madly',
+        'not at all'
+    ];
+    return phrases[(petals - 1) % phrases.length];
   }
