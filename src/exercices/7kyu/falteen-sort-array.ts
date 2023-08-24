f08b143eac75339aeb7e0e312da555ff090d9024
Cartago <cartago.relaxingcup@gmail.com>
2023-08-24 19:58:52 +0200

    Solucionamos la kata de ordenar la base de un array dimensional

---
+++ src/exercices/7kyu/falteen-sort-array.ts
@@ -0,0 +1,7 @@
+// * https://www.codewars.com/kata/57ee99a16c8df7b02d00045f/train/typescript
+
+export function flattenAndSort(inputArray: number[][]): number[] {
+    // Good luck, brave code warrior!
+    return inputArray.flat().sort((a, b) => a - b);
+  }
+  
