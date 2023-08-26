637e7f0aff9c90759455c139419efa8e5e8d1347
Cartago <cartago.relaxingcup@gmail.com>
2023-08-26 15:35:22 +0200

    Avanzamos en el ejercicio de kingdoms 2 simplificado

--- src/exercices/6kyu/kingdoms-ep2-simplified.ts
+++ src/exercices/6kyu/kingdoms-ep2-simplified.ts
@@ -1,5 +1,25 @@
 //* https://www.codewars.com/kata/6159dda246a119001a7de465/train/typescript
 
 export function translate(speech: string, vocabulary: string[]): string {
+	const speechWords = speech.split(' ');
+	const correctedSpeech = speechWords
+		.map((speechWord) => {
+			const matchingVocabulary = vocabulary.find((vocabWord) =>
+				isMatching(speechWord, vocabWord)
+			);
+			return matchingVocabulary || speechWord;
+		})
+		.join(' ');
+	return correctedSpeech;
+}
 
-}  
+const isMatching = (censoredWord: string, vocabularyWord: string) => {
+    // Clean punctuation of censored word
+    const censoredWordWithoutPunctuation = censoredWord.replace(/[?!.,]/g, '');
+	if (censoredWordWithoutPunctuation.length !== vocabularyWord.length) return false;
+	const censoredChars =censoredWordWithoutPunctuation.split('');
+	for (let [index, char] of censoredChars.entries()) {
+		if (char !== '*' && char !== vocabularyWord[index]) return false;
+	}
+	return true;
+};
