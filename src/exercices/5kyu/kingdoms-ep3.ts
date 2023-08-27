3fb0fabc9bb6f2482d71f5f225f6b2d01204f288
Cartago <cartago.relaxingcup@gmail.com>
2023-08-27 17:41:29 +0200

    Corregimos el score y la cantidad de flechas segun su posicion

--- src/exercices/5kyu/kingdoms-ep3.ts
+++ src/exercices/5kyu/kingdoms-ep3.ts
@@ -4,36 +4,31 @@ export function countAndSort(target: string[]): string[] {
 	console.log({ target });
 	let counterScore: Record<string, { score: number; arrows: number }> = {};
 	const maxScores = (target.length + 1) / 2;
-	const rowScores = Array.from({ length: target.length }, (_, index) => {
+	const positionScores = Array.from({ length: target.length }, (_, index) => {
 		if (maxScores >= index + 1) return index + 1;
 		return target.length - index;
 	});
-	const colScores = [...rowScores];
 
 	for (let [rowIndex, row] of target.entries()) {
 		console.log({ rowIndex, row });
 		for (let [colIndex, char] of row.split('').entries()) {
 			console.log({ colIndex, char });
 			if (char === '*') continue;
-			let { score, arrows } = counterScore[char] || {
+			const lowerChar = char.toLowerCase();
+			let { score, arrows } = counterScore[lowerChar] || {
 				score: 0,
 				arrows: 0,
 			};
-			const arrowsInPosition = isUpperCase(char) ? 2 : 1;
+			const arrowsInPosition = char === char.toUpperCase() ? 2 : 1;
 			arrows = arrows + arrowsInPosition;
 			score =
 				score +
-				getPositionScore(rowScores[rowIndex], colScores[colIndex]) *
+				positionScores[Math.min(...[rowIndex, colIndex])] *
 					arrowsInPosition;
-			counterScore[char] = { score, arrows };
+			counterScore[lowerChar] = { score, arrows };
 		}
 	}
 	console.log({ counterScore });
+
 	return [];
 }
-
-const isUpperCase = (char: string) => char === char.toUpperCase();
-
-const getPositionScore = (rowScore: number, colScore: number): number => {
-	return rowScore * colScore;
-};
