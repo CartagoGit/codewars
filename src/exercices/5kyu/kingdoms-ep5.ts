df9136ce5f4331074b20313470c582ff7ba0a7d5
Cartago <cartago.relaxingcup@gmail.com>
2023-08-30 21:03:41 +0200

    Enfocamos el problema

--- src/exercices/5kyu/kingdoms-ep5.ts
+++ src/exercices/5kyu/kingdoms-ep5.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/61771d1cf1c7eb004b7329f0/train/typescript
 
+interface Coordinates {
+	x: number;
+	y: number;
+}
+
 export function searchForFood(
 	island: string[],
 	coordinates: number[],
@@ -8,46 +13,80 @@ export function searchForFood(
 	const islandCoordinates = island.map((row, y) => {
 		return row.split('');
 	});
+	const coordinatesXY = { y: coordinates[0], x: coordinates[1] };
 	console.log({ island, coordinates, steps });
-	const result = walk({ island: islandCoordinates, coordinates, steps });
+	const result = walk({
+		island: islandCoordinates,
+		coordinates: coordinatesXY,
+		steps,
+	});
 	return result;
 }
 
 const walk = (data: {
 	island: string[][];
-	coordinates: number[];
+	coordinates: Coordinates;
 	steps: number;
 }): number => {
+	const { island, coordinates } = data;
+	let { steps } = data;
+	while (steps > 0) {
+		const { top, right, bottom, left } = getBesides(coordinates);
+		if (!isValidPosition({ island, coordinates: top })) continue;
+		if (!isValidPosition({ island, coordinates: right })) continue;
+		if (!isValidPosition({ island, coordinates: bottom })) continue;
+		if (!isValidPosition({ island, coordinates: left })) continue;
+		steps--;
+	}
 	let result = 0;
 	return result;
 };
 
 //* Get the coordinates of the 4 squares besides the current one
 const getBesides = (
-	coordinates: number[]
+	coordinates: Coordinates
 ): {
-	top: [number, number];
-	right: [number, number];
-	bottom: [number, number];
-	left: [number, number];
+	top: Coordinates;
+	right: Coordinates;
+	bottom: Coordinates;
+	left: Coordinates;
 } => {
-	const [x, y] = coordinates;
+	const { x, y } = coordinates;
 	return {
-		top: [x, y - 1],
-		right: [x + 1, y],
-		bottom: [x, y + 1],
-		left: [x - 1, y],
+		top: { x, y: y - 1 },
+		right: { x: x + 1, y },
+		bottom: { x, y: y + 1 },
+		left: { x: x - 1, y },
 	};
 };
 
 //* Check if the coordinates are valid
 const isValidPosition = (data: {
 	island: string[][];
-	coordinates: [number, number];
+	coordinates: Coordinates;
 }): boolean => {
 	const { island, coordinates } = data;
-	const [x, y] = coordinates;
+	const { x, y } = coordinates;
 	const islandWidth = island[0].length;
 	const islandHeight = island.length;
 	return x >= 0 && x < islandWidth && y >= 0 && y < islandHeight;
+};
+
+const validBesides = (data: {
+	island: string[][];
+	coordinates: Coordinates;
+}): {
+	top: boolean;
+	right: boolean;
+	bottom: boolean;
+	left: boolean;
+} => {
+	const { island, coordinates } = data;
+	const { top, right, bottom, left } = getBesides(coordinates);
+	return {
+		top: isValidPosition({ island, coordinates: top }),
+		right: isValidPosition({ island, coordinates: right }),
+		bottom: isValidPosition({ island, coordinates: bottom }),
+		left: isValidPosition({ island, coordinates: left }),
+	};
 };
