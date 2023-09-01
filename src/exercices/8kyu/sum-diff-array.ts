4e5daf0f24811cb63c3a10c5fc7df427fa658a64
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 00:49:27 +0200

    Solucionamos la kata de suma de diferencias de un array

--- src/exercices/8kyu/sum-diff-array.ts
+++ src/exercices/8kyu/sum-diff-array.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5b73fe9fb3d9776fbf00009e/train/typescript
 
 export function sumOfDifferences(arr: number[]): number {
-    return 0;
-  }
+	return arr
+		.sort((a, b) => b - a)
+		.reduce((acc, curr, index, array) => {
+			if (index === 0) return acc;
+			return acc + (array[index - 1] - curr);
+		}, 0);
+}
