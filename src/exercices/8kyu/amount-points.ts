e46b53f82d2ac4831ffbb3be3bc52f1d908ecafc
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 00:52:40 +0200

    Solucionamos la kata de puntos de cantidad de puntos de equipo de footbal

--- src/exercices/8kyu/amount-points.ts
+++ src/exercices/8kyu/amount-points.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/5bb904724c47249b10000131/train/typescript
 
-export function points(games : string[]): number {
-    return 0 // your code here
-  }
+export function points(games: string[]): number {
+	const getPoints = (game: string): number => {
+		const [x, y] = game.split(':').map(Number);
+		if (x > y) return 3;
+		if (x < y) return 0;
+		return 1;
+	};
+	return games.reduce((acc, game) => acc + getPoints(game), 0);
+}
