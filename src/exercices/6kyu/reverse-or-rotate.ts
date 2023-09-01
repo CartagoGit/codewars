27b2878fbf57e86288d6ffbd39f8b3f4a776b3ee
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 00:40:32 +0200

    Solucionamos la kata de revertir o rotar

--- src/exercices/6kyu/reverse-or-rotate.ts
+++ src/exercices/6kyu/reverse-or-rotate.ts
@@ -1,16 +1,19 @@
 //* https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991/train/typescript
 
 export function revRot(str: string, sz: number): string {
-    console.log({str, sz})
+	console.log({ str, sz });
 	if (sz <= 0 || !str || sz > str.length) return '';
 	let chunks = [];
 	for (let i = 0; i < str.length; i += sz) {
 		if (i > str.length - sz) break;
 		const chunk = str.slice(i, i + sz);
-		const chunkNum = Number(chunk);
-		const isCubeDivisibleForTwo = chunkNum ** 3 % 2 === 0;
+
+		const isCubeSumsDivisibleForTwo =
+			chunk.split('').reduce((act, val) => act + Number(val) ** 3, 0) %
+				2 ===
+			0;
 		chunks.push(
-			isCubeDivisibleForTwo
+			isCubeSumsDivisibleForTwo
 				? chunk.split('').reverse().join('')
 				: chunk.slice(1) + chunk[0]
 		);
