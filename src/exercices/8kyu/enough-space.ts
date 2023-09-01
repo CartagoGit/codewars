d24f7a907abf9247fe50169c4052ac73066872c9
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 00:44:37 +0200

    Solucionamos la kata de suficientes pasajeros para el autobus

--- src/exercices/8kyu/enough-space.ts
+++ src/exercices/8kyu/enough-space.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/5875b200d520904a04000003/train/typescript
 
 export function enough(cap: number, on: number, wait: number): number {
-    
-  }
+	const canTake = cap - on - wait;
+	return canTake < 0 ? Math.abs(canTake) : 0;
+}
