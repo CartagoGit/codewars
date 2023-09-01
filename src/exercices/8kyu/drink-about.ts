3be5b37f03d60368f0ada7df3db3059b5a8d3617
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 01:00:21 +0200

    Solucionamos la kata de bebidas  segun la edad

--- src/exercices/8kyu/drink-about.ts
+++ src/exercices/8kyu/drink-about.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/56170e844da7c6f647000063/train/typescript
 
 export function peopleWithAgeDrink(old: number): string {
-    return '';
-  }
+	const drink = (old: number) => {
+		if (old < 14) return 'toddy';
+		if (old < 18) return 'coke';
+		if (old < 21) return 'beer';
+		return 'whisky';
+	};
+	return `drink ${drink(old)}`;
+}
