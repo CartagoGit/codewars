a16619b8fe9e8e9bbf63d32c0169977a55e7dbc0
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 00:56:30 +0200

    Solucionamos la kata de filtrar un array segun los resultados que esten en otro array

--- src/exercices/6kyu/array-diff.ts
+++ src/exercices/6kyu/array-diff.ts
@@ -1,6 +1,5 @@
 //* https://www.codewars.com/kata/523f5d21c841566fde000009/train/typescript
 
 export function arrayDiff(a: number[], b: number[]): number[] {
-    return 0;
-  }
-  
+	return a.filter((num) => !b.includes(num));
+}
