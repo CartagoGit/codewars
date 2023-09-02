cab93e01ecb03ffbb40def89d871ac2b60f790bb
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 13:51:22 +0200

    Preparamos el ejercicio de carrera de tortugas

---
+++ src/exercices/6kyu/tortoise-racing.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/582087e5b81f7063a7000242/train/typescript
+
+export function race(
+	v1: number,
+	v2: number,
+	g: number
+): [number, number, number] | null {
+	return null;
+}
