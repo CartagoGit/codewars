cf32cb1f130c8d71fd27e00853ad7b056fc18b31
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 14:20:20 +0200

    Refactorizamos el ejercicio de carrera de tortugas

--- src/exercices/6kyu/tortoise-racing.ts
+++ src/exercices/6kyu/tortoise-racing.ts
@@ -5,12 +5,14 @@ export function race(
 	bSpeed: number,
 	adventage: number
 ): [number, number, number] | null {
-	console.log({ aSpeed, bSpeed, adventage });
 	if (aSpeed >= bSpeed) return null;
 	const timeInHours: number = adventage / (bSpeed - aSpeed);
-	const seconds = Math.trunc((timeInHours * 3600) % 60); // Total seconds
-	const minutes = Math.trunc((timeInHours * 60) % 60); // Total minutes
-	const hours = Math.trunc(timeInHours); // Total hours
-
-	return [hours, minutes, seconds];
+	const seconds = (timeInHours * 3600) % 60;
+	const minutes = (timeInHours * 60) % 60;
+	const hours = timeInHours;
+	return [hours, minutes, seconds].map(Math.trunc) as [
+		number,
+		number,
+		number
+	];
 }
