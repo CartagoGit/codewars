c9c3033806debf173bc17f328908b9a9519b9278
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 14:35:50 +0200

    Hacemos el ejercicio de la carrera de tortugas lo mas preciso posible

--- src/exercices/6kyu/tortoise-racing.ts
+++ src/exercices/6kyu/tortoise-racing.ts
@@ -9,9 +9,7 @@ export function race(
 	const hours: number = adventage / (bSpeed - aSpeed);
 	const seconds = (hours * 3600) % 60;
 	const minutes = (hours * 60) % 60;
-	return [hours, minutes, seconds].map(Math.floor) as [
-		number,
-		number,
-		number
-	];
+	return [hours, minutes, seconds].map((time) =>
+		Math.floor(time + Number.EPSILON)
+	) as [number, number, number];
 }
