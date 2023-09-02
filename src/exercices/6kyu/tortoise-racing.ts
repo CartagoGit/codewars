6d871c1acb6c9edc018460b3b9adc1ad8abe5912
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 14:14:35 +0200

    Solucionamos la kata de carrera de tortugas

--- src/exercices/6kyu/tortoise-racing.ts
+++ src/exercices/6kyu/tortoise-racing.ts
@@ -1,9 +1,16 @@
 //* https://www.codewars.com/kata/582087e5b81f7063a7000242/train/typescript
 
 export function race(
-	v1: number,
-	v2: number,
-	g: number
+	aSpeed: number,
+	bSpeed: number,
+	adventage: number
 ): [number, number, number] | null {
-	return null;
+	console.log({ aSpeed, bSpeed, adventage });
+	if (aSpeed >= bSpeed) return null;
+	const timeInHours: number = adventage / (bSpeed - aSpeed);
+	const seconds = Math.trunc((timeInHours * 3600) % 60); // Total seconds
+	const minutes = Math.trunc((timeInHours * 60) % 60); // Total minutes
+	const hours = Math.trunc(timeInHours); // Total hours
+
+	return [hours, minutes, seconds];
 }
