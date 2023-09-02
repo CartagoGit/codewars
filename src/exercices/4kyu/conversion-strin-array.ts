fb13d8b521ec6da476d5f69208f9ab43d580a279
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 13:47:03 +0200

    Quitamos la kata debido a la complegidad del enunciado

--- src/exercices/4kyu/conversion-strin-array.ts
+++
@@ -1,9 +0,0 @@
-//* https://www.codewars.com/kata/582087e5b81f7063a7000242/train/typescript
-
-export function arrayToString(arr: number[]): string {
-    throw new Error('TODO: arrayToString');
-  }
-  
-  export function stringToArray(str: string): number[] {
-    throw new Error('TODO: stringToArray');
-  }
