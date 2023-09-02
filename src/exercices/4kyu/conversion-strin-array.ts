ed676f1ef42530828951956abde58b17a9bbde6d
Cartago <cartago.relaxingcup@gmail.com>
2023-09-02 13:39:01 +0200

    Preparamos la kata de conversion de array y strings

---
+++ src/exercices/4kyu/conversion-strin-array.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/582087e5b81f7063a7000242/train/typescript
+
+export function arrayToString(arr: number[]): string {
+    throw new Error('TODO: arrayToString');
+  }
+  
+  export function stringToArray(str: string): number[] {
+    throw new Error('TODO: stringToArray');
+  }
