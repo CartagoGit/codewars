e2e8a528f5ee8abd68790eda9ca54f8b08ea4fca
Cartago <cartago.relaxingcup@gmail.com>
2023-09-03 21:35:59 +0200

    Solucionamos la kata de rgb to hex

--- src/exercices/5kyu/rgb-to-hex.ts
+++ src/exercices/5kyu/rgb-to-hex.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/513e08acc600c94f01000001/train/typescript
 
 export function rgb(r: number, g: number, b: number): string {
-    // Your code here
-  }
+	const result = [r, g, b]
+		.map((color) => {
+			color = color < 0 ? 0 : color > 255 ? 255 : color;
+            let hex = color.toString(16);
+            hex = hex.length === 1 ? '0' + hex : hex;
+			return hex;
+		})
+		.join('')
+		.toUpperCase();
+	return result;
+}
