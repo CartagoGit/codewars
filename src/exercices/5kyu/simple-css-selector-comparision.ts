4d3e12e714b3174ec6494421325fcd54492a7a9a
Cartago <cartago.relaxingcup@gmail.com>
2023-09-03 16:46:34 +0200

    Preparamos el ejercicio de comparacion simple de selectores css

---
+++ src/exercices/5kyu/simple-css-selector-comparision.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/5379fdfad08fab63c6000a63/train/typescript
+
+export function compare (a: string, b: string): string {
+    // do your own comparison
+    return b;
+  }
