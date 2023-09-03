d2a5dbf6c84a81bb260d9f26d1f6b98fdefb3820
Cartago <cartago.relaxingcup@gmail.com>
2023-09-03 23:19:49 +0200

    Preparamos el ejercicio de una clase para vectores

---
+++ src/exercices/5kyu/vector-class.ts
@@ -0,0 +1,8 @@
+//* https://www.codewars.com/kata/526dad7f8c0eb5c4640000a4/train/typescript
+
+export class Vector {
+  
+    constructor(components: number[]) {
+      // Start here...
+    }
+  };
