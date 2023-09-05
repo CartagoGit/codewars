3baed4666b0f84f4266213cfec3bc9470a35a0ef
Cartago <cartago.relaxingcup@gmail.com>
2023-09-05 22:15:46 +0200

    Solucionamos la kata de your order please

--- src/exercices/6kyu/your-order-please.ts
+++ src/exercices/6kyu/your-order-please.ts
@@ -1,5 +1,15 @@
 //* https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/typescript
 
 export function order(words: string): string {
-    
+	const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+	const orderWords = words
+		.split(' ')
+		.map((word) => ({
+			word,
+			number: word.split('').find((char) => numbers.includes(char)),
+		}));
+	return orderWords
+		.sort((a, b) => Number(a.number) - Number(b.number))
+		.map(({ word }) => word)
+		.join(' ');
 }
