cf1102d9f9528d19554c1ed6fc2b4532283ea354
Cartago <cartago.relaxingcup@gmail.com>
2023-09-06 20:32:30 +0200

    Enfocamos el ejercicio para encontrar las rutas

--- src/exercices/6kyu/follow-spy.ts
+++ src/exercices/6kyu/follow-spy.ts
@@ -1,5 +1,8 @@
 //* https://www.codewars.com/kata/5899a4b1a6648906fe000113/train/typescript
 
 export function findRoutes(routes: string[][]): string {
-    //your code here
-  }
+	const orderRoutes = getRoute(routes).join(', ');
+    return orderRoutes;
+}
+
+const getRoute = (routes: string[][]): string[] => {};
