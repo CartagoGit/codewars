0b5aebad928456ffb098030cb06874cb1ca7ff69
Cartago <cartago.relaxingcup@gmail.com>
2023-09-06 23:10:54 +0200

    Solucionamos el ejercicio de suma de angulos

--- src/exercices/7kyu/sum-of-angles.ts
+++ src/exercices/7kyu/sum-of-angles.ts
@@ -1,5 +1,3 @@
 //* https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/typescript
 
-export function angle(n: number): number {
-    // your code here
-  }
+export const angle = (n: number): number => (n - 2) * 180;
