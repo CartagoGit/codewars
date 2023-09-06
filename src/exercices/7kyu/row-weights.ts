063823a77ba3c94973a7dfecff0ff509dffdb8b0
Cartago <cartago.relaxingcup@gmail.com>
2023-09-06 23:18:08 +0200

    Realizamos la solucion con menos codigo

--- src/exercices/7kyu/row-weights.ts
+++ src/exercices/7kyu/row-weights.ts
@@ -1,11 +1,9 @@
 //* https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/typescript
 
-export function rowWeights(arr: number[]) {
-	return arr.reduce(
-		(acc, weight, index) => {
-			acc[index % 2] += weight;
-			return acc;
-		},
+export const rowWeights = (arr: number[]) =>
+	arr.reduce(
+		(acc, weight, index) => ((acc[index % 2] += weight), acc),
 		[0, 0]
 	);
-}
+
+
