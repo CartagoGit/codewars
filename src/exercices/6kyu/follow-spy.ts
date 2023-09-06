342da028f3892cbc3e9750e1b5f92c978201533f
Cartago <cartago.relaxingcup@gmail.com>
2023-09-06 20:55:16 +0200

    Orientamos el ejercicio

--- src/exercices/6kyu/follow-spy.ts
+++ src/exercices/6kyu/follow-spy.ts
@@ -1,8 +1,31 @@
 //* https://www.codewars.com/kata/5899a4b1a6648906fe000113/train/typescript
 
 export function findRoutes(routes: string[][]): string {
-	const orderRoutes = getRoute(routes).join(', ');
-    return orderRoutes;
+	console.log({ routes });
+	const actualOrder = [routes[0]];
+	const listNotTravelled = routes.slice(1);
+	const orderRoutes = getRoute({ listNotTravelled, actualOrder })
+		.map((route) => route[0])
+		.join(', ');
+	return orderRoutes;
 }
 
-const getRoute = (routes: string[][]): string[] => {};
+const getRoute = (data: {
+	listNotTravelled: string[][];
+	actualOrder: string[][];
+}): string[][] => {
+	const { listNotTravelled, actualOrder } = data;
+	if (listNotTravelled.length === 0) return actualOrder;
+	const nextRoute = listNotTravelled.find((route, index) => {
+		const isNextRoute = route[0] === actualOrder[actualOrder.length - 1][1];
+		if (isNextRoute) listNotTravelled.splice(index, 1);
+		return isNextRoute;
+	});
+	if (!nextRoute) {
+		actualOrder.push(listNotTravelled[0]);
+		return actualOrder;
+	}
+	actualOrder.push(nextRoute);
+	getRoute({ listNotTravelled, actualOrder });
+	return actualOrder;
+};
