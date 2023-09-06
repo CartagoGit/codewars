c140a3af5ffdb8bdfeae0d11771eb80f8cb4bbed
Cartago <cartago.relaxingcup@gmail.com>
2023-09-06 21:15:17 +0200

    Solucionamos la kata de encontrar las rutas en orden segun unas rutas dadas

--- src/exercices/6kyu/follow-spy.ts
+++ src/exercices/6kyu/follow-spy.ts
@@ -1,13 +1,19 @@
 //* https://www.codewars.com/kata/5899a4b1a6648906fe000113/train/typescript
 
 export function findRoutes(routes: string[][]): string {
-	console.log({ routes });
-	const actualOrder = [routes[0]];
-	const listNotTravelled = routes.slice(1);
-	const orderRoutes = getRoute({ listNotTravelled, actualOrder })
-		.map((route) => route[0])
-		.join(', ');
-	return orderRoutes;
+	let orderRoutes: string[][] = [];
+	for (let [index, route] of routes.entries()) {
+		const actualOrder = [[...route]];
+		let listNotTravelled = [...routes];
+		listNotTravelled.splice(index, 1);
+		orderRoutes = getRoute({ listNotTravelled, actualOrder });
+		if (orderRoutes.length === routes.length) break;
+	}
+	const result = [
+		orderRoutes[0][0],
+		...orderRoutes.map((route) => route[1]),
+	].join(', ');
+	return result;
 }
 
 const getRoute = (data: {
@@ -22,7 +28,8 @@ const getRoute = (data: {
 		return isNextRoute;
 	});
 	if (!nextRoute) {
-		actualOrder.push(listNotTravelled[0]);
+		if (listNotTravelled[0][0] === actualOrder[actualOrder.length - 1][1])
+			actualOrder.push(listNotTravelled[0]);
 		return actualOrder;
 	}
 	actualOrder.push(nextRoute);
