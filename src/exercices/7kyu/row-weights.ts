ade750a7576bcb34dd9b1659427626743eb5d55b
Cartago <cartago.relaxingcup@gmail.com>
2023-09-06 23:14:51 +0200

    Solucionamos la kata de suma de peso en cada equipo

--- src/exercices/7kyu/row-weights.ts
+++ src/exercices/7kyu/row-weights.ts
@@ -1,6 +1,11 @@
 //* https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9/typescript
 
 export function rowWeights(arr: number[]) {
-    // your code here
-  }
-  
+	return arr.reduce(
+		(acc, weight, index) => {
+			acc[index % 2] += weight;
+			return acc;
+		},
+		[0, 0]
+	);
+}
