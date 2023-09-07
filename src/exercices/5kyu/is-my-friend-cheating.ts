57ecbc6081f1172f1eee77009607fba40c4354fa
Cartago <cartago.relaxingcup@gmail.com>
2023-09-07 22:51:58 +0200

    Enfocamos el ejercicio

--- src/exercices/5kyu/is-my-friend-cheating.ts
+++ src/exercices/5kyu/is-my-friend-cheating.ts
@@ -1,6 +1,25 @@
 //* https://www.codewars.com/kata/5547cc7dcad755e480000004/train/typescript
 
-export function removeNb (n:number):number[][] {
-    // your code
-    return []
-  }
+export function removeNb(n: number): number[][] {
+	let pairsExcluded: number[][] = [];
+	let sum = 0;
+	for (let indexA = 1; indexA <= n; indexA++) {
+		const sumA = (indexA / 2) * (2 + (indexA - 1)) - indexA;
+    if(sumA === 0) continue;
+		for (let indexB = 1; indexB <= n; indexB++) { 
+			const sumB = (indexB / 2) * (2 + (indexB - 1)) - indexB;
+      if(sumB === 0) continue;
+			console.log({
+				indexA,
+				indexB,
+				sumA,
+				sumB,
+				suma: sumA + sumB,
+				multi: indexA * indexB,
+			});
+			if (sumA + sumB === indexA * indexB)
+				pairsExcluded.push([indexA, indexB]);
+		}
+	}
+	return pairsExcluded;
+}
