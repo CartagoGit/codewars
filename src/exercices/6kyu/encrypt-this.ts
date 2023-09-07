ef28fad432fb63bb1aaad2f65fe242e45ca1146e
Cartago <cartago.relaxingcup@gmail.com>
2023-09-07 21:54:09 +0200

    Solucionamos la kata

--- src/exercices/6kyu/encrypt-this.ts
+++ src/exercices/6kyu/encrypt-this.ts
@@ -1,6 +1,14 @@
 //* https://www.codewars.com/kata/5848565e273af816fb000449/train/typescript
 
 export const encryptThis = (str: string): string => {
-    // Your code goes here
-    return str
-  }
+	return str
+		.split(' ')
+		.map((word) => {
+			const firstLetter = word[0] ? word.charCodeAt(0) : '';
+			const secondLetter = word[1] ?? '';
+			const lastLetter = word.length > 2 ? word[word.length - 1] : '';
+			const middle = word.slice(2, word.length - 1) ?? '';
+			return `${firstLetter}${lastLetter}${middle}${secondLetter}`;
+		})
+		.join(' ');
+};
