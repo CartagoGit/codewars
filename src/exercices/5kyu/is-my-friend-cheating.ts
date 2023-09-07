111389311d57ae433305a72ab82fccead926d868
Cartago <cartago.relaxingcup@gmail.com>
2023-09-08 00:35:11 +0200

    Añadimos anotacion de como se ha resuelto la ecuacion factorizando la ecuacion

--- src/exercices/5kyu/is-my-friend-cheating.ts
+++ src/exercices/5kyu/is-my-friend-cheating.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5547cc7dcad755e480000004/train/typescript
 
+//   t as Total
+//   t - (a + b) = a * b
+//   t = a * b + a + b
+//   t = b * (a + 1) + a
+//   (t - a ) / (a + 1) = b
 export function removeNb(n: number): number[][] {
 	let pairsExcluded: number[][] = [];
 	const sumTotal = (n * (n + 1)) / 2;
