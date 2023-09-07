2b4c9e531fe4a42dd2b4250876fcff8e314cc232
Cartago <cartago.relaxingcup@gmail.com>
2023-09-07 21:46:04 +0200

    Modificamos variables para hacerlo mas legible

--- src/exercices/6kyu/how-much.ts
+++ src/exercices/6kyu/how-much.ts
@@ -4,11 +4,11 @@ export function howMuch(m: number, n: number): string[][] {
 	const boat = { price: 7, rest: 2 };
 	const car = { price: 9, rest: 1 };
 	let answers: string[][] = [];
-	for (let i = Math.min(m, n); i <= Math.max(m, n); i++) {
-		const numBoats = (i - boat.rest) / boat.price;
-		const numCars = (i - car.rest) / car.price;
+	for (let money = Math.min(m, n); money <= Math.max(m, n); money++) {
+		const numBoats = (money - boat.rest) / boat.price;
+		const numCars = (money - car.rest) / car.price;
 		if (Number.isInteger(numBoats) && Number.isInteger(numCars))
-			answers.push([`M: ${i}`, `B: ${numBoats}`, `C: ${numCars}`]);
+			answers.push([`M: ${money}`, `B: ${numBoats}`, `C: ${numCars}`]);
 	}
 	return answers;
 }
