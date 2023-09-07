e894a2b553605e8d7c9a81c7d8abc61624132d5b
Cartago <cartago.relaxingcup@gmail.com>
2023-09-07 21:39:15 +0200

    Refactorizamos el codigo

--- src/exercices/6kyu/how-much.ts
+++ src/exercices/6kyu/how-much.ts
@@ -1,17 +1,14 @@
 //* https://www.codewars.com/kata/55b4d87a3766d9873a0000d4/train/typescript
 
 export function howMuch(m: number, n: number): string[][] {
-	const carPrice = 9;
-	const carRest = 1;
-	const boatPrice = 7;
-	const boatRest = 2;
+	const boat = { price: 7, rest: 2 };
+	const car = { price: 9, rest: 1 };
 	let answers: string[][] = [];
 	for (let i = Math.min(m, n); i <= Math.max(m, n); i++) {
-		const cars = (i - carRest) / carPrice;
-		const boats = (i - boatRest) / boatPrice;
-		if (Number.isInteger(cars) && Number.isInteger(boats))
-			answers.push([`M: ${i}`, `B: ${boats}`, `C: ${cars}`]);
+		const numBoats = (i - boat.rest) / boat.price;
+		const numCars = (i - car.rest) / car.price;
+		if (Number.isInteger(numBoats) && Number.isInteger(numCars))
+			answers.push([`M: ${i}`, `B: ${numBoats}`, `C: ${numCars}`]);
 	}
-
 	return answers;
 }
