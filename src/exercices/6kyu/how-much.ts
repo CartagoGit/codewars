75a4a0753171f8010cebbc752759853ca564a85a
Cartago <cartago.relaxingcup@gmail.com>
2023-09-07 21:31:35 +0200

    Enfocamos el ejercicio

--- src/exercices/6kyu/how-much.ts
+++ src/exercices/6kyu/how-much.ts
@@ -1,6 +1,19 @@
 //* https://www.codewars.com/kata/55b4d87a3766d9873a0000d4/train/typescript
 
 export function howMuch(m: number, n: number): string[][] {
-    // your code
-    return []
-  }
+	const min = Math.min(m, n);
+	const max = Math.max(m, n);
+	let answers: string[][] = [];
+	const carPrice = 9;
+	const carRest = 1;
+	const boatPrice = 7;
+	const boatRest = 2;
+	for (let i = min; i <= max; i++) {
+		const cars = (carRest - (i - carRest)) / carPrice;
+		const boats = (boatRest - (i - boatRest)) / boatPrice;
+		if (cars >= 0 && boats >= 0 && cars === boats)
+			answers.push([`M: ${i}`, `B: ${boats}`, `C: ${cars}`]);
+	}
+
+	return answers;
+}
