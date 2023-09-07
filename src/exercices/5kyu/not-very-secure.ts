63415e01e31cb480b74b31f9aa31f4dbcfe4a82d
Cartago <cartago.relaxingcup@gmail.com>
2023-09-07 22:19:53 +0200

    Solucionamos la kata de ver si un string contiene solo caracteres alfanumericos

--- src/exercices/5kyu/not-very-secure.ts
+++ src/exercices/5kyu/not-very-secure.ts
@@ -1,6 +1,4 @@
 //* https://www.codewars.com/kata/526dbd6c8c0eb53254000110/train/typescript
 
-export function alphanumeric(string: string): boolean {
-    //your code here
-    return false;
-  }
+export const alphanumeric = (text: string): boolean =>
+	/^[a-zA-Z0-9]+$/.test(text);
