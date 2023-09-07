9db25e6e958c581bdf68790a9f7640130b08dc50
Cartago <cartago.relaxingcup@gmail.com>
2023-09-07 23:39:28 +0200

    Encontramos una solucion al problema

--- src/exercices/5kyu/is-my-friend-cheating.ts
+++ src/exercices/5kyu/is-my-friend-cheating.ts
@@ -2,13 +2,12 @@
 
 export function removeNb(n: number): number[][] {
 	let pairsExcluded: number[][] = [];
-	let sum = 0;
 	for (let indexA = 1; indexA <= n; indexA++) {
 		const sumA = (indexA / 2) * (2 + (indexA - 1)) - indexA;
-    if(sumA === 0) continue;
-		for (let indexB = 1; indexB <= n; indexB++) { 
+		if (sumA === 0) continue;
+		for (let indexB = 1; indexB <= n; indexB++) {
 			const sumB = (indexB / 2) * (2 + (indexB - 1)) - indexB;
-      if(sumB === 0) continue;
+			if (sumB === 0) continue;
 			console.log({
 				indexA,
 				indexB,
@@ -23,3 +22,19 @@ export function removeNb(n: number): number[][] {
 	}
 	return pairsExcluded;
 }
+
+
+export function removeNb2(n: number): [number, number][] {
+    const totalSum = (n * (n + 1)) / 2; // Suma de todos los números del 1 al n
+    const result: [number, number][] = [];
+  
+    for (let a = 1; a <= n; a++) {
+      const b = (totalSum - a) / (a + 1);
+      console.log({a,b, totalSum})
+      if (Number.isInteger(b) && 1 <= b && b <= n) {
+        result.push([a, b]);
+      }
+    }
+  
+    return result;
+  }
