91d3d412dd10f5e1c22f667110e534417f9e3097
Cartago <cartago.relaxingcup@gmail.com>
2023-09-14 21:45:54 +0200

    Solucionamos el ejercicio del vendedor de libros

--- src/exercices/6kyu/bookseller.ts
+++ src/exercices/6kyu/bookseller.ts
@@ -1,6 +1,15 @@
 //* https://www.codewars.com/kata/54dc6f5a224c26032800005c/train/typescript
 
-export const stockList = (listOfArt:string[], listOfCat:string[]):string => {
-    // your code
-    return "..."
-  }
+export const stockList = (listOfArt: string[], listOfCat: string[]): string => {
+	if ([listOfArt.length, listOfCat.length].includes(0)) return '';
+	let listResult: { cat: string; sun: number }[] = [];
+	for (const cat of listOfCat) {
+		const sum = listOfArt.reduce((acc, art) => {
+			const num = art.split(' ')[1];
+			const firstLetter = art[0];
+			return acc + (firstLetter === cat ? +num : 0);
+		}, 0);
+		listResult.push({ cat, sun: sum });
+	}
+	return listResult.map(({ cat, sun }) => `(${cat} : ${sun})`).join(' - ');
+};
