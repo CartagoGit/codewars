099b0f6ae93ba901168fb9e7fd4f58e6b6993484
Cartago <cartago.relaxingcup@gmail.com>
2023-09-14 21:37:19 +0200

    Preparamos el ejercicio de ayudar al vendedor de libros

---
+++ src/exercices/6kyu/bookseller.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/54dc6f5a224c26032800005c/train/typescript
+
+export const stockList = (listOfArt:string[], listOfCat:string[]):string => {
+    // your code
+    return "..."
+  }
