e2ea09521b4ee69aee524dfdbedca464a4d14911
Cartago <cartago.relaxingcup@gmail.com>
2023-09-15 21:47:33 +0200

    Terminamos el ejercicio de missin letter

--- src/exercices/6kyu/missing-letter.ts
+++ src/exercices/6kyu/missing-letter.ts
@@ -1,5 +1,12 @@
 //* https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/typescript
 
-export function findMissingLetter(array:string[]):string
-{
+export function findMissingLetter(array: string[]): string {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	const isUpper = array[0] === array[0].toUpperCase();
+	const start = alphabet.indexOf(array[0].toLowerCase());
+	const missingLetter = alphabet
+		.slice(start, start + array.length)
+		.split('')
+		.find((letter, index) => letter !== array[index].toLowerCase());
+	return isUpper ? (missingLetter ?? '').toUpperCase() : missingLetter ?? '';
 }
