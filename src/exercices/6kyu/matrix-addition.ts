2ef0b90bf66fad3fd3a68d7f6e498e7edacaeef5
Cartago <cartago.relaxingcup@gmail.com>
2023-09-16 14:01:39 +0200

    Preparamos el ejercicio de matrix addition

---
+++ src/exercices/6kyu/matrix-addition.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/526233aefd4764272800036f/train/typescript
+
+type matrix = number[][];
+export function matrixAddition(a: matrix, b: matrix): matrix {
+  // TODO
+  return [[0, 0], [0, 0]];
+}
