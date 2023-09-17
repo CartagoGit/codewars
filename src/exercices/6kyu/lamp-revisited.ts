df38c5840b2d1ff177a22b22453a596ecf3eea62
Cartago <cartago.relaxingcup@gmail.com>
2023-09-17 15:54:18 +0200

    Refactorizamos la clase de Lamp

--- src/exercices/6kyu/lamp-revisited.ts
+++ src/exercices/6kyu/lamp-revisited.ts
@@ -1,18 +1,8 @@
 //* https://www.codewars.com/kata/570e6e32de4dc8a8340016dd/train/typescript
 
 export class Lamp {
-	public color!: string;
 	public on: boolean = false;
-
-	constructor(color: string) {
-		this.color = color;
-	}
-
-	public toggleSwitch(): void {
-		this.on = !this.on;
-	}
-
-	public state(): string {
-		return `The lamp is ${this.on ? 'on' : 'off'}.`;
-	}
+	constructor(public color: string) {}
+	public toggleSwitch = (): boolean => (this.on = !this.on);
+	public state = (): string => `The lamp is ${this.on ? 'on' : 'off'}.`;
 }
