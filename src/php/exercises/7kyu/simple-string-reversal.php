40214e33ba6185ca85c5d37295efbdfd829fb6ec
Cartago <cartago.relaxingcup@gmail.com>
2023-09-18 00:44:03 +0200

    Damos una solucion en php a la kata de simple-string reversal

--- src/php/exercises/7kyu/simple-string-reversal.php
+++ src/php/exercises/7kyu/simple-string-reversal.php
@@ -1,7 +1,27 @@
 //* https://www.codewars.com/kata/5a71939d373c2e634200008e/train/php
 
 
-<?php 
-function solve(string $s): string {
-    // your code here
+<?php
+function solve(string $chain): string
+{
+    $positionSpaces = [];
+    for ($i = 0; $i < strlen($chain); $i++) {
+        if ($chain[$i] === ' ') {
+            $positionSpaces[] = $i;
+        }
+    }
+    $unionChain = str_replace(' ', '', $chain);
+    $reverseChain = strrev($unionChain);
+    $result = '';
+    $spaceIndex = 0;
+
+    for ($i = 0; $i < strlen($chain); $i++) {
+        if (in_array($i, $positionSpaces)) {
+            $result .= ' ';
+            $spaceIndex++;
+        } else {
+            $result .= $reverseChain[$i - $spaceIndex];
+        }
+    }
+    return $result;
 }
