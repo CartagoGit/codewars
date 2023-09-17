99bab27c07a5370869026768c6bad04697f27a5d
Cartago <cartago.relaxingcup@gmail.com>
2023-09-17 16:24:47 +0200

    Solucionamos la kata de php para comprobar si un numero es primo

--- src/php/exercises/6kyu/is-prime.php
+++ src/php/exercises/6kyu/is-prime.php
@@ -4,5 +4,13 @@
 
 function is_prime(int $n): bool
 {
-    // TODO
+    if ($n <= 1) {
+        return false;
+    }
+    for ($i = 2; $i <= sqrt($n); $i++) {
+        if ($n % $i === 0) {
+            return false;
+        }
+    }
+    return true;
 }
