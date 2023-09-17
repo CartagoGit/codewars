8900d3b762a756d66bfb5d87b19775cd742bc57b
Cartago <cartago.relaxingcup@gmail.com>
2023-09-17 23:29:32 +0200

    Optimizamos un poco el codigo

--- src/php/exercises/6kyu/deaf-rats-of-hameling.php
+++ src/php/exercises/6kyu/deaf-rats-of-hameling.php
@@ -4,5 +4,23 @@
 
 function countDeafRats($town)
 {
-    //Your code here
+    $town = str_replace(' ', '', $town);
+    //* Chars
+    $hamelin = 'P';
+    $ratToRight = '~O';
+    $ratToLeft = 'O~';
+
+    // * Separate chains
+    $indexP = strpos($town, $hamelin);
+    $beforeP = substr($town, 0, $indexP);
+    $afterP = substr($town, $indexP + 1);
+
+    // * Separate rats
+    $ratsBeforeP = implode(' ', str_split($beforeP, 2));
+    $ratsAfterP = implode(' ', str_split($afterP, 2));
+
+    // * Count deaf rats
+    $deafRatsBefore = substr_count($ratsBeforeP, $ratToLeft);
+    $deafRatsAfter = substr_count($ratsAfterP, $ratToRight);
+    return $deafRatsBefore + $deafRatsAfter;
 }
