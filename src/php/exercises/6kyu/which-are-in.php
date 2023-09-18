12fb2f894cfa47ddade34d09822266148f048346
Cartago <cartago.relaxingcup@gmail.com>
2023-09-18 21:03:05 +0200

    Solucionamos el ejercicio de comprobar si un array contiene un substring en los elementos de otro array en php

--- src/php/exercises/6kyu/which-are-in.php
+++ src/php/exercises/6kyu/which-are-in.php
@@ -2,6 +2,17 @@
 
 <?php
 
-function inArray($array1, $array2) { 
-
+function inArray($array1, $array2)
+{
+    $result = [];
+    foreach ($array1 as $value1) {
+        foreach ($array2 as $value2) {
+            if (strpos($value2, $value1) !== false) {
+                $result[] = $value1;
+                break;
+            }
+        }
+    }
+    sort($result);
+    return $result;
 }
