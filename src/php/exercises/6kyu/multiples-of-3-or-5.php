5f274585de3fc690522a03f563ef007622a91e72
Cartago <cartago.relaxingcup@gmail.com>
2023-09-18 20:54:36 +0200

    Solucionamos la kata de multiplos de 3 o 5

--- src/php/exercises/6kyu/multiples-of-3-or-5.php
+++ src/php/exercises/6kyu/multiples-of-3-or-5.php
@@ -1,6 +1,14 @@
 //* https://www.codewars.com/kata/514b92a657cdc65150000006/train/php
 
 <?php 
-function solution($number){
+function solution($number): int{
+    if($number < 0) return 0;
 
+    $sum = 0;
+    for($i = 0; $i < $number; $i++) {
+        if($i % 3 === 0 || $i % 5 === 0) {
+            $sum += $i;
+        }
+    }
+    return $sum;
 }
