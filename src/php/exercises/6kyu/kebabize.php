b5199ddb0d0bf5cf419e10691499392ee1eb9955
Cartago <cartago.relaxingcup@gmail.com>
2023-09-20 21:43:12 +0200

    Solucionamos la kata de convertir camel case en kebab case

--- src/php/exercises/6kyu/kebabize.php
+++ src/php/exercises/6kyu/kebabize.php
@@ -1,8 +1,15 @@
-
 //* https://www.codewars.com/kata/57f8ff867a28db569e000c4a/train/php
 
-<?php 
+<?php
 
-function kebabize($string) {
-  
+function kebabize($string)
+{
+    echo $string;
+    while (!ctype_alpha(substr($string, 0, 1)))  $string = substr($string, 1);
+    $string = lcfirst($string);
+    return implode(array_map(function ($char) {
+        if (!ctype_alpha($char)) return '';
+        if (ctype_upper($char)) $char = '-' . strtolower($char);
+        return $char;
+    }, str_split($string)));
 }
