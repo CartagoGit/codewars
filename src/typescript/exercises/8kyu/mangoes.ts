16b1c4614c42a780b824bb3156d51f88f9d87cd5
Cartago <cartago.relaxingcup@gmail.com>
2023-09-22 19:10:05 +0200

    Solucionamos la kata de los mangos

--- src/typescript/exercises/8kyu/mangoes.ts
+++ src/typescript/exercises/8kyu/mangoes.ts
@@ -1,8 +1,8 @@
 //* https://www.codewars.com/kata/57a77726bb9944d000000b06/train/typescript
 
 export function mango(quantity: number, price: number): number{
-    return 1
+    const paidQ = Math.floor(quantity / 3) * 2 + (quantity % 3);
+    return paidQ * price;
   }
 
-  
   
