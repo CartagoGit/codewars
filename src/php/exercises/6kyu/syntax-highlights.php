f3a710884f1ee857e5dcfb2cdc0f34b2f51c9528
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-09-23 10:46:49 +0200

    Avanzamos en el ejercicio de highlights

--- src/php/exercises/6kyu/syntax-highlights.php
+++ src/php/exercises/6kyu/syntax-highlights.php
@@ -2,6 +2,15 @@
 
 <?php
 
-function highlight(code) {
+
+function getWrap (String $char) {
+  $type = '';
+  if(is_int($char)) $type = 'num';
+  else $type = $char;
+  $dictionary = ['F' => 'pink', 'L' => 'red', 'R' => 'green', 'num' => 'orange'];
+    return '<span style="color: ' . $dictionary[$type] . '">'.$char. '</span>';
+}
+function highlight(String $code) {
+  
     // Implement your syntax highlighter here
   }
