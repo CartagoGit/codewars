d3b23aaae774ba9a275a458155b1cbdf8a9e8535
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-09-24 13:08:03 +0200

    Damos una solucion a la kata

--- src/typescript/exercises/7kyu/maximum-length-difference.ts
+++ src/typescript/exercises/7kyu/maximum-length-difference.ts
@@ -1,6 +1,13 @@
 //* https://www.codewars.com/kata/5663f5305102699bad000056/train/typescript
 
 export const mxdiflg = (a1:string[], a2:string[]): number => {
-    // your code
-    return -1
+    if([a1, a2].some(arr => arr.length === 0)) return -1;
+    let result = -1;
+    for(let a1Field of a1) {
+        for(let a2Field of a2) {
+           const abs = Math.abs(a1Field.length - a2Field.length);
+           result = Math.max(result, abs);
+        }
+    }
+    return result;
   }
