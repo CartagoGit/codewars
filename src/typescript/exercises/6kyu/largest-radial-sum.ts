03b32134fb2454d77ae12150ff441247ef9cada5
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-09-24 12:55:35 +0200

    Encontramos solucion a la kata de largest radial sum

--- src/typescript/exercises/6kyu/largest-radial-sum.ts
+++ src/typescript/exercises/6kyu/largest-radial-sum.ts
@@ -1,12 +1,12 @@
 //* https://www.codewars.com/kata/64edf7ab2b610b16c2067579/train/java
 
 export function largestRadialSum(arr: number[], d: number): number {
-	const result: number[] = Array.from({ length: d }, () => 0);
-
-	for (let i = 0; i < arr.length; i++) {
-		const indexResArray = i % d;
+	const n = arr.length;
+	const size = n / d;
+	const result: number[] = Array.from({ length: size }, () => 0);
+	for (let i = 0; i < n; i++) {
+		const indexResArray = i % size;
 		result[indexResArray] = result[indexResArray] + arr[i];
 	}
-
 	return Math.max(...result);
 }
