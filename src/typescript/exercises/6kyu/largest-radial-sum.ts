86996b2f8c6861825edd2aff317ec93f6db97353
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-09-24 12:59:05 +0200

    Damos una solucion mas elegante

--- src/typescript/exercises/6kyu/largest-radial-sum.ts
+++ src/typescript/exercises/6kyu/largest-radial-sum.ts
@@ -5,8 +5,8 @@ export function largestRadialSum(arr: number[], d: number): number {
 	const size = n / d;
 	const result: number[] = Array.from({ length: size }, () => 0);
 	for (let i = 0; i < n; i++) {
-		const indexResArray = i % size;
-		result[indexResArray] = result[indexResArray] + arr[i];
+		const indexResult = i % size;
+		result[indexResult] += arr[i];
 	}
 	return Math.max(...result);
 }
