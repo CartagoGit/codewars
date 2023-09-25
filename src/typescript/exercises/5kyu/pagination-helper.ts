6f2856e615cb53ab0ff0106a53eeb3a8f1100bf2
Cartago <cartago.relaxingcup@gmail.com>
2023-09-25 21:22:09 +0200

    Orientamos el ejercicio de la clase de paginacion

--- src/typescript/exercises/5kyu/pagination-helper.ts
+++ src/typescript/exercises/5kyu/pagination-helper.ts
@@ -1,22 +1,23 @@
 //* https://www.codewars.com/kata/515bb423de843ea99400000a/train/typescript
 
-class PaginationHelper {
-	constructor(collection, itemsPerPage) {
-	// The constructor takes in an array of items and a integer indicating how many
-	// items fit within a single page
+export class PaginationHelper {
+	public collection: any[];
+	public itemsPerPage: number;
+
+	constructor(collection: any[], itemsPerPage: number) {
+		this.collection = collection;
+		this.itemsPerPage = itemsPerPage;
 	}
-	itemCount() {
-	// returns the number of items within the entire collection
+	public itemCount(): number {
+        return this.collection.length;
 	}
-	pageCount() {
-	// returns the number of pages
+	public pageCount(): number {
+        return Math.ceil(this.collection.length / this.itemsPerPage);
 	}
-	pageItemCount(pageIndex) {
-	// returns the number of items on the current page. page_index is zero based.
-	// this method should return -1 for pageIndex values that are out of range
+	public pageItemCount(pageIndex: number) {
+        return pageIndex < this.pageCount() - 1 ? this.itemsPerPage : this.collection.length % this.itemsPerPage;
 	}
-	pageIndex(itemIndex) {
-	// determines what page an item is on. Zero based indexes
-	// this method should return -1 for itemIndex values that are out of range
+	public pageIndex(itemIndex: number) {
+        return itemIndex < 0 || itemIndex >= this.collection.length ? -1 : Math.floor(itemIndex / this.itemsPerPage);
 	}
 }
