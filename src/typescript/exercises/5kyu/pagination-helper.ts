056778aa964400d85ce1ce9f55b1d8ebb49c27a3
Cartago <cartago.relaxingcup@gmail.com>
2023-09-25 21:29:55 +0200

    Solucionamos la kata de la clase helper de paginacion

--- src/typescript/exercises/5kyu/pagination-helper.ts
+++ src/typescript/exercises/5kyu/pagination-helper.ts
@@ -4,20 +4,28 @@ export class PaginationHelper {
 	public collection: any[];
 	public itemsPerPage: number;
 
-	constructor(collection: any[], itemsPerPage: number) {
-		this.collection = collection;
-		this.itemsPerPage = itemsPerPage;
+	constructor(collection?: any[], itemsPerPage?: number) {
+		this.collection = collection ?? [];
+		this.itemsPerPage = itemsPerPage ?? 0;
 	}
 	public itemCount(): number {
-        return this.collection.length;
+		return this.collection.length;
 	}
 	public pageCount(): number {
-        return Math.ceil(this.collection.length / this.itemsPerPage);
+		return Math.ceil(this.collection.length / this.itemsPerPage);
 	}
-	public pageItemCount(pageIndex: number) {
-        return pageIndex < this.pageCount() - 1 ? this.itemsPerPage : this.collection.length % this.itemsPerPage;
+	public pageItemCount(pageIndex: number): number {
+		if (pageIndex < 0 || pageIndex >= this.pageCount()) return -1;
+		else if (
+			pageIndex < this.pageCount() - 1 ||
+			this.collection.length % this.itemsPerPage === 0
+		)
+			return this.itemsPerPage;
+		else return this.collection.length % this.itemsPerPage;
 	}
-	public pageIndex(itemIndex: number) {
-        return itemIndex < 0 || itemIndex >= this.collection.length ? -1 : Math.floor(itemIndex / this.itemsPerPage);
+	public pageIndex(itemIndex: number): number {
+		return itemIndex < 0 || itemIndex >= this.collection.length
+			? -1
+			: Math.floor(itemIndex / this.itemsPerPage);
 	}
 }
