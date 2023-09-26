cfc14e4b31b313c762ff0d349b9f717c687197a9
Cartago <cartago.relaxingcup@gmail.com>
2023-09-26 22:55:20 +0200

    Arreglamos condicionales de periodicos

--- src/typescript/exercises/5kyu/string-generator-by-pattern.ts
+++ src/typescript/exercises/5kyu/string-generator-by-pattern.ts
@@ -4,9 +4,11 @@ type KindToken = 'INC_INT' | 'INC_FLOAT' | 'INTERVAL' | 'PERIODIC';
 
 class Token {
 	private _value: number;
+	private _initValue: number;
 	private _step: number;
-	private _count: number;
+	private _count: number = 0;
 	private _kind: KindToken;
+	private _justFirstParam: boolean = false;
 
 	constructor(data: {
 		start?: number;
@@ -14,10 +16,15 @@ class Token {
 		count?: number;
 		kind: KindToken;
 	}) {
-		const { start = 1, step = 1, count = 0, kind } = data;
+		const { start = 1, step = 1, kind } = data;
+		this._justFirstParam = data.step === undefined;
 		this._value = start;
-		this._step = step;
-		this._count = count;
+		this._initValue = start;
+		this._step = this._justFirstParam
+			? kind === 'PERIODIC'
+				? 1
+				: start
+			: step;
 		this._kind = kind;
 	}
 
@@ -45,17 +52,19 @@ class Token {
 
 	private _interval(): string {
 		const value = this._value;
-		this._value = this._value == this._step ? this._value : this._value + 1;
+		this._value =
+			this._value === this._step ? this._initValue : this._value + 1;
 		return value.toString();
 	}
 
 	private _periodic(): string {
+		const value = this._value;
 		this._count++;
 		if (this._count === this._step) {
 			this._count = 0;
 			this._value++;
 		}
-		return this._value.toString();
+		return value.toString();
 	}
 }
 
@@ -68,6 +77,7 @@ export function* stringGenerator(pattern: string): Generator<string> {
 		(match: string, type: KindToken, param1: string, param2: string) => {
 			const start = param1 ? parseInt(param1) : undefined;
 			const step = param2 ? parseInt(param2) : undefined;
+			type = (type as string).trim() as KindToken;
 			const method: Record<KindToken, Token> = {
 				INC_INT: new Token({
 					start: start,
