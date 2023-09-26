09d24d80e8b7b1b180e1366f6f93efb851165d9a
Cartago <cartago.relaxingcup@gmail.com>
2023-09-26 21:04:50 +0200

    Preparamos el ejercicio de string generetor con yield

---
+++ src/typescript/exercises/5kyu/string-generator-by-pattern.ts
@@ -0,0 +1,8 @@
+//* https://www.codewars.com/kata/62b3356dacf409000f53cab7/train/typescript
+
+export function *stringGenerator(pattern: string): Generator<string> {
+  
+    while (true) {
+      yield "code me please!";
+    }
+  }
