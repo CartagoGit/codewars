ade1ee6cee9b7c3c6dbccb3f964c56c6a1134ad1
Cartago <cartago.relaxingcup@gmail.com>
2023-09-27 19:39:12 +0200

    Encontramos una solucion a la kata de comprando un coche

--- src/typescript/exercises/6kyu/buying-a-car.ts
+++ src/typescript/exercises/6kyu/buying-a-car.ts
@@ -1,10 +1,21 @@
 //* https://www.codewars.com/kata/554a44516729e4d80b000012/train/typescript
 
 export function nbMonths(
-	startPriceOld: number,
-	startPriceNew: number,
-	savingperMonth: number,
+	priceOld: number,
+	priceNew: number,
+	savingPerMonth: number,
 	percentLossByMonth: number
 ): number[] {
-	// your code
+	let month = 0;
+	let savings = 0;
+
+	while (savings < priceNew - priceOld) {
+		month++;
+		savings += savingPerMonth;
+		if (month % 2 === 0) percentLossByMonth += 0.5;
+
+		priceOld -= priceOld * (percentLossByMonth / 100);
+		priceNew -= priceNew * (percentLossByMonth / 100);
+	}
+	return [month, Math.round(priceOld + savings - priceNew)];
 }
