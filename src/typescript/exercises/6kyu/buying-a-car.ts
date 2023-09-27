feaa0c89fd53cf2abbe7e0a5719607cc8ced3409
Cartago <cartago.relaxingcup@gmail.com>
2023-09-27 19:23:24 +0200

    Preparamos el ejercicio de comprando un coche

---
+++ src/typescript/exercises/6kyu/buying-a-car.ts
@@ -0,0 +1,10 @@
+//* https://www.codewars.com/kata/554a44516729e4d80b000012/train/typescript
+
+export function nbMonths(
+	startPriceOld: number,
+	startPriceNew: number,
+	savingperMonth: number,
+	percentLossByMonth: number
+): number[] {
+	// your code
+}
