b64929681f3f4a821814bc38f79f51c27ec632cb
Cartago <cartago.relaxingcup@gmail.com>
2023-10-01 00:37:27 +0200

    Solucionamos la kata de 20 años subiendo escaleras de suzuki

--- src/typescript/exercises/8kyu/suzuki-climbs.ts
+++ src/typescript/exercises/8kyu/suzuki-climbs.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/56fc55cd1f5a93d68a001d4e/train/typescript
 
-export function stairsIn20(stairs:number[][]) {
-    //code here
-  }
+export function stairsIn20(stairs: number[][]) {
+	return (
+		stairs.reduce(
+			(total, day) =>
+				total + day.reduce((sum, stairs) => sum + stairs, 0),
+			0
+		) * 20
+	);
+}
