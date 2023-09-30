4061f25f08cb65d64c7f96b9ed878bc6fb50c2bb
Cartago <cartago.relaxingcup@gmail.com>
2023-09-30 23:31:29 +0200

    Solucionamos el ejercicio de isuppercase

--- src/typescript/exercises/8kyu/is-uppercase.ts
+++ src/typescript/exercises/8kyu/is-uppercase.ts
@@ -1,9 +1,5 @@
 //* https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/typescript
 
-
-
 export function isUpperCase(str: string) {
-    // your code here
-  }
-
-  
+	return str === str.toUpperCase();
+}
