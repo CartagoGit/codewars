43093e252f8382f84497bc50f456f72203bb5e85
Cartago <cartago.relaxingcup@gmail.com>
2023-10-02 21:26:14 +0200

    Encontramos solucion al ejercicio de encontrar al elemento que no es par o impar dentro del array

--- src/typescript/exercises/6kyu/parity-outlier.ts
+++ src/typescript/exercises/6kyu/parity-outlier.ts
@@ -1,6 +1,7 @@
 //* https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/typescript
 
 export function findOutlier(integers: number[]): number {
-    // your code here
-  }
-  
+	const isEvenArray =
+		[...integers].slice(0, 3).filter((num) => num % 2 === 0).length > 1;
+	return integers.find((num) => Math.abs(num) % 2 === (isEvenArray ? 1 : 0))!;
+}
