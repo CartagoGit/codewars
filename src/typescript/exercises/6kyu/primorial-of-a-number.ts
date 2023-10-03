c96329820b30ad5be45e6ee45dc9873442a9a6c7
Cartago <cartago.relaxingcup@gmail.com>
2023-10-03 20:34:48 +0200

    Añadimos anotacion

--- src/typescript/exercises/6kyu/primorial-of-a-number.ts
+++ src/typescript/exercises/6kyu/primorial-of-a-number.ts
@@ -1,7 +1,7 @@
 //* https://www.codewars.com/kata/5a99a03e4a6b34bb3c000124/train/typescript
 
 const isPrime = (num: number): boolean => {
-	for (let i = 2,sqrt = Math.sqrt(num); i <= sqrt; i++)
+	for (let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++)
 		if (num % i === 0) return false;
 	return num > 1;
 };
