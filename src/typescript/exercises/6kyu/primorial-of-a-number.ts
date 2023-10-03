c10702ddd678f1becbec25fc474c84ab0ac3eefe
Cartago <cartago.relaxingcup@gmail.com>
2023-10-03 20:32:33 +0200

    Encontramos solucion para el ejercicio de primorial of numbers

--- src/typescript/exercises/6kyu/primorial-of-a-number.ts
+++ src/typescript/exercises/6kyu/primorial-of-a-number.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/5a99a03e4a6b34bb3c000124/train/typescript
 
-export function numPrimorial(n: number) {
-    // your code here
-  }
+const isPrime = (num: number): boolean => {
+	for (let i = 2,sqrt = Math.sqrt(num); i <= sqrt; i++)
+		if (num % i === 0) return false;
+	return num > 1;
+};
+
+export const numPrimorial = (n: number): number => {
+	let primes: number[] = [];
+	for (let i = 2; primes.length < n; i++) if (isPrime(i)) primes.push(i);
+	return primes.reduce((curr, num) => curr * num, 1);
+};
