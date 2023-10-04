de533b03200b42b13c3e6406d0b00e190b3a1a2d
Cartago <cartago.relaxingcup@gmail.com>
2023-10-04 21:50:57 +0200

    Solucionamos la funcion generadora de rangos

--- src/typescript/exercises/6kyu/common-generator-functions.ts
+++ src/typescript/exercises/6kyu/common-generator-functions.ts
@@ -1,17 +1,26 @@
 //* https://www.codewars.com/kata/64f86726f8ce0f003342b79d/train/typescript
 
-export function* range(start?: number, stop?: number, step?: number): Generator<number> {
-  
+export function* range(
+	start: number = 0,
+	stop?: number,
+	step: number = 1
+): Generator<number> {
+	for (let i = start; i < (stop ?? Infinity); i += step) yield i;
 }
 
-export function* generate<T>(start: T, iterate: (e: T) => T, continues?: (e: T) => boolean): Generator<T> {
-  
-}
+export function* generate<T>(
+	start: T,
+	iterate: (e: T) => T,
+	continues?: (e: T) => boolean
+): Generator<T> {}
 
-export function* delta<T, D>(sequence: Iterable<T>, delta: (a: T, b: T) => D, start?: T): Generator<D> {
-  
-}
+export function* delta<T, D>(
+	sequence: Iterable<T>,
+	delta: (a: T, b: T) => D,
+	start?: T
+): Generator<D> {}
 
-export function* zip<T, U>(sequence1: Iterable<T>, sequence2: Iterable<U>): Generator<[T, U]> {
-  
-}
+export function* zip<T, U>(
+	sequence1: Iterable<T>,
+	sequence2: Iterable<U>
+): Generator<[T, U]> {}
