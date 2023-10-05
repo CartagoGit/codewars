6e00c19fffee632a25fc006173074980dcc18a64
Cartago <cartago.relaxingcup@gmail.com>
2023-10-05 22:30:29 +0200

    Encontramos una solucion a la puntuacion de consonantes

--- src/typescript/exercises/6kyu/consonant-value.ts
+++ src/typescript/exercises/6kyu/consonant-value.ts
@@ -1,18 +1,15 @@
 //* https://www.codewars.com/kata/59c633e7dcc4053512000073/train/typescript
 
 export function solve(s: string) {
-	const dictonary = 'abcdefghijklmnopqrstuvwxyz'
+	let points: Record<string, number> = 'abcdefghijklmnopqrstuvwxyz'
 		.split('')
-		.map((char, i) => ({ char, value: i + 1 }));
-	const vowels = /[^aeiou]+/gi;
+		.reduce((points, letter, i) => ({ ...points, [letter]: i + 1 }), {});
+	const vowels = new RegExp('[aeiou]', 'gi');
 	const consonants = s.trim().toLowerCase().split(vowels);
 	return consonants.reduce((acc, subs) => {
-		const valueSubs = subs.split('').reduce((acc, char) => {
-			return (acc += dictonary.find(
-				({ char: charDic }) => charDic === char
-			)!.value);
-		}, 0);
-		console.log({ acc, valueSubs, consonants, subs });
+		const valueSubs = subs
+			.split('')
+			.reduce((acc, char) => acc + (points[char] ?? 0), 0);
 		return Math.max(acc, valueSubs);
 	}, 0);
 }
