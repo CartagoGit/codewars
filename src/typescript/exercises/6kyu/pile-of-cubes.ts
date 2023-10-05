9108bcb365bb213573f908869dee4ed9d13faab0
Cartago <cartago.relaxingcup@gmail.com>
2023-10-05 21:53:37 +0200

    Solucionamos la kata de pila de cubos

--- src/typescript/exercises/6kyu/pile-of-cubes.ts
+++ src/typescript/exercises/6kyu/pile-of-cubes.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/typescript
 
 export function findNb(m: number): number {
-    // your code
-  }
+	for (let n = 0; n < m; n++) {
+		m -= Math.pow(n, 3);
+		if (m === 0) return n;
+	}
+    return -1;
+}
