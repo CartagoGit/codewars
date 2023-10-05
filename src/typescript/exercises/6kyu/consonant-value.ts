4b47fbdad304fed1c74037e41f0943a0751a2dce
Cartago <cartago.relaxingcup@gmail.com>
2023-10-05 22:32:23 +0200

    Optimizamos solucion

--- src/typescript/exercises/6kyu/consonant-value.ts
+++ src/typescript/exercises/6kyu/consonant-value.ts
@@ -1,11 +1,11 @@
 //* https://www.codewars.com/kata/59c633e7dcc4053512000073/train/typescript
 
-export function solve(s: string) {
-	let points: Record<string, number> = 'abcdefghijklmnopqrstuvwxyz'
+export function solve(word: string) {
+	word = word.trim().toLowerCase();
+	const points: Record<string, number> = 'abcdefghijklmnopqrstuvwxyz'
 		.split('')
 		.reduce((points, letter, i) => ({ ...points, [letter]: i + 1 }), {});
-	const vowels = new RegExp('[aeiou]', 'gi');
-	const consonants = s.trim().toLowerCase().split(vowels);
+	const consonants = word.split(/[aeiou]/gi);
 	return consonants.reduce((acc, subs) => {
 		const valueSubs = subs
 			.split('')
