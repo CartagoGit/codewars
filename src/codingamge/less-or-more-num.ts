e183a176d5acda44f5c780e897670ac2f49ff7cb
Cartago <cartago.relaxingcup@gmail.com>
2023-10-06 19:31:17 +0200

    Comenzamos a plantear el ejercicio de codingames

---
+++ src/codingamge/less-or-more-num.ts
@@ -0,0 +1,17 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+const results = ['Higher', 'Lower', 'Correct!'];
+let range = [1, 100]
+
+const N: number = parseInt(readline());
+const T: number = parseInt(readline());
+let guess : number = 0;
+ while( guess !== N) {
+	const guess: number = parseInt(readline());
+}
+// Write an answer using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('Higher/Lower/Correct!');
