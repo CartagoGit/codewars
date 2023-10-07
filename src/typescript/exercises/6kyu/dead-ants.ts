7e18c9deddc8261fcabb9dbd4ba42901ceda24c2
Cartago <cartago.relaxingcup@gmail.com>
2023-10-07 12:54:45 +0200

    Solucionamos la kata de hormigas muertas

--- src/typescript/exercises/6kyu/dead-ants.ts
+++ src/typescript/exercises/6kyu/dead-ants.ts
@@ -1,6 +1,12 @@
 //* https://www.codewars.com/kata/57d5e850bfcdc545870000b7/train/typescript
 
-export function deadAntCount (ants:string | null) : number {
-    // Your code here
-    return 0
-  }
+export function deadAntCount(ants: string | null): number {
+	if (!ants) return 0;
+	const partsBodyAnts = ants.replace(/ant/g, ' ');
+	const deadAnts = Math.max(
+		partsBodyAnts.match(/a/g)?.length ?? 0,
+		partsBodyAnts.match(/n/g)?.length ?? 0,
+		partsBodyAnts.match(/t/g)?.length ?? 0
+	);
+	return deadAnts;
+}
