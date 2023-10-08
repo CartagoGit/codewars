b23b470949f6499f96ace254710d7d37ae435039
Cartago <cartago.relaxingcup@gmail.com>
2023-10-08 15:13:25 +0200

    Cambiamos el enfoque del ejercicio

--- src/typescript/exercises/4kyu/array-vs-string.ts
+++ src/typescript/exercises/4kyu/array-vs-string.ts
@@ -1,65 +1,56 @@
 //* https://www.codewars.com/kata/582087e5b81f7063a7000242/train/typescript
 
-type Kind = 'increase' | 'decrease' | 'equal' | 'none';
-type DataKind = { kind: Kind; value: number };
-type RangeKind = {
-	kind: Kind;
-	value: number;
+type Kind = 'increase' | 'decrease' | 'equal';
+type DataKind = { kind: Kind; difference: number };
+type RangeKind = DataKind & {
 	range: { init: number; final: number };
 	startNumber: number;
 };
 
 const getDataKind = (a: number, b: number): DataKind => {
-	if (a < b) return { kind: 'increase', value: b - a };
-	else if (a > b) return { kind: 'decrease', value: a - b };
-	else if (a === b) return { kind: 'equal', value: a ?? b };
-	else return { kind: 'none', value: 0 };
+	if (a < b) return { kind: 'increase', difference: b - a };
+	else if (a > b) return { kind: 'decrease', difference: a - b };
+	else return { kind: 'equal', difference: a ?? b };
 };
 
 const getStringArray = (arg: RangeKind) => {
 	if (arg.kind === 'equal') {
-		return `${arg.value}:${arg.range.final - arg.range.init + 1}`;
+		return `${arg.difference}:${arg.range.final - arg.range.init + 1}`;
 	} else {
 		return `${arg.startNumber}:${arg.range.final + 1 - arg.range.init}${
 			arg.kind === 'increase' ? '+' : '-'
-		}${arg.value}`;
+		}${arg.difference}`;
 	}
 };
 export function arrayToString(arr: number[]): string {
 	console.log({ arr });
 	if (arr.length < 3) return arr.join(',');
 	let actual: RangeKind = {
-		kind: 'equal',
-		value: 0,
-		range: { init: 0, final: 0 },
+		...getDataKind(arr[0], arr[1]),
+		range: { init: 0, final: 1 },
 		startNumber: arr[0],
 	};
-	let isNewRange = true;
 	let rangesKinds: RangeKind[] = [];
-	for (let i = 1; i < arr.length; i++) {
-		const { kind, value } = getDataKind(arr[i - 1], arr[i]);
-		if (isNewRange) {
-			actual = {
-				kind,
-				value,
-				range: { init: i - 1, final: i },
-				startNumber: arr[i - 1],
-			};
-			isNewRange = false;
-			continue;
-		}
-		if (kind === 'none') return arr.join(',');
-		const { kind: actualKind, value: actualValue } = actual;
-		actual.range.final = i;
+	for (let i = 2; i < arr.length; i++) {
+		const { kind: actualKind, difference: actualDifference } = getDataKind(
+			arr[i - 1],
+			arr[i]
+		);
+		const { kind: prevKind, difference: prevDifference } = actual;
+		const isLast = i === arr.length - 1;
+		const areDifferent =
+			prevKind !== actualKind || prevDifference !== actualDifference;
 
-		if (
-			actualKind !== kind ||
-			actualValue !== value ||
-			i === arr.length - 1
-		) {
-			isNewRange = true;
-			if (actual.range.final - actual.range.init < 3) continue;
+		if (isLast || areDifferent) {
+			actual.range.final = i;
+			if (actual.range.final - actual.range.init < 2) continue;
 			rangesKinds.push(actual);
+			actual = {
+				kind: actualKind,
+				difference: actualDifference,
+				range: { init: i, final: i },
+				startNumber: arr[i],
+			};
 		}
 	}
 	console.log(1, {
@@ -85,11 +76,6 @@ export function arrayToString(arr: number[]): string {
 		if (j === rangesKinds.length - 1)
 			result.push(getStringArray(rangesKinds[j]));
 	}
-	console.log(2, {
-		actual,
-		rangesKinds,
-		ranges: rangesKinds.map((range) => range.range),
-	});
 	return result.join(',');
 }
 
