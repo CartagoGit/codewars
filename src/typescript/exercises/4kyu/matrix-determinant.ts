b99e789543a939886b08d2c4d4afdc693a84c730
Cartago <cartago.relaxingcup@gmail.com>
2023-10-10 20:13:10 +0200

    Pareparamos el ejercicio de hayar el determinante de una matriz

---
+++ src/typescript/exercises/4kyu/matrix-determinant.ts
@@ -0,0 +1,6 @@
+//*  https://www.codewars.com/kata/52a382ee44408cea2500074c/train/typescript
+
+export function determinant(m:number[][]) {
+    // return the determinant of the matrix passed in
+  }
+  
