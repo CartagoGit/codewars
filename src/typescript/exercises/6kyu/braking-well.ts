5d0b118a4ce467fe5ff42366c47cc0ed240f7491
Cartago <cartago.relaxingcup@gmail.com>
2023-10-10 21:40:12 +0200

    Enfocamos el ejercicio

--- src/typescript/exercises/6kyu/braking-well.ts
+++ src/typescript/exercises/6kyu/braking-well.ts
@@ -1,10 +1,17 @@
 //* https://www.codewars.com/kata/565c0fa6e3a7d39dee000125/train/typescript
 
+const gravity = 9.81; // Earth gravity m/s^2
+const reactionTime = 1; // Expected reaction time in seconds
+
 export function dist(speed: number, friction: number): number {
-	// suppose reaction time is 1
-	return 0;
+	const msSpeed = (speed * 1000) / 3600; // Parse speed from km/h to m/s
+	const brakeDistance = msSpeed ** 2 / (2 * friction * gravity);
+	const reactionDistance = msSpeed * reactionTime;
+	return brakeDistance + reactionDistance;
 }
+
 export function speed(distance: number, friction: number): number {
-	// suppose reaction time is 1
-	return 0;
+	const reactionDistance = distance - reactionTime;
+	const msSpeed = Math.sqrt(2 * friction * gravity * reactionDistance);
+	return (msSpeed * 3600) / 1000; // Parse speed from m/s to km/h
 }
