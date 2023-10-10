11526799c86a3b7fa7cef31daf3456bf073e0444
Cartago <cartago.relaxingcup@gmail.com>
2023-10-10 21:31:17 +0200

    Preparamos el ejercicios de braking well

---
+++ src/typescript/exercises/6kyu/braking-well.ts
@@ -0,0 +1,10 @@
+//* https://www.codewars.com/kata/565c0fa6e3a7d39dee000125/train/typescript
+
+export function dist(v: number, mu: number): number {
+	// suppose reaction time is 1
+	return 0;
+}
+export function speed(d: number, mu: number): number {
+	// suppose reaction time is 1
+	return 0;
+}
