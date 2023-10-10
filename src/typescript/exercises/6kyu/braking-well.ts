092b0dcef3d38b9b2b9cf9065c88867936876690
Cartago <cartago.relaxingcup@gmail.com>
2023-10-10 22:58:08 +0200

    Solucionamos la kata de las leyes de newton y coulomb

--- src/typescript/exercises/6kyu/braking-well.ts
+++ src/typescript/exercises/6kyu/braking-well.ts
@@ -1,7 +1,6 @@
-//* https://www.codewars.com/kata/565c0fa6e3a7d39dee000125/train/typescript
-
 const gravity = 9.81; // Earth gravity m/s^2
 const reactionTime = 1; // Expected reaction time in seconds
+const mass = 1; // Expected car mass in kg
 
 export function dist(speed: number, friction: number): number {
 	const msSpeed = (speed * 1000) / 3600; // Parse speed from km/h to m/s
@@ -11,7 +10,11 @@ export function dist(speed: number, friction: number): number {
 }
 
 export function speed(distance: number, friction: number): number {
-	const reactionDistance = distance - reactionTime;
-	const msSpeed = Math.sqrt(2 * friction * gravity * reactionDistance);
-	return (msSpeed * 3600) / 1000; // Parse speed from m/s to km/h
+	// This formula is based on fundamental laws of physics, including Newton's laws and the Coulomb's law of friction.
+	const speedDown: number = -(2 * friction * gravity * reactionTime) / mass;
+	// The quadratic formula is used to solve the motion equation with friction.
+	// The equation is: speedDown * t^2 + speedDown * t - 2 * distance = 0, and it's solved for t.
+	const msSpeed =
+		(speedDown + Math.sqrt(speedDown ** 2 - 4 * speedDown * distance)) / 2;
+	return (3600 * msSpeed) / 1000; // Convert speed from m/s to km/h
 }
