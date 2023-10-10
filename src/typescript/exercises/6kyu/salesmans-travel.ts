46267a3920d584ab394b9abb5f40fd8d8c1b94b8
Cartago <cartago.relaxingcup@gmail.com>
2023-10-10 21:28:58 +0200

    Solucionamos la kata de el vendedor y las calles

--- src/typescript/exercises/6kyu/salesmans-travel.ts
+++ src/typescript/exercises/6kyu/salesmans-travel.ts
@@ -1,6 +1,21 @@
 //* https://www.codewars.com/kata/56af1a20509ce5b9b000001e/train/typescript
 
-export const travel = (r:string, zipcode:string): string => {
-    // your code
-    return ''
-  }
+// Regexp to extract the house number, street and zipcode from the address
+const patternAddress = /^(\d+) (.+?)\s+([A-Z]{2} \d{5})$/;
+
+export const travel = (addressList: string, zipcode: string): string => {
+	if (!zipcode || !addressList.includes(zipcode)) return `${zipcode}:/`;
+	const addresses = addressList.split(',');
+	const streets: string[] = [];
+	const houseNumbers: string[] = [];
+	// Find the addresses that match the zipcode
+	addresses.forEach((address) => {
+		const [, houseNumber, street, zipFromAddress] =
+			address.match(patternAddress)!;
+		if (zipFromAddress !== zipcode) return;
+		streets.push(street);
+		houseNumbers.push(houseNumber);
+	});
+	if (streets.length === 0) return `${zipcode}:/`;
+	return `${zipcode}:${streets.join(',')}/${houseNumbers.join(',')}`;
+};
