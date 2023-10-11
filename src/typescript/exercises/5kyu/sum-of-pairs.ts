3dfb2f10a52d00f938996d08a6646afbfc0ced25
Cartago <cartago.relaxingcup@gmail.com>
2023-10-11 20:10:28 +0200

    Enfocamos el ejercicio

--- src/typescript/exercises/5kyu/sum-of-pairs.ts
+++ src/typescript/exercises/5kyu/sum-of-pairs.ts
@@ -1,5 +1,16 @@
 //* https://www.codewars.com/kata/54d81488b981293527000c8f/train/typescript
 
-export function sumPairs(ints: number[], s: number): [number, number] | void {  
-    return undefined // your code here...
-  }
+export function sumPairs(
+	nums: number[],
+	sum: number
+): [number, number] | undefined {
+	const uniques = [...new Set(nums).values()];
+
+	for (let i = 0; i < uniques.length; i++) {
+		for (let j = i + 1; j < uniques.length; j++) {
+			if (uniques[i] + uniques[j] === sum)
+				return [uniques[i], uniques[j]];
+		}
+	}
+	return undefined;
+}
