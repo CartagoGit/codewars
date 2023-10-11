26dd9c3f2978b858c1326f216c6b7344d2f637e5
Cartago <cartago.relaxingcup@gmail.com>
2023-10-11 20:36:38 +0200

    Dejamos una solucion lenta y buscamos una solucion mas eficiente

--- src/typescript/exercises/5kyu/sum-of-pairs.ts
+++ src/typescript/exercises/5kyu/sum-of-pairs.ts
@@ -1,16 +1,22 @@
 //* https://www.codewars.com/kata/54d81488b981293527000c8f/train/typescript
 
-export function sumPairs(
-	nums: number[],
-	sum: number
-): [number, number] | undefined {
-	const uniques = [...new Set(nums).values()];
+type Pair = [number, number];
+type PairData = { values: Pair; secondIndex: number };
 
-	for (let i = 0; i < uniques.length; i++) {
-		for (let j = i + 1; j < uniques.length; j++) {
-			if (uniques[i] + uniques[j] === sum)
-				return [uniques[i], uniques[j]];
+export function sumPairs(nums: number[], sum: number): Pair | undefined {
+	let pairs: PairData[] = [];
+	for (let i = 0; i < nums.length; i++) {
+		for (let j = i + 1; j < nums.length; j++) {
+			if (nums[i] + nums[j] === sum) {
+				pairs.push({ values: [nums[i], nums[j]], secondIndex: j });
+				break;
+			}
 		}
 	}
-	return undefined;
+	let result: PairData | undefined = undefined;
+	for (let pair of pairs) {
+		if (result === undefined || pair.secondIndex < result.secondIndex)
+			result = { ...pair };
+	}
+	return result?.values;
 }
