8ca3fd3fa9159994324cf1efc8b9f767655ef72e
Cartago <cartago.relaxingcup@gmail.com>
2023-10-11 20:47:54 +0200

    Encontramos una solucion mas eficiente con sets

--- src/typescript/exercises/5kyu/sum-of-pairs.ts
+++ src/typescript/exercises/5kyu/sum-of-pairs.ts
@@ -1,22 +1,36 @@
 //* https://www.codewars.com/kata/54d81488b981293527000c8f/train/typescript
 
 type Pair = [number, number];
-type PairData = { values: Pair; secondIndex: number };
+
+//* Looking for efficient solution
 
 export function sumPairs(nums: number[], sum: number): Pair | undefined {
-	let pairs: PairData[] = [];
-	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] + nums[j] === sum) {
-				pairs.push({ values: [nums[i], nums[j]], secondIndex: j });
-				break;
-			}
-		}
-	}
-	let result: PairData | undefined = undefined;
-	for (let pair of pairs) {
-		if (result === undefined || pair.secondIndex < result.secondIndex)
-			result = { ...pair };
-	}
-	return result?.values;
+    const numSet = new Set<number>();
+    for (const num of nums) {
+        const complement = sum - num;
+        if (numSet.has(complement)) return [complement, num];
+        numSet.add(num);
+    }
+    return undefined;
 }
+
+// type PairData = { values: Pair; secondIndex: number };
+
+// export function sumPairs(nums: number[], sum: number): Pair | undefined {
+// 	let pairs: PairData[] = [];
+// 	for (let i = 0; i < nums.length; i++) {
+// 		for (let j = i + 1; j < nums.length; j++) {
+// 			if (nums[i] + nums[j] === sum) {
+// 				pairs.push({ values: [nums[i], nums[j]], secondIndex: j });
+// 				break;
+// 			}
+// 		}
+// 	}
+// 	let result: PairData | undefined = undefined;
+// 	for (let pair of pairs) {
+// 		if (result === undefined || pair.secondIndex < result.secondIndex)
+// 			result = { ...pair };
+// 	}
+// 	return result?.values;
+// }
+
