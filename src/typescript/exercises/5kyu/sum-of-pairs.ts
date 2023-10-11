4d42fc2235510f4d8d0b38f1973d245c8645860e
Cartago <cartago.relaxingcup@gmail.com>
2023-10-11 19:58:23 +0200

    Preparamos el ejercicio de suma de pares

---
+++ src/typescript/exercises/5kyu/sum-of-pairs.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/54d81488b981293527000c8f/train/typescript
+
+export function sumPairs(ints: number[], s: number): [number, number] | void {  
+    return undefined // your code here...
+  }
