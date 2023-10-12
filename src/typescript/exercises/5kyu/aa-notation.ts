e747e6ea417c59fda5bc8be4416b8c04ba4891f2
Cartago <cartago.relaxingcup@gmail.com>
2023-10-12 20:13:09 +0200

    Solucionamos todos los problemas menos el de desbordamiento por numeros grandes

--- src/typescript/exercises/5kyu/aa-notation.ts
+++ src/typescript/exercises/5kyu/aa-notation.ts
@@ -7,11 +7,14 @@ export function formatNumber(startNum: number): string {
 	const sign = startNum < 0 ? '-' : '';
 	const cap = Math.pow(10, 3);
 	let num = Math.abs(startNum);
+	if (num > cap) console.log({ num });
 	let exp = 0;
 	while (num >= cap) {
+		
 		exp++;
 		num /= cap;
 	}
+	console.log({ num });
 	const int = Math.floor(num);
 	const intLength = 3 - int.toString().length;
 	const decLength = Math.pow(10, intLength);
@@ -21,7 +24,7 @@ export function formatNumber(startNum: number): string {
 	console.log({ startNum });
 	const finalNum = int + dec;
 	const result = (finalNum ? sign : '') + finalNum + getLetters(exp);
-	console.log({ result });
+	console.log({ result, finalNum });
 	return result;
 }
 
@@ -30,14 +33,14 @@ const getLetters = (exp: number) => {
 	if (exp < 5) return initLetters[exp];
 	exp -= 5;
 	const result = getFromAlphabet(exp).join('');
-	return result.length > 1 ? result : 'a' + result;
+	return result.length > 1 ? result : alphabet[0] + result;
 };
 
 const getFromAlphabet = (exp: number): string[] => {
 	const alphabetRepeatedTimes = Math.floor(exp / sizeAlphabet);
 	let letters: string[] = [];
 	if (alphabetRepeatedTimes)
-		letters.push(...getFromAlphabet(alphabetRepeatedTimes));
+		letters.push(...getFromAlphabet(alphabetRepeatedTimes).flat());
 	const position = exp % sizeAlphabet;
 	letters.push(alphabet[position]);
 	return letters;
