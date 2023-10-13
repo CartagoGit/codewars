8686d1b5108d30a0696fb18e5325f3188e8f0e20
Cartago <cartago.relaxingcup@gmail.com>
2023-10-13 19:01:36 +0200

    Solucionamos la kata de sumar numero hasta tener el mismo numero de bits

--- src/typescript/exercises/6kyu/basic-08-next-same-bit.ts
+++ src/typescript/exercises/6kyu/basic-08-next-same-bit.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/56bdd0aec5dc03d7780010a5/train/typescript
 
 export function nextHigher(num: number): number {
-    return 0;
-  }
+	// Count the number of bits '1' in the number
+	const countBits = (value: number) =>
+		value.toString(2).split('1').length - 1;
+	const initBits = countBits(num);
+	// Increment the number until we have the same number of bits '1'
+	while (initBits !== countBits(++num)) {}
+	return num;
+}
