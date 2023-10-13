fbf943d73e39b777b33f71cdb7824071f92e36d9
Cartago <cartago.relaxingcup@gmail.com>
2023-10-13 18:26:25 +0200

    Encontramos solucion a la kata de la ruta por el oeste

--- src/typescript/exercises/5kyu/directions-reduction.ts
+++ src/typescript/exercises/5kyu/directions-reduction.ts
@@ -1,5 +1,22 @@
 //* https://www.codewars.com/kata/550f22f4d758534c1100025a/train/typescript
 
-export function dirReduc(arr: string[]): string[] {
-    return []
+export type Direction = 'NORTH' | 'SOUTH' | 'EAST' | 'WEST';
+const oppositeDirections: { [key in Direction]: Direction } = {
+	NORTH: 'SOUTH',
+	SOUTH: 'NORTH',
+	EAST: 'WEST',
+	WEST: 'EAST',
+};
+
+export function dirReduc(route: string[]): Direction[] {
+	let result = [...route] as Direction[];
+	for (let i = 0; i < result.length; i++) {
+		const current = result[i];
+		const next = result[i + 1];
+		if (next === oppositeDirections[current]) {
+			result.splice(i, 2);
+			i = -1;
+		}
+	}
+	return result;
 }
