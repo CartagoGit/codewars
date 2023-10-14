9611a1a9a0a1bfac8737ea77ccc47ec46093b3d3
Cartago <cartago.relaxingcup@gmail.com>
2023-10-14 17:34:49 +0200

    Preparamos el ejercicio de pongs

---
+++ src/typescript/exercises/6kyu/pongs.ts
@@ -0,0 +1,13 @@
+//* https://www.codewars.com/kata/5b432bdf82417e3f39000195/train/typescript
+
+export class Pong {
+	maxScore: number;
+
+	constructor(_maxScore: number) {
+		this.maxScore = _maxScore;
+	}
+
+	play(ballPos: number, playerPos: number): string {
+		return '';
+	}
+}
