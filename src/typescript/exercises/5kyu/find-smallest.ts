0a617f6f31b1738a3b995c4b9eaa6cc46b068e1a
Cartago <cartago.relaxingcup@gmail.com>
2023-10-15 20:09:46 +0200

    Damos soluciuon a la kata

--- src/typescript/exercises/5kyu/find-smallest.ts
+++ src/typescript/exercises/5kyu/find-smallest.ts
@@ -1,6 +1,17 @@
 //* https://www.codewars.com/kata/573992c724fc289553000e95/train/typescript
 
-export function smallest(n: number): number[] {
-    
-    return []
-  }
+export function smallest(num: number): number[] {
+	let result: [number, number, number] = [num, 0, 0]; // [smallest, initIndex, finalIndex]
+	const nums = num.toString().split('').map(Number);
+	for (let initIndex = 0; initIndex < nums.length; initIndex++) {
+		for (let finalIndex = 0; finalIndex < nums.length; finalIndex++) {
+			if (initIndex === finalIndex) continue;
+			const newNums = [...nums];
+			newNums.splice(initIndex, 1);
+			newNums.splice(finalIndex, 0, nums[initIndex]);
+			const newNum = Number(newNums.join(''));
+			if (newNum < result[0]) result = [newNum, initIndex, finalIndex];
+		}
+	}
+	return result;
+}
