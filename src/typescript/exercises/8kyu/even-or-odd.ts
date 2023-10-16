1c2633d2f745c07fb6e359aae4072ce172fd2531
Cartago <cartago.relaxingcup@gmail.com>
2023-10-16 22:47:55 +0200

    Realizamos el ejercicio de even or odd

--- src/typescript/exercises/8kyu/even-or-odd.ts
+++ src/typescript/exercises/8kyu/even-or-odd.ts
@@ -1,5 +1,4 @@
 //* https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/typescript
 
-export function evenOrOdd(num:number):string {
-    return "";
-  }
+export const evenOrOdd = (num: number): string =>
+	num % 2 === 0 ? 'Even' : 'Odd';
