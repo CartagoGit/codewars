60299444b8488e7f982b45bf1065401b50883f72
Cartago <cartago.relaxingcup@gmail.com>
2023-10-17 18:35:28 +0200

    Solucionamos el ejercicio

--- src/typescript/exercises/8kyu/array-plus-array.ts
+++ src/typescript/exercises/8kyu/array-plus-array.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/typescript
 
-export const arrayPlusArray = (arr1 : number[], arr2 : number[]) : number => {
-    return arr1 + arr2; // something went wrong ?
-  }
+export const arrayPlusArray = (arr1: number[], arr2: number[]): number => {
+	return [...arr1, ...arr2].reduce((curr, num) => (curr += num), 0);
+};
