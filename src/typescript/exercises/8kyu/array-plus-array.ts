b30733a0584a18d1572e76c6f919a350ad411e80
Cartago <cartago.relaxingcup@gmail.com>
2023-10-17 18:33:48 +0200

    Planteamos el ejercicio de array plus array

---
+++ src/typescript/exercises/8kyu/array-plus-array.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/5a2be17aee1aaefe2a000151/train/typescript
+
+export const arrayPlusArray = (arr1 : number[], arr2 : number[]) : number => {
+    return arr1 + arr2; // something went wrong ?
+  }
