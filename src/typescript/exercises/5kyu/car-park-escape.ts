de2a42be2d9c366820d9047348639ac4dd01cfbb
Cartago <cartago.relaxingcup@gmail.com>
2023-10-19 21:07:08 +0200

    Enfocamos el ejercicio

---
+++ src/typescript/exercises/5kyu/car-park-escape.ts
@@ -0,0 +1,40 @@
+//* https://www.codewars.com/kata/591eab1d192fe0435e000014/train/typescript
+
+export function escape(carpark: number[][]): string[] {
+	console.log({ carpark });
+	let startPlace: { floor: number; position: number };
+	// Get start place
+	carpark.forEach((element, index) => {
+		const place = element.indexOf(2);
+		if (place !== -1)
+			return (startPlace = { floor: index, position: place });
+	});
+	let actualPlace = { ...startPlace! };
+	const result: string[] = [];
+
+	for (let floor = startPlace!.floor; floor < carpark.length; floor++) {
+		const staircase = carpark[floor].indexOf(1);
+		// If is last floor
+		console.log({ staircase, actualPlace });
+		if (staircase === -1) {
+			if (actualPlace.position === carpark[floor].length - 1) break;
+			const steps = carpark[floor].length - actualPlace.position - 1;
+			result.push(`R${steps}`);
+			break;
+		}
+		// Else
+		const side: string = actualPlace.position > staircase ? 'L' : 'R';
+		const steps = Math.abs(actualPlace.position - staircase);
+		result.push(`${side}${steps}`);
+		// Look for downstairs
+		for (let downs = floor + 1; downs < carpark.length; downs++) {
+			const nextStaircase = carpark[downs].indexOf(1);
+			if (nextStaircase !== -1 && nextStaircase === staircase) continue;
+			result.push(`D${downs - floor}`);
+			floor = downs - 1;
+			actualPlace = { floor: downs, position: staircase };
+			break;
+		}
+	}
+	return result;
+}
