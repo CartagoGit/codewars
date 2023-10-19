b4ea43a0def9bb8ce7a6347e153a81d1b0a25172
Cartago <cartago.relaxingcup@gmail.com>
2023-10-19 23:29:13 +0200

    Solucionamos la kata de la tragaperras

--- src/typescript/exercises/6kyu/fruit-machine.ts
+++ src/typescript/exercises/6kyu/fruit-machine.ts
@@ -1,29 +1,14 @@
 //* https://www.codewars.com/kata/590adadea658017d90000039/train/typescript
 
-const posibilities = [
-	'Jack',
-	'Queen',
-	'King',
-	'Bar',
-	'Cherry',
-	'Seven',
-	'Shell',
-	'Bell',
-	'Star',
-	'Wild',
-];
+const posibilities = ['Jack','Queen','King','Bar','Cherry','Seven','Shell','Bell','Star','Wild'] as const;
 
 export function fruit(reels: string[][], spins: number[]): number {
-	const result = [reels[0][spins[0]], reels[1][spins[1]], reels[2][spins[2]]];
-	console.log({ reels, spins, result });
+	const result = [reels[0][spins[0]], reels[1][spins[1]], reels[2][spins[2]]] as typeof posibilities[number][];
 	const points = Math.max(
-		...result.map((element) => {
-			const valuePossibility = posibilities.indexOf(element) + 1;
-			const counter = result.filter(
-				(element) => element === element
-			).length;
-			const hasWild = result.includes('Wild') && element !== 'Wild';
-			console.log({ result, element, valuePossibility, counter, hasWild });
+		...result.map((reel) => {
+			const valuePossibility = posibilities.indexOf(reel ) + 1;
+			const counter = result.filter((element) => element === reel).length;
+			const hasWild = result.includes('Wild') && reel !== 'Wild';
 			if (counter === 3) return valuePossibility * 10;
 			else if (counter === 2) return valuePossibility * (hasWild ? 2 : 1);
 			else return 0;
