d87b1b97218b203465f8089acfe587277bffe139
Cartago <cartago.relaxingcup@gmail.com>
2023-10-20 21:42:37 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/color-choice.ts
+++ src/typescript/exercises/6kyu/color-choice.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/55be10de92aad5ef28000023/train/typescript
 
 export function checkchoose(numPosters: number, numColors: number): number {
-	return 0;
+	let x = 1;
+	let nChooseX = numColors;
+	while (nChooseX !== numPosters && x <= numColors) {
+		x++;
+		nChooseX = (nChooseX * (numColors - x + 1)) / x;
+	}
+	return nChooseX === numPosters ? x : -1;
 }
