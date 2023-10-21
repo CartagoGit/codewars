f1a94d2031202b97e1fd7e22f129b2f3b221a883
Cartago <cartago.relaxingcup@gmail.com>
2023-10-21 05:03:59 +0200

    Encontramos solucion a la kata

--- src/typescript/exercises/6kyu/tank-truck.ts
+++ src/typescript/exercises/6kyu/tank-truck.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/55f3da49e83ca1ddae0000ad/train/typescript
 
-export function tankvol(height: number, diameter: number, volume: number) {
-	// your code
-}
+export const tankvol = (
+	height: number,
+	diameter: number,
+	volumeTotal: number
+): number => {
+	const radius = diameter / 2;
+	const tankLong = volumeTotal / (Math.PI * radius * radius);
+	const theta = 2 * Math.acos((radius - height) / radius);
+	const liquidArea = (radius * radius * (theta - Math.sin(theta))) / 2;
+	const liquidVol = liquidArea * tankLong;
+	return Math.floor(liquidVol);
+};
