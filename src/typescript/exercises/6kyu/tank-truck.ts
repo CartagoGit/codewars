e1c62d470397cf2f8d3bee69ada18e0c7cfaa811
Cartago <cartago.relaxingcup@gmail.com>
2023-10-21 04:41:47 +0200

    Modificamos el nombre de las variables

--- src/typescript/exercises/6kyu/tank-truck.ts
+++ src/typescript/exercises/6kyu/tank-truck.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/55f3da49e83ca1ddae0000ad/train/typescript
 
-export function tankvol(h:number, d:number, vt:number) {
-    // your code
-  }
+export function tankvol(height: number, diameter: number, volume: number) {
+	// your code
+}
