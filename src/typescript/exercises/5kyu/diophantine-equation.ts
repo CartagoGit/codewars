a692c2bb4bb31b2d74b905cfb0ec1a0c2fbf52bc
Cartago <cartago.relaxingcup@gmail.com>
2023-10-22 14:26:21 +0200

    Damos solucion a la kata

--- src/typescript/exercises/5kyu/diophantine-equation.ts
+++ src/typescript/exercises/5kyu/diophantine-equation.ts
@@ -1,7 +1,16 @@
 //* https://www.codewars.com/kata/554f76dca89983cc400000bb/train/typescript
 
-export function solequa(n: number): [number, number][] {
-    const result: [number, number][] = [];
-    
-    return result;
+// x2 - 4 * y2 = n
+// x2 - 4 * y2 = (x - 2*y) * (x + 2*y)
+export function solequa(num: number): [number, number][] {
+	const solutions: [number, number][] = [];
+	const sqrt = Math.sqrt(num);
+	for (let i = 1; i <= sqrt; i++) {
+		if (num % i !== 0) continue;
+		const divisor = num / i;
+		const x = (divisor + i) / 2;
+		const y = (divisor - i) / 4;
+		if ([x, y].every(Number.isInteger)) solutions.push([x, y]);
+	}
+	return solutions;
 }
