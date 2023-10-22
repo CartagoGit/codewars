6d1a32511ad6461c9ba111252bf8cb6fd566760b
Cartago <cartago.relaxingcup@gmail.com>
2023-10-22 14:15:40 +0200

    Preparamos el ejercicio de Diophantine Equation

---
+++ src/typescript/exercises/5kyu/diophantine-equation.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/554f76dca89983cc400000bb/train/typescript
+
+export function solequa(n: number): [number, number][] {
+    const result: [number, number][] = [];
+    
+    return result;
+}
