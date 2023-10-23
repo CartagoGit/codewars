8c88f86017d1dcd69c3fdb0e9cdf29cb5c93f963
Cartago <cartago.relaxingcup@gmail.com>
2023-10-23 22:07:06 +0200

    Solucionamos la kata de up and down

--- src/typescript/exercises/6kyu/up-and-down.ts
+++ src/typescript/exercises/6kyu/up-and-down.ts
@@ -1,5 +1,20 @@
 //* https://www.codewars.com/kata/56cac350145912e68b0006f0/train/typescript
 
 export function arrange(strng: string): string {
-    return ''
+	console.log({ strng });
+	const words = strng.split(' ');
+	for (let i = 0; i < words.length - 1; i++) {
+		const prevWord = words[i + 1] ?? '';
+		const currWord = words[i];
+		const prevLength = prevWord.length;
+		const currLength = currWord.length;
+		const isMinus =
+			i % 2 ? prevLength > currLength : prevLength < currLength;
+		if (isMinus) [words[i], words[i + 1]] = [prevWord, currWord];
+	}
+	return words
+		.map((value, index) =>
+			index % 2 ? value.toUpperCase() : value.toLowerCase()
+		)
+		.join(' ');
 }
