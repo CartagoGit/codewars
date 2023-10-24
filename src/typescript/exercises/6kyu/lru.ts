3e532995f170755fc3e2639d64ae3254a500d9e3
Cartago <cartago.relaxingcup@gmail.com>
2023-10-24 17:39:58 +0200

    Comenzamos el ejercicio

--- src/typescript/exercises/6kyu/lru.ts
+++ src/typescript/exercises/6kyu/lru.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/6329d94bf18e5d0e56bfca77/train/typescript
 
-export function lru(num: number, referencesList: number[]): number[] {
-    return [];
-  }
+export function lru(numMemory: number, referencesList: number[]): number[] {
+	const memory: number[] = new Array(numMemory).fill(0);
+	for (let i = 0; i < referencesList.length; i++) {}
+	return memory;
+}
