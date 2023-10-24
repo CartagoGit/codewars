938528423dea0b8855b6cf26bbcafcbf56676d9a
Cartago <cartago.relaxingcup@gmail.com>
2023-10-24 18:40:59 +0200

    Completamos el ejercicio de lru

--- src/typescript/exercises/6kyu/lru.ts
+++ src/typescript/exercises/6kyu/lru.ts
@@ -1,7 +1,29 @@
 //* https://www.codewars.com/kata/6329d94bf18e5d0e56bfca77/train/typescript
 
 export function lru(numMemory: number, referencesList: number[]): number[] {
-	const memory: number[] = new Array(numMemory).fill(0);
-	for (let i = 0; i < referencesList.length; i++) {}
+	let memory: number[] = Array(numMemory).fill(-1);
+	let lastReferences: number[] = [];
+
+	for (let i = 0; i < referencesList.length; i++) {
+		const reference = referencesList[i];
+		if (!lastReferences.includes(reference)) {
+			// If the reference is not in memory
+			if (lastReferences.length >= numMemory) {
+				// If memory is full, replace the least recently used page
+				let leastRecentPage = lastReferences[0];
+				let indexInMemory = memory.indexOf(leastRecentPage);
+				memory[indexInMemory] = reference;
+				lastReferences.shift();
+			} else {
+				// If memory is not full, add the reference to the first available slot
+				let firstAvailableSlot = memory.indexOf(-1);
+				if (firstAvailableSlot !== -1)
+					memory[firstAvailableSlot] = reference;
+			}
+		}
+		// If the reference is in memory, move it to the end of the list
+		else lastReferences = lastReferences.filter((ref) => ref !== reference);
+		lastReferences.push(reference);
+	}
 	return memory;
 }
