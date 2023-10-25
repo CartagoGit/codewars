e6e64b66f860f97f3d60f281e9ce903f127dea4e
Cartago <cartago.relaxingcup@gmail.com>
2023-10-25 18:00:52 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/moves-in-squared-strings.ts
+++ src/typescript/exercises/6kyu/moves-in-squared-strings.ts
@@ -1,13 +1,21 @@
 //* https://www.codewars.com/kata/56dbe7f113c2f63570000b86/train/typescript
 
-export function rot(chain: string): string {
-	return '';
-}
+export const rot = (chain: string): string =>
+	chain
+		.split('\n')
+		.reverse()
+		.map((value) => value.split('').reverse().join(''))
+		.join('\n');
 
-export function selfieAndRot(chain: string): string {
-	return '';
-}
+export const selfieAndRot = (chain: string): string => {
+	const chainSide = chain
+		.split('\n')
+		.map((value) => value + '.'.repeat(value.length));
+	const rotSide = rot(chain)
+		.split('\n')
+		.map((value, index) => '.'.repeat(value.length) + value);
+	return [...chainSide, ...rotSide].join('\n');
+};
 
-export function oper(fn: (chain: string) => string, chainCb: string): string {
-	return '';
-}
+export const oper = (fn: (chain: string) => string, chainCb: string): string =>
+	fn(chainCb);
