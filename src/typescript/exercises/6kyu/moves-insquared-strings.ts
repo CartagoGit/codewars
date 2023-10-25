b19e40a3965f418cf242c129db65f6a1cbe26e05
Cartago <cartago.relaxingcup@gmail.com>
2023-10-25 17:32:59 +0200

    Preparamos el ejercicio de moves in  squared strings

---
+++ src/typescript/exercises/6kyu/moves-insquared-strings.ts
@@ -0,0 +1,13 @@
+//* https://www.codewars.com/kata/56dbe7f113c2f63570000b86/train/typescript
+
+export function rot(s: string): string {
+	return '';
+}
+
+export function selfieAndRot(s: string): string {
+	return '';
+}
+
+export function oper(fn: (s: string) => string, s: string): string {
+	return '';
+}
