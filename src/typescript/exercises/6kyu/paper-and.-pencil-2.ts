0fb8f4c92711d0b8a655d1f8ada121e537b0a4fe
Cartago <cartago.relaxingcup@gmail.com>
2023-10-28 15:26:05 +0200

    Planteamos el ejercicio de las cuadraticas

--- src/typescript/exercises/6kyu/paper-and.-pencil-2.ts
+++ src/typescript/exercises/6kyu/paper-and.-pencil-2.ts
@@ -1,27 +1,17 @@
 //* https://www.codewars.com/kata/63bd8cc3a78e0578b608ac80/train/typescript
 
-export function quadraticFormula(
-	y1: number,
-	y2: number,
-	y3: number
-): [string, number, number] {
-	// Calculamos los coeficientes de la fórmula
-	const a = y3 - 2 * y2 + y1;
-	const b = y2 - y1;
-	const c = y1;
-
-	// Construimos la fórmula en el formato adecuado
-	let formula = `x^2`;
-	if (b !== 0) {
-		formula += b > 0 ? `+${b}x` : `${b}x`;
-	}
-	if (c !== 0) {
-		formula += c > 0 ? `+${c}` : `${c}`;
-	}
-
-	// Calculamos los siguientes dos términos de la secuencia
-	const y4 = a + 3 * b + 3 * c;
-	const y5 = a + 4 * b + 6 * c;
-
-	return [formula, y4, y5];
+export const quadraticFormula = (
+	first: number,
+	second: number,
+	third: number
+): [string, number, number] => {
+    let a = second - first;
+    let b = third - second - a;
+    let c = first - a - b;
+    let formula = `x^2${b>=0?'+':''}${b}x${c>=0?'+':''}${c}`;
+    
+    let fourth = (4 * 4) + (b * 4) + c;
+    let fifth = (5 * 5) + (b * 5) + c;
+    
+    return [formula, fourth, fifth];
 }
