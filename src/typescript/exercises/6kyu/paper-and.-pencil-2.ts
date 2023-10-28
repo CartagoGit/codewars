d75cf0b4149c4e4e5890d9ead06bdfa5efbe6157
Cartago <cartago.relaxingcup@gmail.com>
2023-10-28 17:43:04 +0200

    Reenfocamos la kata

--- src/typescript/exercises/6kyu/paper-and.-pencil-2.ts
+++ src/typescript/exercises/6kyu/paper-and.-pencil-2.ts
@@ -1,40 +1,39 @@
+//* https://www.codewars.com/kata/63bd8cc3a78e0578b608ac80/train/typescript
+
+/* 
+            y1 = Math.pow(a*1,2) + b*1 + c
+            y2 = Math.pow(a*2,2) + b*2 + c
+            y3 = Math.pow(a*3,2) + b*3 + c
+            ...
+            Resolve -> 
+            y4 = Math.pow(a*4,2) + b*4 + c
+            y5 = Math.pow(a*5,2) + b*5 + c
+*/
+
+
 export const quadraticFormula = (
 	y1: number,
 	y2: number,
 	y3: number
 ): [string, number, number] => {
-	let formula = `x^2`;
-	if (b !== 0) {
-		formula += `+${y2}x`;
-	}
-	if (c !== 0) {
-		formula += `+${c}`;
-	}
+	const a = 1;
+	const b = (y2 - y1) / (2 * (2 - 1));
+	const c = y1 - b * (1 * 1) - 1 * 1 * (1 * 1);
 
-	// Calcula los dos siguientes términos
-	const fourthTerm = a + 2 * b + c;
-	const fifthTerm = a + 3 * b + c;
+	const formula = getFormula(b, c);
+	const y4 = Math.pow(a * 4, 2) + b * 4 + c;
+	const y5 = Math.pow(a * 5, 2) + b * 5 + c;
 
-	return [formula, fourthTerm, fifthTerm];
+	return [formula, y4, y5];
 };
 
-/* 
-            y1 = a*1^2 + b*1 + c
-            y2 = a*2^2 + b*2 + c
-            y3 = a*3^2 + b*3 + c
-            ...
-            Resolve -> 
-            y4 = a*4^2 + b*4 + c
-            y5 = a*5^2 + b*5 + c
-*/
+const getFormula = (b: number, c: number) => {
+	let bString = '';
+	if (b > 1) bString = `+${b}x`;
+	else if (b === 1) bString = '+x';
+	else if (b < 0) bString = `${b}x`;
+	let cString = c === 0 ? '' : c > 0 ? `+${c}` : `${c}`;
+	return `x^2${bString}${cString}`;
+};
 
-// // Form the quadratic formula
-// const formula = `x^2${b > 0 ? '+' + b + 'x' : b < 0 ? b + 'x' : ''}${
-//     c > 0 ? '+' + c : c < 0 ? c : ''
-// }`;
 
-// // Calculate the fourth and fifth terms of the sequence
-// const y4 = a * 4 * 4 + b * 4 + c;
-// const y5 = a * 5 * 5 + b * 5 + c;
-
-// return [formula, y4, y5];
