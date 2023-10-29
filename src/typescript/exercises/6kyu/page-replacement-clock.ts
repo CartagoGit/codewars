3287835ce71972f026f0e925c9708d6f219cf43f
Cartago <cartago.relaxingcup@gmail.com>
2023-10-29 16:16:12 +0100

    Modificamos la logica y la rehacemos

--- src/typescript/exercises/6kyu/page-replacement-clock.ts
+++ src/typescript/exercises/6kyu/page-replacement-clock.ts
@@ -1,33 +1,25 @@
 //* https://www.codewars.com/kata/62f23d84eb2533004be50c0d/train/typescript
 
-export function clock(n: number, referenceList: number[]): number[] {
+export function clock(maxMemory: number, refList: number[]): number[] {
 	let clock: { value: number; rBit: boolean }[] = [];
-	for (let reference of referenceList) {
-		const index = clock.findIndex((item) => item.value === reference);
-		if (index !== -1) clock[index].rBit = true;
+	let iterator = 0;
+	for (let refIndex = 0; refIndex < refList.length; refIndex++) {
+		const reference = refList[refIndex];
+		console.log('START ->>', { reference, clock, iterator });
+		const clockIndex = clock.findIndex((item) => item.value === reference);
+		if (clockIndex !== -1) clock[clockIndex].rBit = true;
 		else {
-			if (clock.length < n) {
-				clock.push({ value: reference, rBit: true });
+			if (clock.length < maxMemory) {
+				clock.push({ value: reference, rBit: false });
 				continue;
 			}
-			let wasFound = false;
-			for (let mem of clock) {
-				if (!mem.rBit) {
-					mem = {
-						value: reference,
-						rBit: true,
-					};
-					wasFound = true;
-					break;
-				} else mem.rBit = false;
-			}
-			if (!wasFound) {
-				clock[0] = {
-					value: reference,
-					rBit: true,
-				};
-			}
 		}
+		iterator = iterator++ % maxMemory;
+		console.log('<<-- END', { reference, clock, iterator });
 	}
-	return clock.map((item) => item.value);
+	const fullClock = [
+		...clock.map((item) => item.value),
+		...Array(maxMemory - clock.length).fill(-1),
+	];
+	return fullClock;
 }
