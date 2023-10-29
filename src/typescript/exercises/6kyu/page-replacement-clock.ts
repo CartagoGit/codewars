c96a837f3496335e0565a1f2b2b4ea97682d60dd
Cartago <cartago.relaxingcup@gmail.com>
2023-10-29 16:34:16 +0100

    MEjoramos codigo del reloj

--- src/typescript/exercises/6kyu/page-replacement-clock.ts
+++ src/typescript/exercises/6kyu/page-replacement-clock.ts
@@ -6,33 +6,33 @@ export function clock(maxMemory: number, refList: number[]): number[] {
 	for (let refIndex = 0; refIndex < refList.length; refIndex++) {
 		const reference = refList[refIndex];
 		const clockIndex = clock.findIndex((item) => item.value === reference);
-        // If reference is in clock
+		// If reference is in clock
 		if (clockIndex !== -1) {
 			clock[clockIndex].rBit = true;
 			continue;
 		}
 		// If clock is not full
-        if (clock.length < maxMemory) {
-            clock.push({ value: reference, rBit: false });
-            iterator = ++iterator % maxMemory;
+		if (clock.length < maxMemory) {
+			clock.push({ value: reference, rBit: false });
+			iterator = ++iterator % maxMemory;
 			continue;
 		}
-        // Check value in iterator position on clock until find a value with rBit = false
-        while(true){
-            const actualItem = clock[iterator];
-            if(actualItem.rBit){
-                actualItem.rBit = false;
-            }else{
-                actualItem.value = reference;
-                iterator = ++iterator % maxMemory;
-                break;
-            }
-            iterator = ++iterator % maxMemory;
-        }
+		// Check value in iterator position on clock until find a value with rBit = false
+		while (true) {
+			const actualItem = clock[iterator];
+			iterator = ++iterator % maxMemory;
+			if (actualItem.rBit) {
+				actualItem.rBit = false;
+			} else {
+				actualItem.value = reference;
+				break;
+			}
+		}
 	}
-	const fullClock = [
+    // Return fill clock
+	const fillClock = [
 		...clock.map((item) => item.value),
 		...Array(maxMemory - clock.length).fill(-1),
 	];
-	return fullClock;
+	return fillClock;
 }
