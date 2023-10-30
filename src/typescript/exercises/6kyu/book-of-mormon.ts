031f76061bf24353681fd3660e861a000d647f57
Cartago <cartago.relaxingcup@gmail.com>
2023-10-30 21:06:52 +0100

    Encontramos solucion a la kata con while

--- src/typescript/exercises/6kyu/book-of-mormon.ts
+++ src/typescript/exercises/6kyu/book-of-mormon.ts
@@ -1,9 +1,18 @@
 //* https://www.codewars.com/kata/58373ba351e3b615de0001c3/train/typescript
 
-export function Mormons(
+
+
+export function MormonsWhile(
 	startingNumber: number,
 	reach: number,
 	target: number
 ): number {
-	return 0;
+	if (startingNumber >= target) return 0;
+    let followers = 1;
+    while(startingNumber + reach * startingNumber < target) {
+        followers++;
+        startingNumber += reach * startingNumber;
+
+    }
+	return followers;
 }
