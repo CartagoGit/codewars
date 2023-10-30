37e286e809a4094256340ffd073eb7f082ece8ed
Cartago <cartago.relaxingcup@gmail.com>
2023-10-30 21:08:09 +0100

    Encontramos una solucion a la kata de forma recursiva

--- src/typescript/exercises/6kyu/book-of-mormon.ts
+++ src/typescript/exercises/6kyu/book-of-mormon.ts
@@ -1,18 +1,32 @@
 //* https://www.codewars.com/kata/58373ba351e3b615de0001c3/train/typescript
 
-
-
 export function MormonsWhile(
 	startingNumber: number,
 	reach: number,
 	target: number
 ): number {
 	if (startingNumber >= target) return 0;
-    let followers = 1;
-    while(startingNumber + reach * startingNumber < target) {
-        followers++;
-        startingNumber += reach * startingNumber;
+	let followers = 1;
+	while (startingNumber + reach * startingNumber < target) {
+		followers++;
+		startingNumber += reach * startingNumber;
+	}
+	return followers;
+}
 
-    }
+export function Mormons(
+	startingNumber: number,
+	reach: number,
+	target: number,
+	followers: number = 1
+): number {
+	if (startingNumber >= target) return 0;
+	if (startingNumber + reach * startingNumber < target)
+		return Mormons(
+			startingNumber + reach * startingNumber,
+			reach,
+			target,
+			followers + 1
+		);
 	return followers;
 }
