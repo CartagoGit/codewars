df7bd6acca3525c12ed07f4d1c26570f25519239
Cartago <cartago.relaxingcup@gmail.com>
2023-10-30 20:55:14 +0100

    Preparamos el ejercicio de The Book of Mormon

---
+++ src/typescript/exercises/6kyu/book-of-mormon.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/58373ba351e3b615de0001c3/train/typescript
+
+export function Mormons(
+	startingNumber: number,
+	reach: number,
+	target: number
+): number {
+	return 0;
+}
