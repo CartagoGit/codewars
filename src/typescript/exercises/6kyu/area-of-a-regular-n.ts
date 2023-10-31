fbf444cedd5dbe0bd015eb2c21e99d5cf9a21ea4
Cartago <cartago.relaxingcup@gmail.com>
2023-10-31 17:34:48 +0100

    Damos solucion a la kata de calcular el area de un poligono segun sus lados y el radio del circulo envolvente

--- src/typescript/exercises/6kyu/area-of-a-regular-n.ts
+++ src/typescript/exercises/6kyu/area-of-a-regular-n.ts
@@ -4,5 +4,15 @@ export function areaOfPolygonInsideCircle(
 	circleRadius: number,
 	numberOfSides: number
 ): number {
-	return 0; 
+	if (numberOfSides < 3) return 0;
+	const centralAngle = (2 * Math.PI) / numberOfSides;
+	const apothem = circleRadius * Math.cos(centralAngle / 2);
+	const sideLength = 2 * apothem * Math.tan(centralAngle / 2);
+	const area = (numberOfSides * sideLength * apothem) / 2;
+
+	// Round 3 decimals
+	const numDecimals = 3;
+	const decimalsPow10 = Math.pow(10, numDecimals);
+	const result = Math.round(area * decimalsPow10) / decimalsPow10;
+	return result;
 }
