fe25312529d1a95b4879e34b2a5952447e89d679
Cartago <cartago.relaxingcup@gmail.com>
2023-10-31 17:28:15 +0100

    Preparamos la kata de Calculate the area of a regular n sides polygon inside a circle of radius r

---
+++ src/typescript/exercises/6kyu/area-of-a-regular-n.ts
@@ -0,0 +1,8 @@
+//* https://www.codewars.com/kata/5a58ca28e626c55ae000018a/train/typescript
+
+export function areaOfPolygonInsideCircle(
+	circleRadius: number,
+	numberOfSides: number
+): number {
+	return 0; 
+}
