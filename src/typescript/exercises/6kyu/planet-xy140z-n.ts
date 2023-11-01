304a32885bc8463465ffc88b4cb54b54beee65a2
Cartago <cartago.relaxingcup@gmail.com>
2023-11-01 09:34:04 +0100

    Solucionamos la kata del planeta

--- src/typescript/exercises/6kyu/planet-xy140z-n.ts
+++ src/typescript/exercises/6kyu/planet-xy140z-n.ts
@@ -1,6 +1,4 @@
-
 //* https://www.codewars.com/kata/559ce00b70041bc7b600013d/train/typescript
 
-export function finance(num: number): number {
-    return 0;
-}
+export const finance = (num: number): number =>
+	(num * (num + 1) * (num + 2)) / 2;
