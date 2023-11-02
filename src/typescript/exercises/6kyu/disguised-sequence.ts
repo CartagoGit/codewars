5ae1b628a823aaeda34e654b6d9e1545a06ea45c
Cartago <cartago.relaxingcup@gmail.com>
2023-11-02 10:32:07 +0100

    Damos solucion a la kata

--- src/typescript/exercises/6kyu/disguised-sequence.ts
+++ src/typescript/exercises/6kyu/disguised-sequence.ts
@@ -1,6 +1,3 @@
 //* https://www.codewars.com/kata/563f0c54a22b9345bf000053/train/typescript
 
-export function fcn(n: number): number {
-    // your code
-    return n;
-  }
+export const fcn = (num: number): number => Math.pow(2, num);
