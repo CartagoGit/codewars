82d721f7ace5ff8ab9ab17c15e119321bfc4d162
Cartago <cartago.relaxingcup@gmail.com>
2023-11-04 11:40:25 +0100

    Preparamos la kata de number of divisions

---
+++ src/typescript/exercises/7kyu/number-of-divisions.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/5913152be0b295cf99000001/train/typescript
+
+export function divisions(n: number, divisor: number): number {
+    throw new Error("This method or operations is not implemented.");
+  }
