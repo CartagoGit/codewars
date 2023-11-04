7f92e2ec8614d7dd28896cd94da63038fe8f1c89
Cartago <cartago.relaxingcup@gmail.com>
2023-11-04 11:45:16 +0100

    Resolcemos la kata de averiguar el numero de veces que se puede dividir un numero por un divisor

--- src/typescript/exercises/7kyu/number-of-divisions.ts
+++ src/typescript/exercises/7kyu/number-of-divisions.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5913152be0b295cf99000001/train/typescript
 
-export function divisions(n: number, divisor: number): number {
-    throw new Error("This method or operations is not implemented.");
-  }
+export const divisions = (n: number, divisor: number): number => {
+	let count = 0;
+	while (n >= divisor) {
+		n = Math.floor(n / divisor);
+		count++;
+	}
+	return count;
+};
