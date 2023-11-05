8611cafc5f59cb9a8e410e1db607e47895d65901
Cartago <cartago.relaxingcup@gmail.com>
2023-11-05 17:44:48 +0100

    Solucionamos la kata de generador de multiplicaciones

--- src/typescript/exercises/7kyu/multiplication-generator.ts
+++ src/typescript/exercises/7kyu/multiplication-generator.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5637ead70013386e30000027/train/typescript
 
-export function* generator(num: number): Generator<string> {
-  
+export function* generator(a: number): Generator<string> {
+    let b = 1;
+
+    while (true) {
+        yield `${a} x ${b} = ${a * b}`;
+        b++;
+    }
 }
