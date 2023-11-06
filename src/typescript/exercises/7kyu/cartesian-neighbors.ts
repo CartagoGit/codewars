35c209d8dd7800584bcf1722388232ac3a691ee9
Cartago <cartago.relaxingcup@gmail.com>
2023-11-06 01:29:33 +0100

    Damos solucion a la kata

--- src/typescript/exercises/7kyu/cartesian-neighbors.ts
+++ src/typescript/exercises/7kyu/cartesian-neighbors.ts
@@ -1,6 +1,12 @@
 //* https://www.codewars.com/kata/58989a079c70093f3e00008d/train/typescript
 
-
 export function cartesianNeighbor(x: number, y: number): number[][] {
-    return [];
-  }
+	let result: number[][] = [];
+	for (let neighborX = x - 1; neighborX <= x + 1; neighborX++) {
+		for (let neighborY = y - 1; neighborY <= y + 1; neighborY++) {
+			if (neighborX === x && neighborY === y) continue;
+			result.push([neighborX, neighborY]);
+		}
+	}
+	return result;
+}
