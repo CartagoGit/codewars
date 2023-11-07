59675602ad936956da25a16c3b1d45ffe09269e5
Cartago <cartago.relaxingcup@gmail.com>
2023-11-07 17:39:24 +0100

    Solucionamos la kata de no oddities

--- src/typescript/exercises/7kyu/no-oddities-here.ts
+++ src/typescript/exercises/7kyu/no-oddities-here.ts
@@ -1,5 +1,4 @@
 //* https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/typescript
 
-export function noOdds(values: number[]): number[] {
-	return [];
-}
+export const noOdds = (values: number[]): number[] =>
+	values.filter((value) => value % 2 === 0);
