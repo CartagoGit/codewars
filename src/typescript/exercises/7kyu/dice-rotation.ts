9eba04951f43264f6b41b6cc024ad2c2beed3d37
Cartago <cartago.relaxingcup@gmail.com>
2023-11-09 11:14:18 +0100

    Damos solucion a la kata

--- src/typescript/exercises/7kyu/dice-rotation.ts
+++ src/typescript/exercises/7kyu/dice-rotation.ts
@@ -1,5 +1,15 @@
 //*^https://www.codewars.com/kata/5ff2093d375dca00170057bc/train/typescript
 
+
 export function rotations(dieArray: number[]): number {
-	return 0;
+	const counter: number[] = [];
+	const uniqueValues = [...new Set(dieArray)];
+	for (let face of uniqueValues) {
+		let counterFace = 0;
+		for (let value of dieArray) {
+			if (face !== value) counterFace += face + value === 7 ? 2 : 1;
+		}
+		counter.push(counterFace);
+	}
+	return Math.min(...counter) ?? 0;
 }
