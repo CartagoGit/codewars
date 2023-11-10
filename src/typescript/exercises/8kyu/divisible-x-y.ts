d4d7430ddccb5d1d7362b209b6722e89d58c24bd
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-11-10 21:14:22 +0100

    Solucionamos la kata de divisible

--- src/typescript/exercises/8kyu/divisible-x-y.ts
+++ src/typescript/exercises/8kyu/divisible-x-y.ts
@@ -1,3 +1,4 @@
 //* https://www.codewars.com/kata/5545f109004975ea66000086/train/typescript
 
-export const isDivisible = (n: number, x: number, y: number): boolean => true;
+export const isDivisible = (n: number, x: number, y: number): boolean =>
+	n % x === 0 && n % y === 0;
