c86613fe41af381832da97ce0a01ad5929f2b52e
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-11-11 21:31:47 +0100

    Damos solucion a la kata de welcome

--- src/typescript/exercises/8kyu/welcome.ts
+++ src/typescript/exercises/8kyu/welcome.ts
@@ -1,5 +1,24 @@
 //* https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/typescript
 
-export function greet(language: string): string {
-	return 'Welcome';
-}
+const traduction: Record<string, string> = {
+	english: 'Welcome',
+	czech: 'Vitejte',
+	danish: 'Velkomst',
+	dutch: 'Welkom',
+	estonian: 'Tere tulemast',
+	finnish: 'Tervetuloa',
+	flemish: 'Welgekomen',
+	french: 'Bienvenue',
+	german: 'Willkommen',
+	irish: 'Failte',
+	italian: 'Benvenuto',
+	latvian: 'Gaidits',
+	lithuanian: 'Laukiamas',
+	polish: 'Witamy',
+	spanish: 'Bienvenido',
+	swedish: 'Valkommen',
+	welsh: 'Croeso',
+};
+
+export const greet = (language: string): string =>
+	traduction[language] ?? traduction['english'];
