1a3362d9f4f132023c5ed1077bffa810d670b512
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2023-11-12 12:26:21 +0100

    Solucionamos la kata de exclamation

--- src/typescript/exercises/8kyu/exclamation.ts
+++ src/typescript/exercises/8kyu/exclamation.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/57faf12b21c84b5ba30001b0/train/typescript
 
 export function remove(s: string): string {
-	throw new Error('The method or operation is not implemented.');
+	return (
+		s
+			.split('')
+			.filter((char) => char !== '!')
+			.join('') + '!'
+	);
 }
