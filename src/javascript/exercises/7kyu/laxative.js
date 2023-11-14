d9c945fe9945f1558db01829cbdf16f6bb50ed7e
Cartago <cartago.relaxingcup@gmail.com>
2023-11-14 18:11:11 +0100

    DAmos solucion a la kata

--- src/javascript/exercises/7kyu/laxative.js
+++ src/javascript/exercises/7kyu/laxative.js
@@ -1,28 +1,8 @@
 //* https://www.codewars.com/kata/5b02ae6aa2afd8f1b4001ba4/train/javascript
 
-/**
- * Get the probability of a laxative effect in a given number of shots.
- *
- * @param {number} shots - Total shots.
- * @param {number} lax - Shots with laxative effect.
- * @param {number} times - Number of times the shots are drunk.
- * @returns {number} Probability with two decimal places.
- */
 const getChance = (n, x, a) => {
-	console.log({ n, x, a });
-	if (x >= n || n <= a) return NaN;
-	const totalCombinations = combinations(n, a);
-	const noLaxativeCombinations = combinations(n - x, a);
-	const probability = noLaxativeCombinations / totalCombinations;
-	console.log({ totalCombinations, noLaxativeCombinations, probability });
-	return Math.round(probability * 100) / 100;
-	
-};
-
-const combinations = (n, r) => {
-	let result = 1;
-	for (let i = 1; i <= r; i++) {
-		result *= (n - r + i) / i;
-	}
-	return result;
-};
+	let possibility = 1;
+    for (let i = 0; i < a; i++) 
+        possibility *= (n - x) / n--;
+	return Math.round(possibility * 100) / 100;
+}
