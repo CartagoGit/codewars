071bfffe5be247e728874a31ab73b5a70e01e9e4
Cartago <cartago.relaxingcup@gmail.com>
2023-11-14 17:07:27 +0100

    Preparamos un prototipo para el ejercio

--- src/javascript/exercises/7kyu/laxative.js
+++ src/javascript/exercises/7kyu/laxative.js
@@ -1,5 +1,29 @@
 //* https://www.codewars.com/kata/5b02ae6aa2afd8f1b4001ba4/train/javascript
 
-function getChance(n, x, a){ 
-    return 0;
-  }
+/**
+ * Get probability of laxative effect in a given number of shots.
+ *
+ * @param {number} shots - Shots.
+ * @param {number} lax - Shoots with lax.
+ * @param {number} times -  Drunk times.
+ * @returns {number} Probability with two decimals.
+ */
+const getChance = (shoots, lax, times) => {
+	const probability =
+		calculateCombination(shoots - lax, times) /
+		calculateCombination(shoots, times);
+	return Math.round(probability * 100) / 100;
+};
+
+/**
+ * Calculates the number of possible combinations.
+ *
+ * @param {number} shoots - Total number of elements.
+ * @param {number} times - Number of elements to select in each combination.
+ * @returns {number} - Number of possible combinations.
+ */
+const calculateCombination = (shoots, times) => {
+	const factorial = (shoots) =>
+		shoots > 1 ? shoots * factorial(shoots - 1) : 1;
+	return factorial(times) / (factorial(times) * factorial(shoots - times));
+};
