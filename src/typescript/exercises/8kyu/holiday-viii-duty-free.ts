f8914e845a1b47e4c50cadeaed2a8c9dc9ecb0e2
Cartago <cartago.relaxingcup@gmail.com>
2023-11-15 23:01:42 +0100

    Preparamos la kata de duty free

---
+++ src/typescript/exercises/8kyu/holiday-viii-duty-free.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/57e92e91b63b6cbac20001e5/train/typescript
+
+export function dutyFree(
+	normPrice: number,
+	discount: number,
+	hol: number
+): number {
+	return 0;
+}
