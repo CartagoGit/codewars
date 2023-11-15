9f555f55a8afd6a1dfc530b073a493adfd56ce82
Cartago <cartago.relaxingcup@gmail.com>
2023-11-15 23:07:08 +0100

    Solucionamos la kata de dutyfree

--- src/typescript/exercises/8kyu/holiday-viii-duty-free.ts
+++ src/typescript/exercises/8kyu/holiday-viii-duty-free.ts
@@ -1,9 +1,7 @@
 //* https://www.codewars.com/kata/57e92e91b63b6cbac20001e5/train/typescript
 
-export function dutyFree(
+export const dutyFree = (
 	normPrice: number,
 	discount: number,
 	hol: number
-): number {
-	return 0;
-}
+): number => Math.floor(hol / (normPrice * (discount / 100)));
