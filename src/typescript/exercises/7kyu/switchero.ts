5ce3133b73cdaf541edc4471d4089a8f85fef892
Cartago <cartago.relaxingcup@gmail.com>
2023-11-16 08:36:27 +0100

    Solucionamos la kata de switchero

--- src/typescript/exercises/7kyu/switchero.ts
+++ src/typescript/exercises/7kyu/switchero.ts
@@ -1,5 +1,4 @@
 //* https://www.codewars.com/kata/57f759bb664021a30300007d/train/typescript
 
-export function switcheroo(x: string): string {
-    return x;
-  }
+export const switcheroo = (text: string): string =>
+	text.split('').map((char) => (char === 'a' ? 'b' : char === 'b' ? 'a' : char)).join('');
