810ca8d5887e6587609886afe027f898c0906048
Cartago <cartago.relaxingcup@gmail.com>
2023-11-18 11:28:45 +0100

    Solucionamos la kata de salutes

--- src/typescript/exercises/7kyu/count-salutes.ts
+++ src/typescript/exercises/7kyu/count-salutes.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/605ae9e1d2be8a0023b494ed/train/typescript
 
 export function countSalutes(hallway: String): number {
-	return 0;
+	const chars = hallway.split('');
+	let counter = 0;
+	for (let i = 0; i < chars.length; i++) {
+		if (['-', '<'].includes(chars[i])) continue;
+		const nextOposite = chars.filter(
+			(char, index) => index > i && char === '<'
+		).length;
+		counter += nextOposite * 2;
+	}
+	return counter;
 }
