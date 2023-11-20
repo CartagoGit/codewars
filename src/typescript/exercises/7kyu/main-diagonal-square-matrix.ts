e709284c958fa20a90be6c688ca9706ce8312a69
Cartago <cartago.relaxingcup@gmail.com>
2023-11-20 21:05:42 +0100

    Damos solucion a la kata de main diagonal

--- src/typescript/exercises/7kyu/main-diagonal-square-matrix.ts
+++ src/typescript/exercises/7kyu/main-diagonal-square-matrix.ts
@@ -1,4 +1,4 @@
-export function mainDiagonalProduct(mat: number[][]): number {
-    
-	return 0;
-}
+//* https://www.codewars.com/kata/551204b7509063d9ba000b45/train/typescript
+
+export const mainDiagonalProduct = (mat: number[][]): number =>
+	mat.reduce((acc, row, index) => acc * row[index], 1);
