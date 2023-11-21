74fe31f6717440f085155ad276944358e6b9019a
Cartago <cartago.relaxingcup@gmail.com>
2023-11-21 16:51:36 +0100

    Preparamos la kata how good are you?

---
+++ src/typescript/exercises/8kyu/how-good-are-you-really.ts
@@ -0,0 +1,8 @@
+//* https://www.codewars.com/kata/5601409514fc93442500010b/train/typescript
+
+export function betterThanAverage(
+	classPoints: number[],
+	yourPoints: number
+): boolean {
+	return true;
+}
