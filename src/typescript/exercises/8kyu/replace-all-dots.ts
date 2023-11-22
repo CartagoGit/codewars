3b3bc45f4816a52e34bc9bf6926e0ed6bc378357
Cartago <cartago.relaxingcup@gmail.com>
2023-11-22 20:46:09 +0100

    Resolvemos la kata de replace all dots

--- src/typescript/exercises/8kyu/replace-all-dots.ts
+++ src/typescript/exercises/8kyu/replace-all-dots.ts
@@ -1,5 +1,3 @@
 //* https://www.codewars.com/kata/596c6eb85b0f515834000049/train/typescript
 
-export function replaceDots(str: string): string {
-    return str.replace(/./g, '-');
-  }
+export const replaceDots = (str: string): string => str.replace(/[.]/g, '-');
