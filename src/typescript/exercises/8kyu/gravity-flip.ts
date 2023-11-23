a8820c3467937da0295ef0498e2e6f7242d4aea0
Cartago <cartago.relaxingcup@gmail.com>
2023-11-23 08:39:41 +0100

    Solucionamos la kata de gravity flip

--- src/typescript/exercises/8kyu/gravity-flip.ts
+++ src/typescript/exercises/8kyu/gravity-flip.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5f70c883e10f9e0001c89673/train/typescript
 
 export function flip(dir: string, arr: number[]): number[] {
-    return [];
-  }
+	return [...arr].sort((a, b) => (dir === 'R' ? a - b : b - a));
+}
