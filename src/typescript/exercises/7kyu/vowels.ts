dfea3a53a7836e0906b21ea3dbacc7f909953094
Cartago <cartago.relaxingcup@gmail.com>
2023-11-24 07:22:44 +0100

    Solucionamos la katad e laos indices de las vocales

--- src/typescript/exercises/7kyu/vowels.ts
+++ src/typescript/exercises/7kyu/vowels.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/5680781b6b7c2be860000036/train/typescript
 
 export function vowelIndices(word: string): number[] {
-    return []
-  }
+	const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+	const result: number[] = [];
+
+	for (let i = 0; i < word.length; i++) {
+		if (vowels.includes(word.toLowerCase()[i])) {
+			result.push(i + 1);
+		}
+	}
+
+	return result;
+}
