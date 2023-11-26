7fe1e87d8f3d5b36a8d5499df7c7072427391c6f
Cartago <cartago.relaxingcup@gmail.com>
2023-11-26 18:12:42 +0100

    Solucionamos la kata de Personalized Message

--- src/typescript/exercises/8kyu/personalized-message.ts
+++ src/typescript/exercises/8kyu/personalized-message.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5772da22b89313a4d50012f7/train/typescript
 
 export function greet(name: string, owner: string): string {
-	return '';
+	return `Hello ${name === owner ? 'boss' : 'guest'}`
 }
