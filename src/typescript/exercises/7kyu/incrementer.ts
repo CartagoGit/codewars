7897a3fd681d6d5d89a9ec5c6f8511c4625d1e22
Cartago <cartago.relaxingcup@gmail.com>
2023-11-27 20:48:07 +0100

    Solucionamos la kata de incrementer

--- src/typescript/exercises/7kyu/incrementer.ts
+++ src/typescript/exercises/7kyu/incrementer.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/590e03aef55cab099a0002e8/train/typescript
 
-export function incrementer(nums: number[]): number {
-	return 0;
+export function incrementer(nums: number[]): number[] {
+	return nums.map((num, index) => (num + index + 1) % 10);
 }
