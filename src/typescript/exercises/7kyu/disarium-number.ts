01edd0b9a0ec809d08328b46757ae608d88581d7
Cartago <cartago.relaxingcup@gmail.com>
2023-11-29 18:11:54 +0100

    Solucionamos la kata de disarium number

--- src/typescript/exercises/7kyu/disarium-number.ts
+++ src/typescript/exercises/7kyu/disarium-number.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/5a53a17bfd56cb9c14000003/train/typescript
 
 export function disariumNumber(n: number) {
-	// your code here
+	return `${n}`
+		.split('')
+		.reduce((acc, cur, i) => acc + Math.pow(Number(cur), i + 1), 0) === n
+		? 'Disarium !!'
+		: 'Not !!';
 }
