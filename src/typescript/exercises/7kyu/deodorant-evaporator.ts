b96bc1ef1038e068c4b55464a4042aaf01b11b72
Cartago <cartago.relaxingcup@gmail.com>
2023-11-30 20:17:13 +0100

    Preparamos el ejercicio de deodorant evaporator

---
+++ src/typescript/exercises/7kyu/deodorant-evaporator.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/5506b230a11c0aeab3000c1f/train/typescript
+
+export function evaporator(
+	content: number,
+	evapPerDay: number,
+	threshold: number
+): number {
+	return 0;
+}
