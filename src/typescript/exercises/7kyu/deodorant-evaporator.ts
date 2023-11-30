06b890fb9524078b1a52f974c344b04b8b7b2ab4
Cartago <cartago.relaxingcup@gmail.com>
2023-11-30 20:27:16 +0100

    Modificamos las variables de la kata

--- src/typescript/exercises/7kyu/deodorant-evaporator.ts
+++ src/typescript/exercises/7kyu/deodorant-evaporator.ts
@@ -1,14 +1,14 @@
 //* https://www.codewars.com/kata/5506b230a11c0aeab3000c1f/train/typescript
 
 export function evaporator(
-	content: number,
+	_content: number,
 	evapPerDay: number,
 	threshold: number
 ): number {
 	let days = 0;
-	let percentageContent = 100;
-	while (percentageContent > threshold) {
-		percentageContent -= percentageContent * (evapPerDay / 100);
+	let percentContent = 100;
+	while (percentContent > threshold) {
+		percentContent -= percentContent * (evapPerDay / 100);
 		days++;
 	}
 	return days;
