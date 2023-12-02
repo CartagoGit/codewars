fd0e43f8648638ab39f21844bbcc14d443f07815
Cartago <cartago.relaxingcup@gmail.com>
2023-12-02 11:35:49 +0100

    Preparamos el ejercicio de squared strings

---
+++ src/typescript/exercises/7kyu/squared-strings.ts
@@ -0,0 +1,11 @@
+//* https://www.codewars.com/kata/56dbe0e313c2f63be4000b25/train/typescript
+
+export function vertMirror(strng: string) {
+	// your code
+}
+export function horMirror(strng: string) {
+	// your code
+}
+export function oper(fct: (s: string) => string, s: string) {
+	// your code
+}
