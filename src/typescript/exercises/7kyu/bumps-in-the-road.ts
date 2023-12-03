c27b5add70f9ded3b30b820925739a865341e2da
Cartago <cartago.relaxingcup@gmail.com>
2023-12-04 00:04:39 +0100

    Solucionamos la kata de bumps in the road

--- src/typescript/exercises/7kyu/bumps-in-the-road.ts
+++ src/typescript/exercises/7kyu/bumps-in-the-road.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/57ed30dde7728215300005fa/train/typescript
 
-export function bump(x: string): string{
-  return ""
+export function bump(x: string): string {
+	return x.split('').filter((char) => char === 'n').length > 15
+		? 'Car Dead'
+		: 'Woohoo!';
 }
