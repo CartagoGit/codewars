cbfd0346b16da0a0491f75df690697ab3267dfdc
Cartago <cartago.relaxingcup@gmail.com>
2023-12-05 21:29:28 +0100

    Solucionamos la kata de chat code calculation

--- src/typescript/exercises/7kyu/char-code-calculation.ts
+++ src/typescript/exercises/7kyu/char-code-calculation.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/57f75cc397d62fc93d000059/train/typescript
 
 export const calc = (str: string): number => {
-	return 0;
+	const total1 = str
+		.split('')
+		.map((char) => char.charCodeAt(0))
+		.join('');
+	const total2 = total1.replace(/7/g, '1');
+	return (
+		total1.split('').reduce((acc, curr) => acc + Number(curr), 0) -
+		total2.split('').reduce((acc, curr) => acc + Number(curr), 0)
+	);
 };
