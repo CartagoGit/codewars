e22875b1b7579a7efc598e0abbb8f93ca8b3c342
Cartago <cartago.relaxingcup@gmail.com>
2023-12-16 03:37:18 +0100

    Preparamos el ejercicio numero del dia 3

---
+++ src/typescript/advent-of-code/day03/exercise.ts
@@ -0,0 +1,9 @@
+import { INPUT_DAY_03 } from "./input";
+
+export const initDay03 = (): number => {
+	return gearRatios(INPUT_DAY_03);
+};
+
+const gearRatios = (input: string) : number => {
+    return 0
+}
