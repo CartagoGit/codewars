2210f6c2777cdd489865ab8cb525d833af891fb3
Cartago <cartago.relaxingcup@gmail.com>
2023-12-16 04:10:10 +0100

    Damos una solucion al ejercicio del dia 3

--- src/typescript/advent-of-code/day03/exercise.ts
+++ src/typescript/advent-of-code/day03/exercise.ts
@@ -9,38 +9,50 @@ const gearRatios = (input: string): number => {
 		.split('\n')
 		.map((line) => line.trim().split(''));
 	const patternNumber = /^\d$/;
+	let partNumbers: number[] = [];
 	for (let row = 0; row < matrix.length; row++) {
 		for (let cell = 0; cell < matrix[row].length; cell++) {
 			let chain = matrix[row][cell];
-			if (!chain.match(patternNumber)) continue;
+			if (!patternNumber.test(chain)) continue;
 			let checkCell = cell + 1;
 			while (
 				checkCell < matrix[row].length &&
-				!matrix[row][checkCell].match(patternNumber)
+				matrix[row][checkCell].match(patternNumber)
 			) {
 				chain += matrix[row][checkCell];
 				checkCell++;
 			}
-			if (hasAdjacentSymbol({ matrix, row, initCell: cell, lastCell: checkCell - 1 })) {
-                
+			if (
+				hasAdjacentSymbol({
+					matrix,
+					numberRow: row,
+					initCell: cell,
+					lastCell: checkCell - 1,
+				})
+			) {
+				partNumbers.push(Number(chain));
 			}
-            cell = checkCell;
+			cell = checkCell;
 		}
 	}
-	return 0;
+	return partNumbers.reduce((acc, curr) => acc + curr, 0);
 };
 
 const hasAdjacentSymbol = (data: {
 	matrix: string[][];
-	row: number;
+	numberRow: number;
 	initCell: number;
 	lastCell: number;
 }): boolean => {
-	const patternSymbol = /[^\w\s\D.]/;
-	const isSymbol = (char: string) => char.match(patternSymbol);
-    const { matrix, row, initCell, lastCell } = data;
-
-    
-
-	return true;
+	const patternSymbol = /[^\w\s\d.]/;
+	const isSymbol = (char: string) => patternSymbol.test(char);
+	const { matrix, numberRow, initCell, lastCell } = data;
+	for (let row = numberRow - 1; row <= numberRow + 1; row++) {
+		if (row < 0 || row >= matrix.length) continue;
+		for (let cell = initCell - 1; cell <= lastCell + 1; cell++) {
+			if (cell < 0 || cell >= matrix[row].length) continue;
+			if (isSymbol(matrix[row][cell])) return true;
+		}
+	}
+	return false;
 };
