704d17f0d048b5a8c28152225734457d9bd95364
Cartago <cartago.relaxingcup@gmail.com>
2023-12-18 00:59:54 +0100

    Nos acercamos a la posible solucion al problema

--- src/typescript/advent-of-code/day17/excercise.ts
+++ src/typescript/advent-of-code/day17/excercise.ts
@@ -1,24 +1,29 @@
 import { INPUT_DAY_17, INPUT_EXAMPLE_17 } from './input';
 
-//!! INIT
-export const initDay17 = (): number => {
-	// return getClumsyCrucible(INPUT_DAY_17);
-	return getClumsyCrucible(INPUT_EXAMPLE_17);
-};
-
 interface Position {
 	x: number;
 	y: number;
 }
 
-type Vertex = Position & {
-	lostHeat: number;
-	prev?: Vertex;
-	timesSameDirection: number;
+const directionsArray = ['up', 'down', 'left', 'right', 'none'] as const;
+type Direction = (typeof directionsArray)[number];
+
+type Vertex = {
+	x: number;
+	y: number;
+	lastSide: Direction;
+	steps: number;
+	heat: number;
+	lowestHeat: number;
+	visited: boolean[][];
+	heatMap: number[][];
 };
 
-const directions = ['up', 'down', 'left', 'right', 'none'] as const;
-type Direction = (typeof directions)[number];
+//!! INIT
+export const initDay17 = (): number => {
+	return getClumsyCrucible(INPUT_DAY_17);
+	// return getClumsyCrucible(INPUT_EXAMPLE_17);
+};
 
 let lavaMap: number[][] = [];
 
@@ -35,154 +40,126 @@ const getClumsyCrucible = (input: string): number => {
 		y: lavaMap.length - 1,
 	};
 
-	const path = aStar({ initialPosition, endPosition });
-	const result = getLostHeat({ path });
+	const result = calculateHeat({ start: initialPosition, end: endPosition });
+
 	return result;
 };
 
-const dijkstra = (data: {
-	initialPosition: Position;
-	endPosition: Position;
-}) => {
-	const { initialPosition, endPosition } = data;
+const calculateHeat = (data: { start: Position; end: Position }) => {
+	const { start, end } = data;
+	const visited = new Array(lavaMap.length)
+		.fill(false)
+		.map(() => new Array(lavaMap[0].length).fill(false));
+	const heatMap = new Array(lavaMap.length)
+		.fill(false)
+		.map(() => new Array(lavaMap[0].length).fill(-1));
+	visited[start.y][start.x] = true;
+	heatMap[start.y][start.x] = lavaMap[start.y][start.x];
+	const initVertex: Vertex = {
+		...start,
+		steps: 0,
+		lastSide: 'none',
+		heat: lavaMap[start.y][start.x],
+		lowestHeat: lavaMap[start.y][start.x],
+		heatMap,
+		visited,
+	};
+	let possibilities: Vertex[] = [];
+
+	getPath({ end, current: initVertex, possibilities });
+	return Math.min(
+		...possibilities.map((possibility) => possibility.lowestHeat - possibility.heat)
+	);
 };
 
-const aStar = (data: {
-	initialPosition: Position;
-	endPosition: Position;
-}): Vertex[] => {
-	const { initialPosition, endPosition } = data;
-	let openList: Vertex[] = [];
-	let closedList: Vertex[] = [];
-	openList.push({
-		x: initialPosition.x,
-		y: initialPosition.y,
-		lostHeat: 0,
-		timesSameDirection: 0,
-	});
-
-	while (openList.length > 0) {
-		openList.sort((a, b) => a.lostHeat - b.lostHeat);
-		// openList.sort((a, b) => b.lostHeat - a.lostHeat);
-		let current = openList.shift() as Vertex;
-		closedList.push(current);
-
-		if (current.x === endPosition.x && current.y === endPosition.y) {
-			let path = [];
-			while (current.prev) {
-				path.unshift(current);
-				current = current.prev;
-			}
-			return path;
-		}
-
-		let neighbors = getNeighbors(current);
-		for (let neighbor of neighbors) {
-			if (
-				!isInLavaMap({ vertex: neighbor, lavaMap }) ||
-				lavaMap[neighbor.y][neighbor.x] === 1 ||
-				isInList({ vertex: neighbor, list: closedList })
-			) {
-				continue;
-			}
-			neighbor.lostHeat =
-				current.lostHeat +
-				1 +
-				heuristic({ vertex: neighbor, end: endPosition });
-			neighbor.prev = current;
-
-			if (!isInList({ vertex: neighbor, list: openList })) {
-				openList.push(neighbor);
-			}
-		}
-	}
-	return [];
-};
-
-const isInList = (data: { vertex: Vertex; list: Vertex[] }): boolean => {
-	const { vertex, list } = data;
-	for (let element of list) {
-		if (element.x === vertex.x && element.y === vertex.y) {
-			return true;
-		}
-	}
-	return false;
-};
-
-const getLastDirection = (data: {
-	prev: Vertex | undefined;
+const getPath = (data: {
+	end: Position;
 	current: Vertex;
-}): Direction => {
-	const { prev, current } = data;
-	if (!prev) return 'none';
-	if (current.x === prev.x) {
-		return current.y > prev.y ? 'down' : 'up';
-	} else {
-		return current.x > prev.x ? 'right' : 'left';
+	possibilities: Vertex[];
+}): void => {
+	const { end, current, possibilities } = data;
+	const neighbors = getNeighbors(current);
+	for (const neighbor of neighbors) {
+		if (neighbor.x === end.x && neighbor.y === end.y) {
+			possibilities.push(neighbor);
+			continue;
+		}
+		getPath({ end, current: neighbor, possibilities });
 	}
 };
 
-const isInLavaMap = (data: {
-	vertex: Vertex;
-	lavaMap: number[][];
-}): boolean => {
-	const { vertex, lavaMap } = data;
-	if (
-		vertex.x < 0 ||
-		vertex.x >= lavaMap[0].length ||
-		vertex.y < 0 ||
-		vertex.y >= lavaMap.length
-	) {
-		return false;
-	}
-	return true;
-};
+const getNeighbors = (current: Vertex): Vertex[] => {
+	let neighbors: Vertex[] = [];
+	const inverseDirections: Omit<Record<Direction, Direction>, 'none'> = {
+		up: 'down',
+		down: 'up',
+		left: 'right',
+		right: 'left',
+	};
+	for (const direction of directionsArray) {
+		if (
+			direction === 'none' ||
+			(current.lastSide !== 'none' &&
+				inverseDirections[current.lastSide] === direction)
+		) {
+			continue;
+		}
 
-const heuristic = (data: { vertex: Vertex; end: Position }): number => {
-	const { vertex, end } = data;
-	let dx = Math.abs(vertex.x - end.x);
-	let dy = Math.abs(vertex.y - end.y);
-	return dx + dy;
-};
+		const newPosition = getNewPosition({ current, direction });
+		if (!isValidDirection({ newPosition, current })) continue;
+		let visitedNeighbor = current.visited.map((row) => [...row]);
+		visitedNeighbor[newPosition.y][newPosition.x] = true;
 
-const getNeighbors = (vertex: Vertex): Vertex[] => {
-	const lastDirection = getLastDirection({
-		prev: vertex.prev,
-		current: vertex,
-	});
-	const neighbors = [];
-	for (let direction of directions) {
-		if (direction === 'none') continue;
-		let { timesSameDirection } = vertex;
-		console.log({ direction, lastDirection, timesSameDirection });
-		if (direction === lastDirection) timesSameDirection++;
-		else timesSameDirection = 0;
-		if (timesSameDirection > 3) continue;
+		current.heatMap[newPosition.y][newPosition.x] =
+			current.lowestHeat + lavaMap[newPosition.y][newPosition.x];
 		const newNeighbor = {
-			// lostHeat: getLostHeat({ path: [vertex] }),
-			lostHeat: lavaMap[vertex.y][vertex.x],
-			timesSameDirection,
-			prev: vertex,
+			...newPosition,
+			lastSide: direction,
+			steps: current.lastSide === direction ? current.steps + 1 : 1,
+			heat: lavaMap[newPosition.y][newPosition.x],
+			lowestHeat:
+				current.lowestHeat + lavaMap[newPosition.y][newPosition.x],
+			visited: visitedNeighbor,
+			heatMap: current.heatMap,
 		};
-		const newPosition: Omit<Record<Direction, Position>, 'none'> = {
-			up: { x: vertex.x, y: vertex.y - 1 },
-			down: { x: vertex.x, y: vertex.y + 1 },
-			left: { x: vertex.x - 1, y: vertex.y },
-			right: { x: vertex.x + 1, y: vertex.y },
-		};
-
-		neighbors.push({ ...newNeighbor, ...newPosition[direction] });
+		if (newNeighbor.steps > 3) continue;
+		neighbors.push(newNeighbor);
 	}
 	return neighbors;
 };
 
-const getLostHeat = (data: { path: Vertex[] }): number => {
-	const { path } = data;
-	let lostHeat = 0;
-	for (let vertex of path) {
-		lostHeat += lavaMap[vertex.y][vertex.x];
-		console.log(getLastDirection({ prev: vertex.prev, current: vertex }));
-	}
-	lostHeat += lavaMap[path[path.length - 1].y][path[path.length - 1].x];
-	return lostHeat;
+const getNewPosition = (data: {
+	current: Vertex;
+	direction: Direction;
+}): Position => {
+	const { current, direction } = data;
+	if (direction === 'none') throw 'Direction is none';
+	const newPosition: Omit<{ [key in Direction]: Position }, 'none'> = {
+		up: { x: 0, y: -1 },
+		down: { x: 0, y: 1 },
+		left: { x: -1, y: 0 },
+		right: { x: 1, y: 0 },
+	};
+	return {
+		x: current.x + newPosition[direction].x,
+		y: current.y + newPosition[direction].y,
+	};
+};
+
+const isValidDirection = (data: {
+	newPosition: Position;
+	current: Vertex;
+}): boolean => {
+	const { newPosition, current } = data;
+
+	return (
+		newPosition.x >= 0 &&
+		newPosition.x < lavaMap[0].length &&
+		newPosition.y >= 0 &&
+		newPosition.y < lavaMap.length &&
+		current.visited[newPosition.y][newPosition.x] === false &&
+		(current.heatMap[newPosition.y][newPosition.x] === -1 ||
+			current.heatMap[newPosition.y][newPosition.x] >
+				current.lowestHeat + lavaMap[newPosition.y][newPosition.x])
+	);
 };
