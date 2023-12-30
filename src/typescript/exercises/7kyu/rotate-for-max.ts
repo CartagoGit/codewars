acf9e6c183e26ed7434a9b2ce2670918a95a2d5f
Cartago <cartago.relaxingcup@gmail.com>
2023-12-30 12:42:40 +0100

    Damos solucion a la  kata de max rotation

--- src/typescript/exercises/7kyu/rotate-for-max.ts
+++ src/typescript/exercises/7kyu/rotate-for-max.ts
@@ -1,6 +1,14 @@
 // https://www.codewars.com/kata/56a4872cbb65f3a610000026/train/typescript
 
 export function maxRot(n: number): number {
-	// your code
-	return -1;
+	let chain: string = n.toString();
+	let results: number[] = [n];
+	for (let i = 0; i < chain.length; i++) {
+		const stable = chain.slice(0, i);
+		const rest = chain.slice(i);
+		chain = stable + rest.slice(1) + rest[0];
+
+		results.push(Number(chain));
+	}
+	return Math.max(...results);
 }
