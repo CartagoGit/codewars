5fab583f62026f8f0a978c090e27a5d87f8dbf30
Cartago <cartago.relaxingcup@gmail.com>
2023-12-31 21:18:24 +0100

    Resolvemos la kata de build square

--- src/typescript/exercises/7kyu/build-square.ts
+++ src/typescript/exercises/7kyu/build-square.ts
@@ -1,5 +1,10 @@
 // https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/typescript
 
 export function generateShape(int: number): string {
-	return '+'; // your code here
+	let chain = '';
+	for (let i = 0; i < int; i++) {
+		chain += '+'.repeat(int);
+		if (i < int - 1) chain += '\n';
+	}
+	return chain;
 }
