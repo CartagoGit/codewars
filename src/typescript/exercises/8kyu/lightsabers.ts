09bab5a5226faa38f45813b5fba807dd6ea6527f
Cartago <cartago.relaxingcup@gmail.com>
2024-01-01 07:46:41 +0100

    Preparamos el ejercico de sables espada

---
+++ src/typescript/exercises/8kyu/lightsabers.ts
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/51f9d93b4095e0a7200001b8/train/typescript
+
+export function howManyLightsabersDoYouOwn(name?: any): number {
+    // So, how many?
+    return 0;
+}
+
+// The question mark (?) after name attribute marks it as optional.
