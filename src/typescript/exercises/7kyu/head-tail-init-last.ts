144ac3604d401f4e04d0de57604b1de70c4a0425
Cartago <cartago.relaxingcup@gmail.com>
2024-01-02 02:35:41 +0100

    Terminamos la kata

--- src/typescript/exercises/7kyu/head-tail-init-last.ts
+++ src/typescript/exercises/7kyu/head-tail-init-last.ts
@@ -1,5 +1,17 @@
 // https://www.codewars.com/kata/54592a5052756d5c5d0009c3/train/typescript
 
-export const head = () => {
-	throw new Error('Todo, as well as the other functions...');
+export const head = (arr: number[]) => {
+	return arr[0];
+};
+
+export const tail = (arr: number[]) => {
+	return arr.slice(1);
+};
+
+export const init = (arr: number[]) => {
+	return arr.slice(0, -1);
+};
+
+export const last = (arr: number[]) => {
+	return arr[arr.length - 1];
 };
