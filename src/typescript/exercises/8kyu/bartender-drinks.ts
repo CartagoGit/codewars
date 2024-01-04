afac1011fec7e53d8ef1066d45e14a94d839119d
Cartago <cartago.relaxingcup@gmail.com>
2024-01-04 08:50:53 +0100

    Damos solucion a la kata

--- src/typescript/exercises/8kyu/bartender-drinks.ts
+++ src/typescript/exercises/8kyu/bartender-drinks.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/568dc014440f03b13900001d/train/typescript
 
 export function getDrinkByProfession(profession: string): string {
-	return '';
+	const response: Record<string, string> = {
+		jabroni: 'Patron Tequila',
+		'school counselor': 'Anything with Alcohol',
+		programmer: 'Hipster Craft Beer',
+		'bike gang member': 'Moonshine',
+		politician: 'Your tax dollars',
+		rapper: 'Cristal',
+		default: 'Beer',
+	};
+	return response[profession.toLowerCase()] || response.default;
 }
