fd870725910a115cc9babb9a7b789c10a50dabb1
Cartago <cartago.relaxingcup@gmail.com>
2024-01-07 20:12:37 +0100

    Preparamos el ejercicio de average scores

---
+++ src/typescript/exercises/7kyu/average-scores.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/57b68bc7b69bfc8209000307/train/typescript
+
+export function average(scores:number[]):number {
+    // code for calculating average
+    return 0; 
+}
