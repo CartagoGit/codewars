8a866d16bc9d5b3287bc0cc2704dfea1dc0a2df0
Cartago <cartago.relaxingcup@gmail.com>
2024-01-07 20:24:51 +0100

    Preparamos el ejercicio de seleccion de caracteres del street fighter 2

---
+++ src/typescript/exercises/6kyu/street-fighter-2-character-selection.ts
@@ -0,0 +1,8 @@
+//* https://www.codewars.com/kata/5853213063adbd1b9b0000be/train/typescript
+
+type Move = 'down'|'up'|'right'|'left'
+
+export function streetFighterSelection(fighters: string[][], position: number[], moves: Move[]) {
+  // Your code here!
+}
+
