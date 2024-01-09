4d1cd0765c2fd99ebdd0f80357aba5d97d1487e9
Cartago <cartago.relaxingcup@gmail.com>
2024-01-09 18:44:44 +0100

    Solucionamos la kata de unlucky days

--- src/typescript/exercises/7kyu/unlucky-days.ts
+++ src/typescript/exercises/7kyu/unlucky-days.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/56eb0be52caf798c630013c0/train/typescript
 
 export function unluckyDays(year: number): number {
-    return 0;
+    let unluckyDays = 0;
+    for (let month = 0; month < 12; month++) {
+        const date = new Date(year, month, 13);
+        if (date.getDay() === 5) unluckyDays++;
+    }
+    return unluckyDays;
 }
