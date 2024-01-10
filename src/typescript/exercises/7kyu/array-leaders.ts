5d63566598561426a0651c264257d042bbf76056
Cartago <cartago.relaxingcup@gmail.com>
2024-01-10 19:20:11 +0100

    Encontramos otra solucion al ejercicio

--- src/typescript/exercises/7kyu/array-leaders.ts
+++ src/typescript/exercises/7kyu/array-leaders.ts
@@ -1,11 +1,13 @@
 //* https://www.codewars.com/kata/5a651865fd56cb55760000e0/train/typescript
 
 export function arrayLeaders(numbers: number[]): number[] {
-	let result: number[] = [];
 	let sumRightNums = 0;
-	for (let i = numbers.length - 1; i >= 0; i--) {	
-		if (numbers[i] > sumRightNums) result.unshift(numbers[i]);
-        sumRightNums += numbers[i];
-	}
-	return result;
+	return numbers
+		.reverse()
+		.filter((num) => {
+			const isLeader = num > sumRightNums;
+			sumRightNums += num;
+			return isLeader;
+		})
+		.reverse();
 }
