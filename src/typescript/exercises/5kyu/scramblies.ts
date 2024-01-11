63356ef43e5eefa7c8d458f82b7139e5799caedd
Cartago <cartago.relaxingcup@gmail.com>
2024-01-11 21:58:49 +0100

    Solucionamos la kata de scramble

--- src/typescript/exercises/5kyu/scramblies.ts
+++ src/typescript/exercises/5kyu/scramblies.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/typescript
 
 export function scramble(str1: string, str2: string): boolean {
-    return false;
-  }
+	const charTimes: { [char: string]: number } = {};
+	for (let char of str1) {
+		charTimes[char] = (charTimes[char] || 0) + 1;
+	}
+	for (let char of str2) {
+		if (!charTimes[char]) return false;
+		charTimes[char]--;
+	}
+	return true;
+}
