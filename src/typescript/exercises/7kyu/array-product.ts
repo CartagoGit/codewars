9f6259ec514eada1a97616e4bf0689996d40aee2
Cartago <cartago.relaxingcup@gmail.com>
2024-01-11 16:50:00 +0100

    Damos una solucion alternativa

--- src/typescript/exercises/7kyu/array-product.ts
+++ src/typescript/exercises/7kyu/array-product.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/5a905c2157c562994900009d/train/typescript
 
-export function productArray(nums: number[]): number[] {
-	return [];
-}
+export const productArray = (nums: number[]): number[] => {
+	const arrayProd = nums.reduce((acc, curr) => acc * curr);
+	return nums.map((num) => arrayProd / num);
+};
