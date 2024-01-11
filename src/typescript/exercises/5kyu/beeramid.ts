f484da6c8209b5ecaa64649b4b75af65ce55ff81
Cartago <cartago.relaxingcup@gmail.com>
2024-01-11 23:25:55 +0100

    Solucionamos la kata de beeramid

--- src/typescript/exercises/5kyu/beeramid.ts
+++ src/typescript/exercises/5kyu/beeramid.ts
@@ -1,6 +1,12 @@
 //* https://www.codewars.com/kata/51e04f6b544cf3f6550000c1/train/typescript
 
 export function beeramid(bonus: number, price: number): number {
-  return 0
+	let beers = Math.floor(bonus / price);
+	let level = 0;
+	while (true) {
+		if (beers < Math.pow(level + 1, 2)) break;
+		beers -= Math.pow(level + 1, 2);
+		level++;
+	}
+	return level;
 }
-
