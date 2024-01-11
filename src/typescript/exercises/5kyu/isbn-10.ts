956cb1a99c9081b60dba74f95b8551e052ec9c6f
Cartago <cartago.relaxingcup@gmail.com>
2024-01-12 00:09:31 +0100

    Solucionamos la kata de isbn-10

--- src/typescript/exercises/5kyu/isbn-10.ts
+++ src/typescript/exercises/5kyu/isbn-10.ts
@@ -1,7 +1,20 @@
 //* https://www.codewars.com/kata/51fc12de24a9d8cb0e000001/train/typescript
 
-export function validISBN10(isbn: string): boolean {
-    // TODO: return true if (and only if) isbn is a valid 10-digit ISBN.
-    return true;
-}
+//  (arr[0]*1 + arr[1]*2 + arr[2]*3 + arr[3]*4 + arr[4]*5 + arr[5]*6 + arr[6]*7 + arr[7]*8 + arr[8]*9 + arr[9]*10 +) % 11 === 0
 
+export function validISBN10(isbn: string): boolean {
+	if (
+		isbn.length !== 10 || // ISBN must be 10 digits long
+		isNaN(Number(isbn.slice(0, isbn.length - 1))) || // first 9 digits must be numbers
+		(isbn[isbn.length - 1].toLowerCase() !== 'x' &&
+			isNaN(Number(isbn[isbn.length - 1]))) // last digit must be a number or 'X'
+	) {
+		return false;
+	}
+	const isbnArr = isbn
+		.split('')
+		.map((char, i) => (char.toLowerCase() === 'x' ? 10 : parseInt(char, 10)));
+	const result =
+		isbnArr.reduce((acc, curr, i) => acc + curr * (i + 1), 0) % 11;
+	return result === 0;
+}
