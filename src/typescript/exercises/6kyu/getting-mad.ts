5cbef8e5ae17a90e5ce261cf3f83e6841115e646
Cartago <cartago.relaxingcup@gmail.com>
2024-01-13 00:36:07 +0100

    Damos solucion a la kata de getting mad

--- src/typescript/exercises/6kyu/getting-mad.ts
+++ src/typescript/exercises/6kyu/getting-mad.ts
@@ -1,6 +1,12 @@
 //* https://www.codewars.com/kata/593a061b942a27ac940000a7/train/typescript
 
 export function gettingMad(array: number[]): number {
-    // return the minimum absolute differnce
-    return 0;
+	const resultArr: number[] = [];
+	for (let i = 0; i < array.length; i++) {
+		for (let j = 0; j < array.length; j++) {
+			if (j === i) continue;
+			resultArr.push(Math.abs(array[i] - array[j]));
+		}
+	}
+	return Math.min(...resultArr);
 }
