d8bc8f6fc576fabb3e95a68b073318d5b2a13a3e
Cartago <cartago.relaxingcup@gmail.com>
2024-01-13 00:31:24 +0100

    Preparmaos el ejercicio de getting mad

---
+++ src/typescript/exercises/6kyu/getting-mad.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/593a061b942a27ac940000a7/train/typescript
+
+export function gettingMad(array: number[]): number {
+    // return the minimum absolute differnce
+    return 0;
+}
