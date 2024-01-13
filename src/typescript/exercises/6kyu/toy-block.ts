fd3be28342864f62a8f0965318f485d8c7506530
Cartago <cartago.relaxingcup@gmail.com>
2024-01-13 10:35:32 +0100

    Completamos la kata de blockes para formar un cuadrado

--- src/typescript/exercises/6kyu/toy-block.ts
+++ src/typescript/exercises/6kyu/toy-block.ts
@@ -1,36 +1,33 @@
 //* https://www.codewars.com/kata/5cab471da732b30018968071/train/typescript
 
 export function buildSquare(blocks: number[]): boolean {
-	const countBlocks = blocks.reduce(
-		(acc, curr) => {
-			acc[curr]++;
-			return acc;
-		},
-		{
-			1: 0,
-			2: 0,
-			3: 0,
-			4: 0,
-		} as Record<number, number>
-	);
+	const countBlocks: Record<number, number> = {
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+	};
+	for (let block of blocks) {
+		countBlocks[block]++;
+	}
+
 	// Block from 4 are complete rows
 	let possibleRows = countBlocks[4];
 	// Block from 3 plus 1, are complete columns
-	for (let i = 0; i < countBlocks[3]; i++) {
-		if (countBlocks[1] > 0) {
-			possibleRows++;
-			countBlocks[1]--;
-		}
+	while (countBlocks[3] >= 1 && countBlocks[1] >= 1) {
+		possibleRows++;
+		countBlocks[3]--;
+		countBlocks[1]--;
 	}
 	// Block from 2 plus 2 or 2 plus two times 1, are complete columns
-	for (let i = 0; i < countBlocks[2]; i++) {
-		if (countBlocks[2] > 0) {
-			possibleRows++;
-			countBlocks[2]--;
-		} else if (countBlocks[1] > 1) {
-			possibleRows++;
-			countBlocks[1] -= 2;
-		}
+	while (countBlocks[2] >= 2) {
+		possibleRows++;
+		countBlocks[2] -= 2;
+	}
+	while (countBlocks[2] >= 1 && countBlocks[1] >= 2) {
+		possibleRows++;
+		countBlocks[2]--;
+		countBlocks[1] -= 2;
 	}
 	// Blocks from 1 four times are complete rows
 	const oneDiffRows = Math.floor(countBlocks[1] / 4);
