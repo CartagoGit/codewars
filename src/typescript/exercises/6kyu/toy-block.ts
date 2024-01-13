63bfc5c56a268c75834015827f7b837cb940ce2e
Cartago <cartago.relaxingcup@gmail.com>
2024-01-13 10:23:48 +0100

    Desarrollamos el ejercicio

--- src/typescript/exercises/6kyu/toy-block.ts
+++ src/typescript/exercises/6kyu/toy-block.ts
@@ -13,5 +13,28 @@ export function buildSquare(blocks: number[]): boolean {
 			4: 0,
 		} as Record<number, number>
 	);
-	return true;
+	// Block from 4 are complete rows
+	let possibleRows = countBlocks[4];
+	// Block from 3 plus 1, are complete columns
+	for (let i = 0; i < countBlocks[3]; i++) {
+		if (countBlocks[1] > 0) {
+			possibleRows++;
+			countBlocks[1]--;
+		}
+	}
+	// Block from 2 plus 2 or 2 plus two times 1, are complete columns
+	for (let i = 0; i < countBlocks[2]; i++) {
+		if (countBlocks[2] > 0) {
+			possibleRows++;
+			countBlocks[2]--;
+		} else if (countBlocks[1] > 1) {
+			possibleRows++;
+			countBlocks[1] -= 2;
+		}
+	}
+	// Blocks from 1 four times are complete rows
+	const oneDiffRows = Math.floor(countBlocks[1] / 4);
+	possibleRows += oneDiffRows;
+
+	return possibleRows >= 4;
 }
