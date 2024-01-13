db39d7179fff0f27e0637fa36b2f276dd8e4d71d
Cartago <cartago.relaxingcup@gmail.com>
2024-01-13 10:13:17 +0100

    Comenzamos a plantear el ejercicio

--- src/typescript/exercises/6kyu/toy-block.ts
+++ src/typescript/exercises/6kyu/toy-block.ts
@@ -1,5 +1,17 @@
 //* https://www.codewars.com/kata/5cab471da732b30018968071/train/typescript
 
 export function buildSquare(blocks: number[]): boolean {
+	const countBlocks = blocks.reduce(
+		(acc, curr) => {
+			acc[curr]++;
+			return acc;
+		},
+		{
+			1: 0,
+			2: 0,
+			3: 0,
+			4: 0,
+		} as Record<number, number>
+	);
 	return true;
 }
