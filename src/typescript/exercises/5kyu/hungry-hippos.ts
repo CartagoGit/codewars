2be54f971deaad589fb3939181d7352c4f99da1e
Cartago <cartago.relaxingcup@gmail.com>
2024-01-14 18:59:00 +0100

    Preparamos el ejercicio de hungry hippos

---
+++ src/typescript/exercises/5kyu/hungry-hippos.ts
@@ -0,0 +1,12 @@
+//* https://www.codewars.com/kata/590300eb378a9282ba000095/train/typescript
+
+export class Game {
+	constructor(board: number[][]) {
+		// Good luck
+	}
+
+	play(): number {
+		// Good luck
+        return 0;
+	}
+}
