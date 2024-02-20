d691eeac9e9e2388bd1730f096192357f4bf8def
Cartago <cartago.relaxingcup@gmail.com>
2024-02-20 20:57:56 +0100

    Actualizar función isReallyNaN a utilizar Number.isNaN

--- src/typescript/exercises/8kyu/is-really-nan.ts
+++ src/typescript/exercises/8kyu/is-really-nan.ts
@@ -1,3 +1,3 @@
 //* https://www.codewars.com/kata/56c24c58e0c0f741d4001aef/train/typescript
 
-export const isReallyNaN = (val: any): boolean => val !== val && isNaN(Number(val));
+export const isReallyNaN = Number.isNaN;
