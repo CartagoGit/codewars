1f6e90c285ac9d9c1d6075702209366c425f7c3f
Cartago <cartago.relaxingcup@gmail.com>
2024-02-20 20:49:21 +0100

    Correcciones de errores y mejoras de rendimiento en el código

---
+++ src/typescript/exercises/8kyu/is-really-nan.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/56c24c58e0c0f741d4001aef/train/typescript
+
+export function isReallyNaN(val: any): boolean {
+    // return isNaN(val);  // wasn't working as planned :-(
+    return true || false;
+  };
