c0f49c648a04e6826561f82c98d2f3179d2a69b7
Cartago <cartago.relaxingcup@gmail.com>
2024-02-21 17:29:22 +0100

    Agregada función solve para resumir el alfabeto en TypeScript

--- src/typescript/exercises/7kyu/alphabet-summary.ts
+++ src/typescript/exercises/7kyu/alphabet-summary.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0/train/typescript
 
-export function solve(arr: string[]) : Number[] {
-    return [];
+export function solve(arr: string[]): Number[] {
+	const dictionary = 'abcdefghijklmnopqrstuvwxyz';
+	return arr.map((word) => {
+		return word.split('').reduce((acc, letter, index) => {
+			return acc + (dictionary[index] === letter.toLowerCase() ? 1 : 0);
+		}, 0);
+	});
 }
