2bbc57221a508204a78861abdd28c00008940687
Cartago <cartago.relaxingcup@gmail.com>
2024-03-14 19:57:58 +0100

    Corregir función de reversión de cadenas

--- src/typescript/exercises/8kyu/reverser-strings.ts
+++ src/typescript/exercises/8kyu/reverser-strings.ts
@@ -1,6 +1,5 @@
-//*!GROUP https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/typescript
-
+//* https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/typescript
 
 export function solution(str: string): string {
-    return str; // reverse this!
-  }
+	return str.split('').reverse().join('');
+}
