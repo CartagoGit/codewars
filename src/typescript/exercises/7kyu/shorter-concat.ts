11ff3b6c7cabcb7278a585da11514a7069c2709d
Cartago <cartago.relaxingcup@gmail.com>
2024-03-16 21:25:23 +0100

    Agregada función shorterReverseLonger para concatenar cadenas de manera más corta

--- src/typescript/exercises/7kyu/shorter-concat.ts
+++ src/typescript/exercises/7kyu/shorter-concat.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/54557d61126a00423b000a45/train/typescript
 
 export function shorterReverseLonger(a: string, b: string): string {
-	return '';
+	const short = a.length < b.length ? a : b;
+	const long = short === a ? b : a;
+	return short + long.split('').reverse().join('') + short;
 }
