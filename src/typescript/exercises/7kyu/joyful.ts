31dfe854f36834ab44e00f656088be9c39c082b9
Cartago <cartago.relaxingcup@gmail.com>
2024-03-18 07:43:37 +0100

    Agregada lógica para verificar si un número es "joyful" en joyful.ts

--- src/typescript/exercises/7kyu/joyful.ts
+++ src/typescript/exercises/7kyu/joyful.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/570523c146edc287a50014b1/train/typescript
 
 export const numberJoy = (n: number): boolean => {
-    return false;
-  }
+	const sum = n
+		.toString()
+		.split('')
+		.reduce((acc, curr) => acc + parseInt(curr), 0);
+	const reverse = parseInt(sum.toString().split('').reverse().join(''), 0);
+	return sum * reverse === n;
+};
