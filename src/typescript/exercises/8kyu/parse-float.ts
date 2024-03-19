e16294863f654c423d84e30040e5b56296c82a8f
Cartago <cartago.relaxingcup@gmail.com>
2024-03-19 15:37:04 +0100

    Solucionamos la kata de parse float

--- src/typescript/exercises/8kyu/parse-float.ts
+++ src/typescript/exercises/8kyu/parse-float.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/57a386117cb1f31890000039/train/typescript
 
-export function parseF(s:string): number|null {
-    return null;
-  }
+export function parseF(s: string): number | null {
+	const num = parseFloat(s);
+	return isNaN(num) ? null : num;
+}
