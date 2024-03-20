53766712463437fe135536b91466e572ed98d644
Cartago <cartago.relaxingcup@gmail.com>
2024-03-20 20:37:43 +0100

    Agregada función para calcular la frecuencia de caracteres en una cadena

--- src/typescript/exercises/7kyu/frequency-sequence.ts
+++ src/typescript/exercises/7kyu/frequency-sequence.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/585a033e3a36cdc50a00011c/train/typescript
 
 export function freqSeq(str: string, sep: string): string {
-	return ''; // your code here
+	const arrChain = str.split('');
+	let freqMap = new Map<string, number>();
+	for (let char of arrChain) {
+		freqMap.set(char, (freqMap.get(char) ?? 0) + 1);
+	}
+	return arrChain.map((char) => freqMap.get(char)).join(sep);
 }
