4e956b13d24a2440a52ea7e80b6b3237aa9123cb
Cartago <cartago.relaxingcup@gmail.com>
2024-03-21 09:16:46 +0100

    Actualizar función countRedBeads en simple-breads-count.ts

--- src/typescript/exercises/7kyu/simple-breads-count.ts
+++ src/typescript/exercises/7kyu/simple-breads-count.ts
@@ -1,5 +1,3 @@
 //* https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/typescript
 
-export function countRedBeads(n: number): number {
-	return 0; //your code here
-}
+export const countRedBeads = (n: number): number => (n < 2 ? 0 : (n - 1) * 2);
