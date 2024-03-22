4cb902d53c6745b62dcc8f6b188061a46b1dda33
Cartago <cartago.relaxingcup@gmail.com>
2024-03-22 07:31:24 +0100

    Actualizar función isVeryEvenNumber en very-even.ts

--- src/typescript/exercises/7kyu/very-even.ts
+++ src/typescript/exercises/7kyu/very-even.ts
@@ -1,5 +1,12 @@
 //* https://www.codewars.com/kata/58c9322bedb4235468000019/train/typescript
 
-export function isVeryEvenNumber(n: number): boolean {
-	return true;
-}
+export const isVeryEvenNumber = (n: number): boolean => {
+	let veryEvenNum = n.toString();
+	while (veryEvenNum.length > 1) {
+		veryEvenNum = veryEvenNum
+			.split('')
+			.reduce((acc, num) => acc + parseInt(num), 0)
+			.toString();
+	}
+    return parseInt(veryEvenNum) % 2 === 0;
+};
