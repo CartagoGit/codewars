3938f55e86da35eb90f22bdc6932f757021fd6ae
Cartago <cartago.relaxingcup@gmail.com>
2024-03-23 12:05:30 +0100

    Agregado cálculo del producto de elementos adyacentes en el arreglo

--- src/typescript/exercises/7kyu/maximum-product.ts
+++ src/typescript/exercises/7kyu/maximum-product.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/5a4138acf28b82aa43000117/train/typescript
 
 export function adjacentElementsProduct(arr: number[]): number {
-	// your code here
+	let products = [];
+	for (let i = 1; i < arr.length; i++) {
+		products.push(arr[i - 1] * arr[i]);
+	}
+	return Math.max(...products);
 }
