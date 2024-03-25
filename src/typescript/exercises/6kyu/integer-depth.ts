ae1e27d9152efce6cf06c42939af5e2f21357b91
Cartago <cartago.relaxingcup@gmail.com>
2024-03-25 20:43:27 +0100

    Añadir lógica para calcular la profundidad de enteros en TypeScript

--- src/typescript/exercises/6kyu/integer-depth.ts
+++ src/typescript/exercises/6kyu/integer-depth.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/59b401e24f98a813f9000026/typescript
 
 export function computeDepth(n: number): number {
-    return 0;
-  }
+	const uniqueDigits = new Set();
+	const areComputed = () => uniqueDigits.size === 10;
+    let i = 0;
+    while (!areComputed()) {
+        i++;
+        const product = n * i;
+        product.toString().split('').forEach(digit => uniqueDigits.add(digit));
+    }
+	return i;
+}
