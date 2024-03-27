4760f76ab3b3b9b015f75799d89f57d09767f94b
Cartago <cartago.relaxingcup@gmail.com>
2024-03-27 18:59:52 +0100

    Agregada implementación para determinar si un número es una potencia de dos en TypeScript

--- src/typescript/exercises/7kyu/power-of-two.ts
+++ src/typescript/exercises/7kyu/power-of-two.ts
@@ -1,6 +1,5 @@
 //* https://www.codewars.com/kata/534d0a229345375d520006a0/train/typescript
 
 export function isPowerOfTwo(n: number): boolean {
-    // Insert magic here
-    return false;
+	return Number.isInteger(Math.log2(n));
 }
