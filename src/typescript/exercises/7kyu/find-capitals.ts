b1efb2fdd7c585d009c9a5c22f63edc355bc3f33
Cartago <cartago.relaxingcup@gmail.com>
2024-03-28 23:10:28 +0100

    Actualizar función capitals para encontrar las letras mayúsculas en una palabra

--- src/typescript/exercises/7kyu/find-capitals.ts
+++ src/typescript/exercises/7kyu/find-capitals.ts
@@ -1,6 +1,7 @@
 //* https://www.codewars.com/kata/539ee3b6757843632d00026b/train/typescript
 
-export function capitals(word: string): number[] {
-	// Write your code here
-	return [] as number[];
-}
+export const capitals = (word: string): number[] =>
+	word
+		.split('')
+		.map((letter, index) => (letter === letter.toUpperCase() ? index : -1))
+		.filter((index) => index !== -1);
