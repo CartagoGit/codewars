72a18d176946c0729100bfad30c032575a5e9513
Cartago <cartago.relaxingcup@gmail.com>
2024-03-28 23:11:54 +0100

    Agrega función para encontrar las letras mayúsculas en una palabra

--- src/typescript/exercises/7kyu/find-capitals.ts
+++ src/typescript/exercises/7kyu/find-capitals.ts
@@ -1,7 +1,8 @@
 //* https://www.codewars.com/kata/539ee3b6757843632d00026b/train/typescript
 
-export const capitals = (word: string): number[] =>
-	word
+export const capitals = (word: string): number[] => {
+	return word
 		.split('')
 		.map((letter, index) => (letter === letter.toUpperCase() ? index : -1))
 		.filter((index) => index !== -1);
+};
