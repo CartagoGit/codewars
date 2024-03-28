e2fd9d341bc87d57384ed5f2c12c1377ec48e9da
Cartago <cartago.relaxingcup@gmail.com>
2024-03-28 23:08:03 +0100

    Correcciones de errores y mejoras de rendimiento en el código

---
+++ src/typescript/exercises/7kyu/find-capitals.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/539ee3b6757843632d00026b/train/typescript
+
+export function capitals(word: string): number[] {
+	// Write your code here
+	return [] as number[];
+}
