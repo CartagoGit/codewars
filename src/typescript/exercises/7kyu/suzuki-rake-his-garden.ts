bc2e97ad44a563a01315f2e82717d01682665468
Cartago <cartago.relaxingcup@gmail.com>
2024-03-29 11:56:10 +0100

    Actualizar función rakeGarden para reemplazar todas las palabras en el jardín, excepto "rock", con "gravel"

--- src/typescript/exercises/7kyu/suzuki-rake-his-garden.ts
+++ src/typescript/exercises/7kyu/suzuki-rake-his-garden.ts
@@ -1,5 +1,8 @@
 //* https://www.codewars.com/kata/571c1e847beb0a8f8900153d/train/typescript
 
 export function rakeGarden(garden: string): string {
-	return '';
+	return garden
+		.split(' ')
+		.map((word) => (word === 'rock' ? word : 'gravel'))
+		.join(' ');
 }
