a6f42f0e286836d4a6012aac5397f3198f9fa32e
Cartago <cartago.relaxingcup@gmail.com>
2024-03-30 16:09:09 +0100

    Agregada función partlist para dividir una lista en partes

--- src/typescript/exercises/7kyu/parts-of-list.ts
+++ src/typescript/exercises/7kyu/parts-of-list.ts
@@ -1,6 +1,13 @@
 //* https://www.codewars.com/kata/56f3a1e899b386da78000732/train/typescript
 
 export function partlist(arr: string[]): string[][] {
-	// your code
-	return [];
+	let result: string[][] = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (i === arr.length - 1) break;
+		result.push([
+			arr.slice(0, i + 1).join(' '),
+			arr.slice(i + 1).join(' '),
+		]);
+	}
+	return result;
 }
