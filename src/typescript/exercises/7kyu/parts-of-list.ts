cbf697394af3203c79d2c09dd81e4814d8ea2bc0
Cartago <cartago.relaxingcup@gmail.com>
2024-03-30 16:14:08 +0100

    Actualizar función partlist en parts-of-list.ts

--- src/typescript/exercises/7kyu/parts-of-list.ts
+++ src/typescript/exercises/7kyu/parts-of-list.ts
@@ -1,13 +1,10 @@
 //* https://www.codewars.com/kata/56f3a1e899b386da78000732/train/typescript
 
 export function partlist(arr: string[]): string[][] {
-	let result: string[][] = [];
-	for (let i = 0; i < arr.length; i++) {
-		if (i === arr.length - 1) break;
-		result.push([
-			arr.slice(0, i + 1).join(' '),
-			arr.slice(i + 1).join(' '),
-		]);
-	}
-	return result;
+	return arr
+		.map((_word, index) => [
+			arr.slice(0, index + 1).join(' '),
+			arr.slice(index + 1).join(' '),
+		])
+		.slice(0, -1);
 }
