4159b91d34b037dc529647ac6b73827190dfdaf1
Cartago <cartago.relaxingcup@gmail.com>
2024-04-01 23:24:46 +0200

    Refactorización de la función menFromBoys para mejorar el rendimiento y la legibilidad del código.

--- src/typescript/exercises/7kyu/sort-out-from-men-from-boys.ts
+++ src/typescript/exercises/7kyu/sort-out-from-men-from-boys.ts
@@ -1,14 +1,13 @@
 //* https://www.codewars.com/kata/5af15a37de4c7f223e00012d/train/typescript
 
 export function menFromBoys(arr: number[]): number[] {
-	const [evens, odds] = arr
-		.reduce(
-			(acc, curr) => {
-				curr % 2 === 0 ? acc[0].add(curr) : acc[1].add(curr);
-				return acc;
-			},
-			[new Set(), new Set()] as Set<number>[]
-		)
-		.map((set) => [...set]);
+	const uniqueValues = [...new Set(arr)];
+	const [evens, odds] = uniqueValues.reduce(
+		(acc, curr) => {
+			acc[curr % 2 === 0 ? 0 : 1].push(curr);
+			return acc;
+		},
+		[[], []] as number[][]
+	);
 	return [...evens.sort((a, b) => a - b), ...odds.sort((c, d) => d - c)];
 }
