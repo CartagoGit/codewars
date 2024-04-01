9ee4b72f96c4400762d95e4b8598bf597bf01f95
Cartago <cartago.relaxingcup@gmail.com>
2024-04-01 23:18:26 +0200

    Realizamos el ejercicio

--- src/typescript/exercises/7kyu/sort-out-from-men-from-boys.ts
+++ src/typescript/exercises/7kyu/sort-out-from-men-from-boys.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/5af15a37de4c7f223e00012d/train/typescript
 
 export function menFromBoys(arr: number[]): number[] {
-	// your code here
+	const [evens, odds] = arr
+		.reduce(
+			(acc, curr) => {
+				curr % 2 === 0 ? acc[0].add(curr) : acc[1].add(curr);
+				return acc;
+			},
+			[new Set(), new Set()] as Set<number>[]
+		)
+		.map((set) => [...set]);
+	return [...evens.sort((a, b) => a - b), ...odds.sort((c, d) => d - c)];
 }
