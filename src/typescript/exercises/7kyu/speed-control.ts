76b9bb493f5e4866b9ac4da5fbebb356dfd69552
Cartago <cartago.relaxingcup@gmail.com>
2024-04-02 07:37:17 +0200

    Corregir cálculo de velocidad máxima en la función gps

--- src/typescript/exercises/7kyu/speed-control.ts
+++ src/typescript/exercises/7kyu/speed-control.ts
@@ -1,6 +1,12 @@
 //* https://www.codewars.com/kata/56484848ba95170a8000004d/train/typescript
 
-export const gps = (s:number, xs:number[]):number => {
-    // your code
-    return -1
-  }
+export const gps = (s: number, xs: number[]): number => {
+	if (xs.length <= 1) return 0;
+	let maxSpeed = 0;
+	for (let i = 0; i < xs.length - 1; i++) {
+		const deltaDistance = xs[i + 1] - xs[i];
+		const speed = (3600 * deltaDistance) / s;
+		maxSpeed = Math.max(maxSpeed, speed);
+	}
+	return Math.floor(maxSpeed);
+};
