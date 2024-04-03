28db194ababb001e68526c0725d9902e61b76feb
Cartago <cartago.relaxingcup@gmail.com>
2024-04-04 00:51:50 +0200

    Corregir reconocimiento de caracteres en el software

--- src/typescript/exercises/8kyu/char-recognition-software.ts
+++ src/typescript/exercises/8kyu/char-recognition-software.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/typescript
 
 export function correct(s: string): string {
-	// your code here
+	return s
+		.split('')
+		.map((char) =>
+			char === '5' ? 'S' : char === '0' ? 'O' : char === '1' ? 'I' : char
+		)
+		.join('');
 }
