78196dad457bb1ae3727cd306f6e560747a4f185
Cartago <cartago.relaxingcup@gmail.com>
2024-04-04 19:55:26 +0200

    Actualización de la función de búsqueda para filtrar resultados por fecha

---
+++ src/typescript/exercises/7kyu/tail-swap.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/5868812b15f0057e05000001/train/typescript
+
+export function tailSwap(arr: [string, string]): [string, string] {
+	// your code here
+	return arr;
+}
