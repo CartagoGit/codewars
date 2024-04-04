4ccea228a29cd70b856042b0944ad30610e98e01
Cartago <cartago.relaxingcup@gmail.com>
2024-04-04 19:57:57 +0200

    Actualizar función tailSwap para intercambiar las colas de las cadenas

--- src/typescript/exercises/7kyu/tail-swap.ts
+++ src/typescript/exercises/7kyu/tail-swap.ts
@@ -1,6 +1,7 @@
 //* https://www.codewars.com/kata/5868812b15f0057e05000001/train/typescript
 
 export function tailSwap(arr: [string, string]): [string, string] {
-	// your code here
-	return arr;
+	const [a0, a1] = arr[0].split(':');
+	const [b0, b1] = arr[1].split(':');
+	return [`${a0}:${b1}`, `${b0}:${a1}`];
 }
