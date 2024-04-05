d12689ac2f035837baaeef46416716fbae8ff303
Cartago <cartago.relaxingcup@gmail.com>
2024-04-06 01:13:43 +0200

    Agregada función para calcular la media y cadena de números en TypeScript

--- src/typescript/exercises/7kyu/calculate-mean.ts
+++ src/typescript/exercises/7kyu/calculate-mean.ts
@@ -1,5 +1,8 @@
 //* https://www.codewars.com/kata/56f7493f5d7c12d1690000b6/train/typescript
 
 export function mean(lst: string[]): [number, string] {
-	//code here
+	const nums = lst.filter((n) => !isNaN(Number(n))).map(Number);
+	const chain = lst.filter((n) => isNaN(Number(n))).join('');
+	const median = nums.reduce((a, b) => a + b, 0) / nums.length;
+	return [Math.floor(median * 10) / 10, chain];
 }
