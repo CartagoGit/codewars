9253ab56192737c22237378ac22479e1ce14c7da
Cartago <cartago.relaxingcup@gmail.com>
2024-04-07 08:57:28 +0200

    Implementar algoritmo de péndulo poeta en TypeScript

--- src/typescript/exercises/7kyu/poet-pendulum.ts
+++ src/typescript/exercises/7kyu/poet-pendulum.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5bd776533a7e2720c40000e5/train/typescript
 
 export function pendulum(values: number[]) {
-	// your code here
+	const arrOrder = values.sort((a, b) => a - b);
+	let result: number[] = [];
+	arrOrder.forEach((num, index) =>
+		index % 2 === 0 ? result.unshift(num) : result.push(num)
+	);
+	return result;
 }
