af616c74c0c36fc65e38128990f1308d30c7b01a
Cartago <cartago.relaxingcup@gmail.com>
2024-04-09 07:37:32 +0200

    Agregada función para dividir la cuenta

--- src/typescript/exercises/7kyu/split-the-bill.ts
+++ src/typescript/exercises/7kyu/split-the-bill.ts
@@ -3,7 +3,11 @@
 export function splitTheBill(x: { [k: string]: number }): {
 	[k: string]: number;
 } {
-	//code away...
-
-	return x;
+	const sum = Object.values(x).reduce((a, b) => a + b, 0);
+	const avg = sum / Object.keys(x).length;
+	let reciveMoney = {} as { [k: string]: number };
+	for (let key in x) {
+		reciveMoney[key] = +(x[key] - avg).toFixed(2);
+	}
+	return reciveMoney;
 }
