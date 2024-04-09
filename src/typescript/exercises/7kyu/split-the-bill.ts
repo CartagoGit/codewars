fa2e7289957395796198ef419da674468fb6aec6
Cartago <cartago.relaxingcup@gmail.com>
2024-04-09 07:34:56 +0200

    Actualización de la función de búsqueda para filtrar resultados por fecha

---
+++ src/typescript/exercises/7kyu/split-the-bill.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/5641275f07335295f10000d0/train/typescript
+
+export function splitTheBill(x: { [k: string]: number }): {
+	[k: string]: number;
+} {
+	//code away...
+
+	return x;
+}
