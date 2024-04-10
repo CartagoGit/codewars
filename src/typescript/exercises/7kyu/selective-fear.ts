fa5ab584704b35688c455a8fc1e7de4e477ff247
Cartago <cartago.relaxingcup@gmail.com>
2024-04-10 17:07:29 +0200

    Corregir tipos de datos en amIAfraid()

--- src/typescript/exercises/7kyu/selective-fear.ts
+++ src/typescript/exercises/7kyu/selective-fear.ts
@@ -4,14 +4,14 @@ const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
 type IDay = typeof days[number];
 
 export function amIAfraid(day: string, num: number): boolean {
-	const numIllness: Record<IDay, () => boolean> = {
-        Monday: () => num === 12,
-        Tuesday: () => num > 95,
-        Wednesday: () => num === 34,
-        Thursday: () => num === 0,
-        Friday: () => num % 2 === 0,
-        Saturday: () => num === 56,
-        Sunday: () => Math.abs(num) === 666 
+	const numIllness: Record<IDay, boolean> = {
+        Monday: num === 12,
+        Tuesday: num > 95,
+        Wednesday: num === 34,
+        Thursday: num === 0,
+        Friday: num % 2 === 0,
+        Saturday: num === 56,
+        Sunday: Math.abs(num) === 666 
     }
-    return numIllness[day as IDay]();
+    return numIllness[day as IDay] ?? false;
 }
