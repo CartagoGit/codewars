4b504f8c95c94680734cbd6d15bbf1d5ef4da199
Cartago <cartago.relaxingcup@gmail.com>
2024-04-10 17:03:59 +0200

    Añadir lógica para determinar si tengo miedo en función del día y el número

--- src/typescript/exercises/7kyu/selective-fear.ts
+++ src/typescript/exercises/7kyu/selective-fear.ts
@@ -1,5 +1,17 @@
 //* https://www.codewars.com/kata/55b1fd84a24ad00b32000075/train/typescript
 
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] as const;
+type IDay = typeof days[number];
+
 export function amIAfraid(day: string, num: number): boolean {
-	//Help me..
+	const numIllness: Record<IDay, () => boolean> = {
+        Monday: () => num === 12,
+        Tuesday: () => num > 95,
+        Wednesday: () => num === 34,
+        Thursday: () => num === 0,
+        Friday: () => num % 2 === 0,
+        Saturday: () => num === 56,
+        Sunday: () => Math.abs(num) === 666 
+    }
+    return numIllness[day as IDay]();
 }
