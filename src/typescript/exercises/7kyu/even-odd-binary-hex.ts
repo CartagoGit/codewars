d98deb4b0f7d7990c02a7eb8a40369d392158db0
Cartago <cartago.relaxingcup@gmail.com>
2024-04-12 09:04:50 +0200

    Comenzamos el ejercicio

---
+++ src/typescript/exercises/7kyu/even-odd-binary-hex.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/583ade15666df5a64e000058/train/typescript
+
+export function evensAndOdds(n: number): string {
+	throw new Error('The method or operation is not implemented.');
+}
