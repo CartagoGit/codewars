e2d24a7cce933d56e95e49bbe6dfc732e210fe8c
Cartago <cartago.relaxingcup@gmail.com>
2024-04-12 09:06:48 +0200

    Actualizar función evensAndOdds para convertir un número en su representación binaria o hexadecimal dependiendo de si es par o impar

--- src/typescript/exercises/7kyu/even-odd-binary-hex.ts
+++ src/typescript/exercises/7kyu/even-odd-binary-hex.ts
@@ -1,5 +1,4 @@
 //* https://www.codewars.com/kata/583ade15666df5a64e000058/train/typescript
 
-export function evensAndOdds(n: number): string {
-	throw new Error('The method or operation is not implemented.');
-}
+export const evensAndOdds = (n: number): string =>
+	n.toString(n % 2 === 0 ? 2 : 16);
