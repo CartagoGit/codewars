b81e19e7b5ea47e10c1bc61118e9e01c46fbc6ec
Cartago <cartago.relaxingcup@gmail.com>
2024-04-15 20:32:08 +0200

    Corregir función de cifrado digital en digital-cypher.ts

--- src/typescript/exercises/7kyu/digital-cypher.ts
+++ src/typescript/exercises/7kyu/digital-cypher.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/592e830e043b99888600002d/train/typescript
 
 export function encode(str: String, n: number): number[] {
-	return [1, 2, 3];
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	const numLimit = n.toString().length;
+	let result: number[] = [];
+	for (let i = 0; i < str.length; i++) {
+		const num = n.toString()[i % numLimit];
+		const char = str[i].toLowerCase();
+		result.push(alphabet.indexOf(char) + 1 + Number(num));
+	}
+	return result;
 }
