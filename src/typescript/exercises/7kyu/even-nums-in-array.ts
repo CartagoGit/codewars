d2b65d5e2291919d6ebcfadd6ab9d66d1bd02ac6
Cartago <cartago.relaxingcup@gmail.com>
2024-04-17 00:47:09 +0200

    Actualizar función evenNumbers para devolver los números pares de un array

--- src/typescript/exercises/7kyu/even-nums-in-array.ts
+++ src/typescript/exercises/7kyu/even-nums-in-array.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c/train/typescript
 
 export function evenNumbers(array: number[], n: number): number[] {
-	return [1, 2, 3]; // your code here
+	return array.filter((num) => num % 2 === 0).slice(-n);
 }
