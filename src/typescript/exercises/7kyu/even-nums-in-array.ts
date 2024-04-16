1e7d1b31e41a9f07c34170c37c44b115934597a3
Cartago <cartago.relaxingcup@gmail.com>
2024-04-17 00:38:10 +0200

    Preparamos el ejercicio

---
+++ src/typescript/exercises/7kyu/even-nums-in-array.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c/train/typescript
+
+export function evenNumbers(array: number[], n: number): number[] {
+	return [1, 2, 3]; // your code here
+}
