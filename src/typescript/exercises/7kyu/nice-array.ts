dbf02c5cba521c22ef07d0c27b7452afd2581f7d
Cartago <cartago.relaxingcup@gmail.com>
2024-04-16 17:20:43 +0200

    Actualizar función isNice para comprobar si un array es agradable

--- src/typescript/exercises/7kyu/nice-array.ts
+++ src/typescript/exercises/7kyu/nice-array.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/59b844528bcb7735560000a0/train/typescript
 
 export function isNice(arr: number[]): boolean {
-	return [true, false][~~(Math.random() * 2)];
+	if (arr.length === 0) return false;
+	const set = new Set(arr);
+	return arr.every((num) => set.has(num - 1) || set.has(num + 1));
 }
