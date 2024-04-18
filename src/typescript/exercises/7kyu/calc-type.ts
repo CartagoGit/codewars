3ccb5340452480a7a36e52c8ae8f3a8d886930b7
Cartago <cartago.relaxingcup@gmail.com>
2024-04-18 08:10:38 +0200

    Preaparamos el ejercicio

---
+++ src/typescript/exercises/7kyu/calc-type.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/5aca48db188ab3558e0030fa/train/typescript
+
+export function calcType(a: number, b: number, res: number): string {
+    return 'calculation'; // TODO: your solution here
+  }
+  
