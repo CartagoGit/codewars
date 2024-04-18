57e5cebace66984cd4817339c7eef974c90c4452
Cartago <cartago.relaxingcup@gmail.com>
2024-04-18 08:13:53 +0200

    Actualizar función calcType para determinar el tipo de cálculo en calc-type.ts

--- src/typescript/exercises/7kyu/calc-type.ts
+++ src/typescript/exercises/7kyu/calc-type.ts
@@ -1,6 +1,8 @@
 //* https://www.codewars.com/kata/5aca48db188ab3558e0030fa/train/typescript
 
 export function calcType(a: number, b: number, res: number): string {
-    return 'calculation'; // TODO: your solution here
-  }
-  
+	if (a + b === res) return 'addition';
+	else if (a - b === res) return 'subtraction';
+	else if (a * b === res) return 'multiplication';
+	else return 'division'; 
+}
