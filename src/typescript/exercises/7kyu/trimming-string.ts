8842ec0bf36450ef19c2d317fb6f749cd0346144
Cartago <cartago.relaxingcup@gmail.com>
2024-04-19 07:36:09 +0200

    Actualizar función trim para recortar una cadena en trimming-string.ts

--- src/typescript/exercises/7kyu/trimming-string.ts
+++ src/typescript/exercises/7kyu/trimming-string.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/563fb342f47611dae800003c/train/typescript
 
 export function trim(str: string, size: number): string {
-	return str;
+	if (str.length <= size) return str;
+	if (size <= 3) return str.slice(0, size) + '.'.repeat(3);
+	return str.slice(0, size - 3) + '.'.repeat(3);
 }
