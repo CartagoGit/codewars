14c3612602cb39e6fb6ab80fb16c3a14fa6f02f5
Cartago <cartago.relaxingcup@gmail.com>
2024-04-21 02:06:20 +0200

    Actualizar función multipleOfIndex para filtrar los números que son múltiplos de su índice en multiples-index.ts

--- src/typescript/exercises/8kyu/multiples-index.ts
+++ src/typescript/exercises/8kyu/multiples-index.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5a34b80155519e1a00000009/train/typescript
 
 export function multipleOfIndex(array: number[]): number[] {
-	// good luck
+	return array.filter((num, index) => num === 0 || num % index === 0);
 }
