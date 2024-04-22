6e8138cbdf6a6fc29e35abe775cc11eeee189f8b
Cartago <cartago.relaxingcup@gmail.com>
2024-04-22 07:40:25 +0200

    Actualizar función flyBy para manejar el vuelo del dron en drone-fly-by.ts

--- src/typescript/exercises/7kyu/drone-fly-by.ts
+++ src/typescript/exercises/7kyu/drone-fly-by.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/58356a94f8358058f30004b5/train/typescript
 
 export function flyBy(lamps: string, drone: string): string {
-    // your code here
-  }
+	return drone.length >= lamps.length
+		? 'o'.repeat(lamps.length)
+		: 'o'.repeat(drone.length) + lamps.slice(drone.length);
+}
