222ce49d17c7b8de8f51d405c95d05e346895a6f
Cartago <cartago.relaxingcup@gmail.com>
2024-04-23 18:01:48 +0200

    Continuamos el ejercicio

--- src/typescript/exercises/7kyu/driving-licence.ts
+++ src/typescript/exercises/7kyu/driving-licence.ts
@@ -1,5 +1,16 @@
 //* https://www.codewars.com/kata/586a1af1c66d18ad81000134/train/typescript
 
-export function driver(data: Array<string>): string{
-    // Good luck
-  }
+const months = [ 'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec' ];
+
+export function driver(data: Array<string>): string {
+	let [firstName, midName, surName, birth, genere] = data;
+	let [day, month, year] = birth.split('-');
+	month = month.split('').slice(0, 3).join('');
+	genere = genere.split('').slice(0, 1).join('');
+	const firstRule = firstName.split('').slice(0, 5).join('').padEnd(5, '9');
+	const secondRule = year.split('')[2];
+    const monthStart = genere === 'M' ? 1 : 51;
+    const thirdRule = months.indexOf(month) + monthStart;
+    
+
+}
