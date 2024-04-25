2ec311ccd17444b1f106cc15b6dfeaec92621992
Cartago <cartago.relaxingcup@gmail.com>
2024-04-25 08:09:48 +0200

    Actualizar función makeUpperCase para convertir una cadena en mayúsculas en make-uppercase.ts

--- src/typescript/exercises/8kyu/make-uppercase.ts
+++ src/typescript/exercises/8kyu/make-uppercase.ts
@@ -1,3 +1,3 @@
-export function makeUpperCase(str: string): string{
-    return "";
-  }
+//* https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/typescript
+
+export const makeUpperCase = (str: string): string => str.toUpperCase();
