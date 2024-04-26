2b9c6f4cbd53cfa81fadad2699616374548f0614
Cartago <cartago.relaxingcup@gmail.com>
2024-04-26 09:19:35 +0200

    Actualizar función wordValue para calcular los valores de las palabras en word-values.ts

--- src/typescript/exercises/7kyu/word-values.ts
+++ src/typescript/exercises/7kyu/word-values.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/598d91785d4ce3ec4f000018/train/typescript
 
 export function wordValue(arr: string[]) {
-    // your code here
-  }
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	return arr.map((word, index) => {
+		return (
+			word.split('').reduce((acc, char) => {
+				const charValue = alphabet.indexOf(char) + 1;
+				return acc + charValue;
+			}, 0) *
+			(index + 1)
+		);
+	});
+}
