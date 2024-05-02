95473d5e446a17922f20294fdbcc81756cf5bad6
Cartago <cartago.relaxingcup@gmail.com>
2024-05-02 21:38:43 +0200

    chore: Refactor bestFriend function to improve performance and readability

--- src/typescript/exercises/7kyu/letter-best-friend.ts
+++ src/typescript/exercises/7kyu/letter-best-friend.ts
@@ -1,12 +1,5 @@
 //* https://www.codewars.com/kata/64fc03a318692c1333ebc04c/train/typescript
 
 export function bestFriend(txt: string, a: string, b: string): boolean {
-    const words = txt.split(' ').filter((word) => word.includes(a));
-    for(let word of words){
-        const letter = word.split('');
-        for (let i = 0; i < letter.length; i++) {
-            if(letter[i] === a && letter[i + 1] !== b) return false;
-        }
-    }
-    return true;
+    return txt.split(a).length === txt.split(a + b).length;
 }
