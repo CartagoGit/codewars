03267f6a58b784efb0bd2de7355ac73d0a2fe4a2
Cartago <cartago.relaxingcup@gmail.com>
2024-05-02 21:32:30 +0200

    feat: Implement bestFriend function to check if a word contains two specific letters

--- src/typescript/exercises/7kyu/letter-best-friend.ts
+++ src/typescript/exercises/7kyu/letter-best-friend.ts
@@ -1,5 +1,12 @@
 //* https://www.codewars.com/kata/64fc03a318692c1333ebc04c/train/typescript
 
 export function bestFriend(txt: string, a: string, b: string): boolean {
-    return true; //code here
+    const words = txt.split(' ').filter((word) => word.includes(a));
+    for(let word of words){
+        const letter = word.split('');
+        for (let i = 0; i < letter.length; i++) {
+            if(letter[i] === a && letter[i + 1] !== b) return false;
+        }
+    }
+    return true;
 }
