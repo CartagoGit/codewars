99ed15dbb5a436704b83fce550e5a8a58a01a063
Cartago <cartago.relaxingcup@gmail.com>
2024-05-02 21:19:57 +0200

    Preparamos el ejercicio

---
+++ src/typescript/exercises/7kyu/letter-best-friend.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/64fc03a318692c1333ebc04c/train/typescript
+
+export function bestFriend(txt: string, a: string, b: string): boolean {
+    return true; //code here
+}
