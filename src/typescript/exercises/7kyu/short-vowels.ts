6924b0c66f6fba7851ffb8db638b61dfd071a4cf
Cartago <cartago.relaxingcup@gmail.com>
2024-05-03 07:37:26 +0200

    feat: Update sortVowels function to handle empty and non-string inputs

--- src/typescript/exercises/7kyu/short-vowels.ts
+++ src/typescript/exercises/7kyu/short-vowels.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/59e49b2afc3c494d5d00002a/train/typescript
 
 export function sortVowels(str?: number | string | null): string {
-    // your code here
-  };
+	if (!str || !isNaN(Number(str))) return '';
+	const vowels = 'aeiou';
+	return str
+		.toString()
+		.split('')
+		.map((letter) =>
+			vowels.includes(letter.toLowerCase()) ? `|${letter}` : `${letter}|`
+		)
+		.join('\n');
+}
