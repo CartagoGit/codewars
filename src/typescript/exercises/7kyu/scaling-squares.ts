7555baca6a3e14f7f991dab61bec5e5fe5e90e64
Cartago <cartago.relaxingcup@gmail.com>
2024-05-06 07:31:02 +0200

    Preparamos el ejercicio

---
+++ src/typescript/exercises/7kyu/scaling-squares.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/56ed20a2c4e5d69155000301/train/typescript
+
+export const scale = (s: string, k: number, n: number): string => {
+	// your code
+	return 'nope...';
+};
