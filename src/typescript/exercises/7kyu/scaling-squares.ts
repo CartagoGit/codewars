a0a9b161f4c30d048642ec9c2da7b2f84dc63166
Cartago <cartago.relaxingcup@gmail.com>
2024-05-06 07:37:47 +0200

    feat: Scale squares based on input parameters

--- src/typescript/exercises/7kyu/scaling-squares.ts
+++ src/typescript/exercises/7kyu/scaling-squares.ts
@@ -1,6 +1,17 @@
 //* https://www.codewars.com/kata/56ed20a2c4e5d69155000301/train/typescript
 
 export const scale = (s: string, k: number, n: number): string => {
-	// your code
-	return 'nope...';
+	if (!s) return '';
+	const arr = s.split('\n');
+	let resultArr: string[] = [];
+	for (let line of arr) {
+		let newLine: string = '';
+		for (let char of line) {
+			newLine += char.repeat(k);
+		}
+		for (let i = 0; i < n; i++) {
+			resultArr.push(newLine);
+		}
+	}
+	return resultArr.join('\n');
 };
