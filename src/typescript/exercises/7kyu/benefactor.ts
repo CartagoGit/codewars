a8f34faa9ba573f7f7b0b848fafbe11cb3aa438c
Cartago <cartago.relaxingcup@gmail.com>
2024-05-07 07:45:38 +0200

    Seguimos intentanod la kata

--- src/typescript/exercises/7kyu/benefactor.ts
+++ src/typescript/exercises/7kyu/benefactor.ts
@@ -1,10 +1,10 @@
 //* https://www.codewars.com/kata/569b5cec755dd3534d00000f/train/typescript
 
 export function newAvg(arr: number[], newavg: number): number {
-	if (!arr?.length) return -1;
-	const currentAvg = arr.reduce((sum, value) => sum + value, 0) / arr.length;
+    if (!arr?.length) return -1;
+    const currentAvg = arr.reduce((sum, value) => sum + value, 0) / arr.length;
     if (newavg <= currentAvg) throw new Error("Expected New Average is too low");
-	const precision = 10 ** -10; // Ajustar según sea necesario
-    const expectedDonation = ((newavg - currentAvg) * (arr.length + 1)) + precision;
+    const expectedDonation = (newavg - currentAvg) * (arr.length + 1);
     return Math.ceil(expectedDonation);
 }
+
