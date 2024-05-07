e2f30e0e986672db4809b8eea33d114b4b1703af
Cartago <cartago.relaxingcup@gmail.com>
2024-05-07 07:30:52 +0200

    Preparamos el ejercicio

---
+++ src/typescript/exercises/7kyu/benefactor.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/569b5cec755dd3534d00000f/train/typescript
+
+export function newAvg(arr: number[], newavg: number): number {
+    // Your code here
+    return -1
+  }
