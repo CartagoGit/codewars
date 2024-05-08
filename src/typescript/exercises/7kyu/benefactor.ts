5dd10512427c183b42265c444dac7ee291ff0056
Cartago <cartago.relaxingcup@gmail.com>
2024-05-08 16:33:24 +0200

    feat: Update newAvg function to calculate expected donation correctly

--- src/typescript/exercises/7kyu/benefactor.ts
+++ src/typescript/exercises/7kyu/benefactor.ts
@@ -1,10 +1,8 @@
 //* https://www.codewars.com/kata/569b5cec755dd3534d00000f/train/typescript
 
 export function newAvg(arr: number[], newavg: number): number {
-    if (!arr?.length) return -1;
-    const currentAvg = arr.reduce((sum, value) => sum + value, 0) / arr.length;
-    if (newavg <= currentAvg) throw new Error("Expected New Average is too low");
-    const expectedDonation = (newavg - currentAvg) * (arr.length + 1);
+    const expectedDonation = newavg * (arr.length + 1) - arr.reduce((sum, value) => sum + value, 0);
+    if (expectedDonation <= 0) throw new Error("Expected New Average is too low");
     return Math.ceil(expectedDonation);
 }
 
