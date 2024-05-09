32b2aed8dfcd4cb03114f59d634348d47264807e
Cartago <cartago.relaxingcup@gmail.com>
2024-05-09 12:41:24 +0200

    Encontramos solucion a la kata

--- src/typescript/exercises/6kyu/ipv4-int32.ts
+++ src/typescript/exercises/6kyu/ipv4-int32.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/52ea928a1ef5cfec800003ee/train/typescript
 
 export function ipToInt32(ip: string): number {
-	return 0;
+	const parts = ip.split('.');
+    let num = 0;
+    for (let i = 0; i < parts.length; i++) {
+        num += parseInt(parts[i], 10) * Math.pow(256, parts.length - 1 - i);
+    }
+    return num;
 }
