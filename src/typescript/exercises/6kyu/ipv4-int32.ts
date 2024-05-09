6207aff4d25f5d0282f32de10092d8e6397e7220
Cartago <cartago.relaxingcup@gmail.com>
2024-05-09 12:50:17 +0200

    Encontramos una solucion mas elegante para la kata

--- src/typescript/exercises/6kyu/ipv4-int32.ts
+++ src/typescript/exercises/6kyu/ipv4-int32.ts
@@ -1,10 +1,7 @@
 //* https://www.codewars.com/kata/52ea928a1ef5cfec800003ee/train/typescript
 
 export function ipToInt32(ip: string): number {
-	const parts = ip.split('.');
-    let num = 0;
-    for (let i = 0; i < parts.length; i++) {
-        num += parseInt(parts[i], 10) * Math.pow(256, parts.length - 1 - i);
-    }
-    return num;
+    const parts = ip.split('.').map(Number);
+    const reverseParts = [...parts].reverse(); // To pow 256^0, 256^1, 256^2, 256^3 in order from ip right to left
+    return reverseParts.reduce((acc, value, index) => acc + value * Math.pow(256, index), 0);
 }
