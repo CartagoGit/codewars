99b77e6da88d32c722b9964614c1cb37708c9dc3
Cartago <cartago.relaxingcup@gmail.com>
2024-05-10 21:56:43 +0200

    Terminamos la kata

--- src/typescript/exercises/6kyu/check-the-exam.ts
+++ src/typescript/exercises/6kyu/check-the-exam.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/typescript
 
 export function checkExam(array1: string[], array2: string[]): number {
-    // good luck 
-   }
+	let score = 0;
+	for (let i = 0; i < array1.length; i++) {
+		if (array1[i] === array2[i]) score += 4;
+		else if (array2[i] !== '') score -= 1;
+	}
+	return score < 0 ? 0 : score;
+}
