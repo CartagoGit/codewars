dcd8799d5a5c359f0c7018c64fce434f301b4cb9
Cartago <cartago.relaxingcup@gmail.com>
2024-05-12 22:37:19 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/odd-march-8bits.ts
+++ src/typescript/exercises/7kyu/odd-march-8bits.ts
@@ -1,6 +1,15 @@
 //* https://www.codewars.com/kata/58ee4db3e479611e6f000086/train/typescript
 
-export function bitMarch (n: number): number[][] {
-  
-    return [];
-  }
+export function bitMarch(n: number): number[][] {
+	let result: number[][] = [];
+	const numBits = 8;
+	let initBits = Array(numBits)
+		.fill(0)
+		.map((_, i) => (i < numBits - n ? 0 : 1));
+	result.push(initBits);
+	while (initBits[0] === 0) {
+		initBits = initBits.slice(1).concat([0]);
+		result.push(initBits);
+	}
+	return result;
+}
