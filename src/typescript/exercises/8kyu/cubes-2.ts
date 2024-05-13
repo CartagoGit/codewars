558c85d2332418be5ac44c0a325a9f1becd59c7b
Cartago <cartago.relaxingcup@gmail.com>
2024-05-13 08:26:14 +0200

    Realizamos la kata

--- src/typescript/exercises/8kyu/cubes-2.ts
+++ src/typescript/exercises/8kyu/cubes-2.ts
@@ -3,6 +3,10 @@
 export class Cube {
   
     private _side: number;
+
+    constructor (side?: number) {
+        this._side = Math.abs(side || 0);
+    }
   
     public getSide(): number {
       return this._side;
