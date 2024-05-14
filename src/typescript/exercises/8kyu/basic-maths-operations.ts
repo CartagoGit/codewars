2922c98514dc59065393a64e9faa4ac4a951795b
Cartago <cartago.relaxingcup@gmail.com>
2024-05-14 07:37:29 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/8kyu/basic-maths-operations.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/typescript
+
+export function basicOp(operation: string, value1: number, value2: number): number {
+    // Good luck!
+  }
