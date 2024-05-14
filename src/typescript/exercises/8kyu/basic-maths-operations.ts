b0b2db7e1b16569c08cd9105d7c57aec0882f14d
Cartago <cartago.relaxingcup@gmail.com>
2024-05-14 07:39:53 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/basic-maths-operations.ts
+++ src/typescript/exercises/8kyu/basic-maths-operations.ts
@@ -1,5 +1,15 @@
 //* https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/typescript
 
-export function basicOp(operation: string, value1: number, value2: number): number {
-    // Good luck!
-  }
+export function basicOp(
+	operation: string,
+	value1: number,
+	value2: number
+): number {
+	const operations: Record<string, (a: number, b: number) => number> = {
+		'+': (a: number, b: number) => a + b,
+		'-': (a: number, b: number) => a - b,
+		'*': (a: number, b: number) => a * b,
+		'/': (a: number, b: number) => a / b,
+	};
+	return operations[operation](value1, value2);
+}
