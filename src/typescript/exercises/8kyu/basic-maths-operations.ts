f6ad2b790e6c0596ed04a2c11df3fa8275a893e1
Cartago <cartago.relaxingcup@gmail.com>
2024-05-14 07:42:23 +0200

    Refac de la kata

--- src/typescript/exercises/8kyu/basic-maths-operations.ts
+++ src/typescript/exercises/8kyu/basic-maths-operations.ts
@@ -1,15 +1,11 @@
 //* https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/typescript
 
-export function basicOp(
-	operation: string,
-	value1: number,
-	value2: number
-): number {
-	const operations: Record<string, (a: number, b: number) => number> = {
-		'+': (a: number, b: number) => a + b,
-		'-': (a: number, b: number) => a - b,
-		'*': (a: number, b: number) => a * b,
-		'/': (a: number, b: number) => a / b,
+export function basicOp(operation: string, a: number, b: number): number {
+	const operations: Record<string, number> = {
+		'+': a + b,
+		'-': a - b,
+		'*': a * b,
+		'/': a / b,
 	};
-	return operations[operation](value1, value2);
+	return operations[operation];
 }
