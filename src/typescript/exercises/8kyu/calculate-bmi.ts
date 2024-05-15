bd1de48e827f389d8353a5be38ae3eed8ef88c43
Cartago <cartago.relaxingcup@gmail.com>
2024-05-15 07:37:54 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/8kyu/calculate-bmi.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/57a429e253ba3381850000fb/train/typescript
+
+export function bmi(weight: number, height: number): string {
+    // TODO: implement this method
+    throw new Error("The method or operation is not implemented.");
+  }
