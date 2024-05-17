a75be4100031e98caaf69bc3464ea598dbe7007d
Cartago <cartago.relaxingcup@gmail.com>
2024-05-18 00:05:01 +0200

    Realizamos la kata

--- src/typescript/exercises/7kyu/single-digit.ts
+++ src/typescript/exercises/7kyu/single-digit.ts
@@ -1,5 +1,12 @@
 //* https://www.codewars.com/kata/5a7778790136a132a00000c1/train/typescript
 
 export function singleDigit(n: number): number {
-    //your code here
-  }
+	while (n.toString().length > 1) {
+		const binary = n.toString(2);
+		n = binary
+			.toString()
+			.split('')
+			.reduce((acc, cur) => acc + parseInt(cur), 0);
+	}
+	return n;
+}
