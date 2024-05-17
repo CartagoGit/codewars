700800a274deb9a71ae9a7fe52b107bf6e056aed
Cartago <cartago.relaxingcup@gmail.com>
2024-05-17 07:27:29 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/search-for-letters.ts
+++ src/typescript/exercises/7kyu/search-for-letters.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/52dbae61ca039685460001ae/train/typescript
 
-export function change(string: string): string{
-    // ..
-  }
+export function change(string: string): string {
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	let resultList = [];
+	for (let i = 0; i < alphabet.length; i++) {
+		if (string.toLowerCase().includes(alphabet[i])) resultList.push('1');
+		else resultList.push('0');
+	}
+    return resultList.join('');
+}
