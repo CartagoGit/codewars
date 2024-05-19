2943a826dbe0cfc797ba608c295caacb7db65058
Cartago <cartago.relaxingcup@gmail.com>
2024-05-19 21:47:04 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/going-cinema.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/562f91ff6a8b77dfe900006e/train/typescript
+
+export function movie(card: number, ticket: number, perc: number): number {
+    // your code
+    return -1
+  }
