73802f3be10f6d2560afa8d3f2316417d57b233b
Cartago <cartago.relaxingcup@gmail.com>
2024-05-19 22:11:49 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/going-cinema.ts
+++ src/typescript/exercises/7kyu/going-cinema.ts
@@ -1,6 +1,15 @@
 //* https://www.codewars.com/kata/562f91ff6a8b77dfe900006e/train/typescript
 
 export function movie(card: number, ticket: number, perc: number): number {
-    // your code
-    return -1
-  }
+	let priceTicketWithCard = ticket * perc;
+	let totalWithCard = card + priceTicketWithCard;
+	let totalWithoutCard = ticket;
+	let times = 1;
+	while (Math.ceil(totalWithCard) >= totalWithoutCard) {
+        times++;
+		priceTicketWithCard *= perc;
+		totalWithCard += priceTicketWithCard;
+		totalWithoutCard += ticket;
+	}
+	return times;
+}
