85528e676e0c20edd26581e2a0420c1f3060da77
Cartago <cartago.relaxingcup@gmail.com>
2024-05-21 21:34:48 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/green-is-my-valley.ts
+++ src/typescript/exercises/7kyu/green-is-my-valley.ts
@@ -4,12 +4,11 @@ export function makeValley(arr: number[]): number[] {
 	let leftWing: number[] = [];
 	let rightWing: number[] = [];
 	const sortArray = [...arr].sort((a, b) => a - b);
-	const isOdd = arr.length % 2 !== 0;
-	for (let i = 0; i < sortArray.length; i++) {
-        if(i === 0 && isOdd) leftWing.push(sortArray[i]);
-		else if (i % 2 === 0) leftWing.unshift(sortArray[i]);
-		else rightWing.push(sortArray[i]);
-        console.log({i, isOdd, leftWing, rightWing})
+	let isTurnLeft = arr.length % 2 !== 0;
+	for (let num of sortArray) {
+		if (isTurnLeft) leftWing.unshift(num);
+		else rightWing.push(num);
+		isTurnLeft = !isTurnLeft;
 	}
 	return [...leftWing, ...rightWing];
 }
