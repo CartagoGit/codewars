40a1fdd6dfd39a7fb1abb2b2655ddd9d2452a6a8
Cartago <cartago.relaxingcup@gmail.com>
2024-05-22 07:22:24 +0200

    Resolvemos la kata

--- src/typescript/exercises/8kyu/whether-shjape-is-cube.ts
+++ src/typescript/exercises/8kyu/whether-shjape-is-cube.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/58d248c7012397a81800005c/train/typescript
 
 export function cubeChecker(volume: number, side: number): boolean {
-	return true;
+	return volume / side ** 2 === side;
 }
