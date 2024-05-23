8988a2057c4fa50374cb3a5f4f030cd8856cf500
Cartago <cartago.relaxingcup@gmail.com>
2024-05-23 08:21:52 +0200

    Avanzamos en la kata

--- src/typescript/exercises/7kyu/ga-de-ry-po-lu-ki.ts
+++ src/typescript/exercises/7kyu/ga-de-ry-po-lu-ki.ts
@@ -1,18 +1,18 @@
 //* https://www.codewars.com/kata/592a6ad46d6c5a62b600003f/train/typescript
-const cipher = {
-    g: "a",
-    d: "e",
-    r: "y",
-    p: "o",
-    l: "u",
-    k: "i",
+const baseCipher: Record<string, string> = {
+	g: 'a',
+	d: 'e',
+	r: 'y',
+	p: 'o',
+	l: 'u',
+	k: 'i',
+};
+const inverseCipher: Record<string, string> = {};
+for (let key in baseCipher) {
+	inverseCipher[baseCipher[key]] = key;
 }
+const cipher = { ...baseCipher, ...inverseCipher };
 
-export function encode(str: string): string {
-    
-	return 'noidea';
-}
+export const encode = (str: string): string=> str.split('').map((char: string) => cipher[char] ?? char).join('');
 
-export function decode(str: string): string {
-	return 'noidea';
-}
+export const decode = encode;
