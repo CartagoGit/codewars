cf419f266b50a9fc708a3c47e1db6800ae9e9d5e
Cartago <cartago.relaxingcup@gmail.com>
2024-05-23 08:07:01 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/ga-de-ry-po-lu-ki.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/592a6ad46d6c5a62b600003f/train/typescript
+
+export function encode ( str:string):string {
+    return 'noidea';
+ }
+ 
+ export function  decode ( str:string):string { 
+    return 'noidea';
+ }
