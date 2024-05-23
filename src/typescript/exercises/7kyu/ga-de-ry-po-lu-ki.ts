e902f90e522002f677c520d394c80631796446d3
Cartago <cartago.relaxingcup@gmail.com>
2024-05-23 08:14:30 +0200

    Continuamos la kata

--- src/typescript/exercises/7kyu/ga-de-ry-po-lu-ki.ts
+++ src/typescript/exercises/7kyu/ga-de-ry-po-lu-ki.ts
@@ -1,9 +1,18 @@
 //* https://www.codewars.com/kata/592a6ad46d6c5a62b600003f/train/typescript
+const cipher = {
+    g: "a",
+    d: "e",
+    r: "y",
+    p: "o",
+    l: "u",
+    k: "i",
+}
 
-export function encode ( str:string):string {
-    return 'noidea';
- }
- 
- export function  decode ( str:string):string { 
-    return 'noidea';
- }
+export function encode(str: string): string {
+    
+	return 'noidea';
+}
+
+export function decode(str: string): string {
+	return 'noidea';
+}
