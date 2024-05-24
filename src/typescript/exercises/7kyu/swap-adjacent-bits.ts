4cd3a6d457bd8a0e3b51f701fb88fdb9042b3078
Cartago <cartago.relaxingcup@gmail.com>
2024-05-24 07:27:11 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/swap-adjacent-bits.ts
+++ src/typescript/exercises/7kyu/swap-adjacent-bits.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/58845a92bd573378f4000035/train/typescript
 
 export function swapAdjacentBits(n: number): number {
-    return 0;
-  }
+	let binaryString = n.toString(2).padStart(32, '0');
+	for (let i = 0; i < binaryString.length - 1; i += 2) {
+		const temp = binaryString[i];
+		binaryString =
+			binaryString.slice(0, i) +
+			binaryString[i + 1] +
+			temp +
+			binaryString.slice(i + 2);
+	}
+	return parseInt(binaryString, 2);
+}
