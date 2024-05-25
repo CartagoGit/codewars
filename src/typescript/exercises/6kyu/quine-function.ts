c4a713f54f44f6f588fe4ea6bb63113d4b4bb1ce
Cartago <cartago.relaxingcup@gmail.com>
2024-05-25 13:13:33 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/quine-function.ts
+++ src/typescript/exercises/6kyu/quine-function.ts
@@ -1,3 +1,4 @@
 //* https://www.codewars.com/kata/61c49a1045dba2004e7acd1f/train/typescript
+declare const exports: any;
 
-export const quine = (): string => { return ""; }
+export const quine = () => { return "export const quine = " + exports.quine.toString(); }
