7ae05dd1f063a35a890966edfac4ecd365488016
Cartago <cartago.relaxingcup@gmail.com>
2024-05-26 13:03:20 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/welcome-to-the-city.ts
+++ src/typescript/exercises/8kyu/welcome-to-the-city.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/typescript
 
-export const sayHello = (name: string[], city: string, state: string):string => {
-    return '';
-  }
+export const sayHello = (
+	name: string[],
+	city: string,
+	state: string
+): string => {
+	return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+};
