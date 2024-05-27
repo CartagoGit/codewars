28b33d14a0e97cdcd6f541b224bf1cfd1527dace
Cartago <cartago.relaxingcup@gmail.com>
2024-05-27 20:14:59 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/draw-stairs.ts
+++ src/typescript/exercises/8kyu/draw-stairs.ts
@@ -1,6 +1,5 @@
 //* https://www.codewars.com/kata/5b4e779c578c6a898e0005c5/train/typescript
 
 export function drawStairs(n: number): string {
-	// your code here
-	return '';
+	return Array.from({ length: n }, (_, i) => ' '.repeat(i) + 'I').join('\n');
 }
