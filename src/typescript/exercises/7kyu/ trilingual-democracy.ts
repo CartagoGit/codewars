e95040c7391f065ca9191abaf98b1909450d1256
Cartago <cartago.relaxingcup@gmail.com>
2024-05-29 21:35:35 +0200

    Preparamos el ejercicio

---
+++ src/typescript/exercises/7kyu/ trilingual-democracy.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/62f17be8356b63006a9899dc/train/typescript
+
+// input is a string of three chars from the set 'D', 'F', 'I', 'K'
+// output is a single char from this set
+export function trilingualDemocracy(group: string): string {
+    // implement the rules...
+    return "?";
+  }
+  
--- src/typescript/exercises/7kyu/sort-array-value-index.ts
+++ src/typescript/exercises/7kyu/sort-array-value-index.ts
@@ -1,10 +1,6 @@
 //* https://www.codewars.com/kata/58e0cb3634a3027180000040/train/typescript
 
 export function sortByValueAndIndex(array: number[]): number[] {
-    let map : Record<number, {prod: number, value: number}> = {};
-    for(let [index, value] of array.entries()){
-        map[index] = {prod: value * (index + 1), value};
-    }
-    let result : number[] = Object.
-	return Object.entries(map).
+	let tuple = array.map((value, index) => [value, value * (index + 1)]);
+	return tuple.sort((a, b) => a[1] - b[1]).map((tuple) => tuple[0]);
 }
