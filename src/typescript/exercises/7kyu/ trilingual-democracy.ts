c3bbc89abc7e040b5065eab550f05b3ad4f9393e
Cartago <cartago.relaxingcup@gmail.com>
2024-05-29 21:50:42 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/ trilingual-democracy.ts
+++ src/typescript/exercises/7kyu/ trilingual-democracy.ts
@@ -2,8 +2,17 @@
 
 // input is a string of three chars from the set 'D', 'F', 'I', 'K'
 // output is a single char from this set
+
+const langs = ['D', 'F', 'I', 'K'];
+
 export function trilingualDemocracy(group: string): string {
-    // implement the rules...
-    return "?";
-  }
-  
+	let langCount: Record<string, number> = {};
+	for (let speaker of group.split('')) {
+		langCount[speaker] = (langCount[speaker] || 0) + 1;
+	}
+	const quantity = Object.values(langCount);
+	if (quantity.length === 1) return group[0];
+	if (quantity.length === 2)
+		return Object.keys(langCount).find((key) => langCount[key] === 1)!;
+	return langs.find((lang) => !langCount[lang])!;
+}
