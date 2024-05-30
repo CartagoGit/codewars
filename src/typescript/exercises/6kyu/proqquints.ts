dcb4d4dfc313beeba28f3956f2ff2f0a8e636514
Cartago <cartago.relaxingcup@gmail.com>
2024-05-30 08:11:18 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/proqquints.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/643462af501b65000f2ce9d7/train/typescript
+
+export function fromProquint(proquint: string): number {
+    return 0  
+  }
+  
+  export function toProquint(number: number): string {
+    return 'babab-babab'
+  }
