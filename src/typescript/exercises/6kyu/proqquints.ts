2840f8f5e6511ec150cbaf806388533af874c88d
Cartago <cartago.relaxingcup@gmail.com>
2024-05-30 08:26:36 +0200

    Enfocamos la primera funcion

--- src/typescript/exercises/6kyu/proqquints.ts
+++ src/typescript/exercises/6kyu/proqquints.ts
@@ -1,7 +1,19 @@
 //* https://www.codewars.com/kata/643462af501b65000f2ce9d7/train/typescript
+const vowels = "aiou";
+const consonants = "bdfghjklmnprstvz";
+
 
 export function fromProquint(proquint: string): number {
-    return 0  
+    const chain = [...proquint.split('-').join('')]
+    const bits  = chain.map((char) => {
+        const isVowel = vowels.includes(char.toLowerCase());
+        const kindChain = isVowel ? vowels : consonants;
+        let index = kindChain.indexOf(char.toLowerCase());
+        if(index > 9) index -= 10;
+        return index.toString(2).padStart(isVowel ? 2 : 4, '0');
+    })
+    console.log(bits)
+    return parseInt(bits.join(''), 2);
   }
   
   export function toProquint(number: number): string {
