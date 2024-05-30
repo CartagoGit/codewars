55743dcac38a6b78a81113f2815f25a18232997b
Cartago <cartago.relaxingcup@gmail.com>
2024-05-30 08:50:07 +0200

    Solucionamos la primera parte de la kata

--- src/typescript/exercises/6kyu/proqquints.ts
+++ src/typescript/exercises/6kyu/proqquints.ts
@@ -8,11 +8,9 @@ export function fromProquint(proquint: string): number {
 		const isVowel = vowels.includes(char.toLowerCase());
 		const kindChain = isVowel ? vowels : consonants;
 		let index = kindChain.indexOf(char.toLowerCase());
-		if (index > 9) index -= 10;
 		return index.toString(2).padStart(isVowel ? 2 : 4, '0');
 	});
-	console.log(bits);
-	return parseInt(bits.join(''));
+	return parseInt(bits.join(''), 2);
 }
 
 export function toProquint(number: number): string {
