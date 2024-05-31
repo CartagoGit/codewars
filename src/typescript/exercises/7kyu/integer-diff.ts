d8950eaa519f345e604bfd3af552bfe373fab24f
Cartago <cartago.relaxingcup@gmail.com>
2024-05-31 07:24:39 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/integer-diff.ts
+++ src/typescript/exercises/7kyu/integer-diff.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/57741d8f10a0a66915000001/train/typescript
 
 export const intDiff = (arr: number[], n: number): number => {
-	// your code goes here
+	arr.sort((a, b) => a - b);
+	let counter = 0;
+	for (const [index, elem] of arr.entries()) {
+		const restArr = arr.slice(index + 1);
+		if (restArr.includes(elem + n)) {
+			counter += restArr.filter((el) => el === elem + n).length;
+		}
+	}
+	return counter;
 };
