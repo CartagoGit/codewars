7e94877aaf17a7e58aa48423cf11a25d943dfa1f
Cartago <cartago.relaxingcup@gmail.com>
2024-06-01 14:33:36 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/paperfold-seq.ts
+++ src/typescript/exercises/6kyu/paperfold-seq.ts
@@ -1,5 +1,17 @@
 //* https://www.codewars.com/kata/5d26721d48430e0016914faa/train/typescript
 
 export function* paperFold(): Generator<number, any, undefined> {
-	yield 1;
+	let current = '1';
+	let counter = 0;
+	while (true) {
+		while (counter < current.length) {
+			yield parseInt(current[counter]);
+			counter++;
+		}
+		current = `${current}1${current
+			.split('')
+			.map((c) => (c === '1' ? '0' : '1'))
+			.reverse()
+			.join('')}`;
+	}
 }
