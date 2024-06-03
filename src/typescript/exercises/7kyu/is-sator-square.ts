28fa0b6116b5991bd55060142cf946ebd4838fa4
Cartago <cartago.relaxingcup@gmail.com>
2024-06-03 21:18:23 +0200

    Continuamos con la kata

--- src/typescript/exercises/7kyu/is-sator-square.ts
+++ src/typescript/exercises/7kyu/is-sator-square.ts
@@ -1,32 +1,48 @@
 //* https://www.codewars.com/kata/5cb7baa989b1c50014a53333/train/typescript
 
 export function isSatorSquare(tablet: string[][]): boolean {
-	const n = tablet.length;
+	console.log(tablet);
+	if (
+		tablet.length !== tablet[0].length ||
+		tablet.some((row) => row.length !== tablet.length)
+	)
+		return false;
+	if (tablet.some((row) => row.some((cell) => !cell))) return false;
 
-	// Verificar que el tamaño del cuadrado sea válido
-	if (n < 2 || n > 33) return false;
-
-	// Función auxiliar para verificar si una palabra puede ser leída en todas las direcciones
-	const isValidWord = (word: string): boolean => {
-		const reversedWord = word.split('').reverse().join('');
-		return (
-			word === reversedWord &&
-			word === word.split('').reverse().join('') &&
-			word ===
-				word
-					.split('\n')
-					.map((row) => row.split('').reverse().join(''))
-					.join('\n')
+	const permutedArray = getPermutedArray(tablet);
+	const premutedChain = permutedArray.map((row) => row.join(''));
+	const permutedReverseChain = permutedArray.map((row) =>
+		row.reverse().join('')
+	);
+	for (let [row, horizontal] of tablet.entries()) {
+		const restTablet = [...tablet.slice(0, row), ...tablet.slice(row + 1)];
+		const restReverseHorizontalChain = restTablet.map((row) =>
+			row.reverse().join('')
 		);
-	};
-
-	// Iterar sobre cada fila y columna para verificar si todas las palabras son válidas
-	for (let i = 0; i < n; i++) {
-		for (let j = 0; j < n; j++) {
-			const word = tablet[i].slice(j, j + n).join('');
-			if (!isValidWord(word)) return false;
-		}
+		const horizontalChain = horizontal.join('');
+		const hasHorizontalReverse =
+			restReverseHorizontalChain.includes(horizontalChain);
+		const hasVertical = premutedChain.includes(horizontalChain);
+		const hasVerticalReverse =
+			permutedReverseChain.includes(horizontalChain);
+		console.log(
+			hasHorizontalReverse,
+			hasVertical,
+			hasVerticalReverse,
+			restTablet, {permutedArray}
+		);
+		if (!hasHorizontalReverse || !hasVertical || !hasVerticalReverse)
+			return false;
 	}
-
 	return true;
 }
+
+const getPermutedArray = (tablet: string[][]) => {
+	let permutedArray: string[][] = [...tablet];
+	for (let i = 0; i < tablet.length; i++) {
+		for (let j = 0; j < i; j++) {
+			permutedArray[i][j] = tablet[j][i];
+		}
+	}
+	return permutedArray;
+};
