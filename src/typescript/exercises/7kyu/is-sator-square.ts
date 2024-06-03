b5db5c8a6bdeee3ac6225b3c961ec2aeef14bbfe
Cartago <cartago.relaxingcup@gmail.com>
2024-06-03 21:38:25 +0200

    Deberia estar solucionada pero creo que hay un error en los tests de la kata

--- src/typescript/exercises/7kyu/is-sator-square.ts
+++ src/typescript/exercises/7kyu/is-sator-square.ts
@@ -1,7 +1,6 @@
 //* https://www.codewars.com/kata/5cb7baa989b1c50014a53333/train/typescript
 
 export function isSatorSquare(tablet: string[][]): boolean {
-	console.log(tablet);
 	if (
 		tablet.length !== tablet[0].length ||
 		tablet.some((row) => row.length !== tablet.length)
@@ -10,27 +9,20 @@ export function isSatorSquare(tablet: string[][]): boolean {
 	if (tablet.some((row) => row.some((cell) => !cell))) return false;
 
 	const permutedArray = getPermutedArray(tablet);
-	const premutedChain = permutedArray.map((row) => row.join(''));
+	const permutedChain = permutedArray.map((row) => [...row].join(''));
 	const permutedReverseChain = permutedArray.map((row) =>
-		row.reverse().join('')
+		[...row].reverse().join('')
 	);
 	for (let [row, horizontal] of tablet.entries()) {
-		const restTablet = [...tablet.slice(0, row), ...tablet.slice(row + 1)];
-		const restReverseHorizontalChain = restTablet.map((row) =>
-			row.reverse().join('')
+		const restReverseHorizontalChain = tablet.map((row) =>
+			[...row].reverse().join('')
 		);
 		const horizontalChain = horizontal.join('');
 		const hasHorizontalReverse =
 			restReverseHorizontalChain.includes(horizontalChain);
-		const hasVertical = premutedChain.includes(horizontalChain);
+		const hasVertical = permutedChain.includes(horizontalChain);
 		const hasVerticalReverse =
 			permutedReverseChain.includes(horizontalChain);
-		console.log(
-			hasHorizontalReverse,
-			hasVertical,
-			hasVerticalReverse,
-			restTablet, {permutedArray}
-		);
 		if (!hasHorizontalReverse || !hasVertical || !hasVerticalReverse)
 			return false;
 	}
@@ -40,7 +32,7 @@ export function isSatorSquare(tablet: string[][]): boolean {
 const getPermutedArray = (tablet: string[][]) => {
 	let permutedArray: string[][] = [...tablet];
 	for (let i = 0; i < tablet.length; i++) {
-		for (let j = 0; j < i; j++) {
+		for (let j = 0; j < tablet.length; j++) {
 			permutedArray[i][j] = tablet[j][i];
 		}
 	}
