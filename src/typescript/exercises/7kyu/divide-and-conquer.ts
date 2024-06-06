7f2fe2fb951728b6a0b14d0198493d304baf53f7
Cartago <cartago.relaxingcup@gmail.com>
2024-06-06 08:33:56 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/divide-and-conquer.ts
+++ src/typescript/exercises/7kyu/divide-and-conquer.ts
@@ -1,6 +1,13 @@
 //* https://www.codewars.com/kata/57eaec5608fed543d6000021/train/typescript
 
-export function divCon(x: `${number}` | number): number {
-
+export function divCon(x: (string | number)[]): number {
+	const [notChainNums, chainNums] = x.reduce(
+		(acc, num) => {
+			if (typeof num === 'string') acc[0] += parseInt(num);
+			else acc[1] += num;
+			return acc;
+		},
+		[0, 0]
+	);
+	return chainNums - notChainNums;
 }
-
