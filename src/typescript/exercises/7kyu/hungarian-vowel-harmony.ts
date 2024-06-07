7b30f58ea0cbf674fdd7965017bbd1e66eb76700
Cartago <cartago.relaxingcup@gmail.com>
2024-06-07 07:33:30 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/hungarian-vowel-harmony.ts
+++ src/typescript/exercises/7kyu/hungarian-vowel-harmony.ts
@@ -1,6 +1,16 @@
 //* https://www.codewars.com/kata/57fd696e26b06857eb0011e7/train/typescript
 
-export function dative (word: string): string | undefined {
-    // Implement me! :)
-  }
-  
+const frontVowels = ['e', 'é', 'i', 'í', 'ö', 'ő', 'ü', 'ű'];
+const backVowels = ['a', 'á', 'o', 'ó', 'u', 'ú'];
+
+export function dative(word: string): string | undefined {
+	const lastVowel = word
+		.split('')
+		.reverse()
+		.find(
+			(letter) =>
+				frontVowels.includes(letter) || backVowels.includes(letter)
+		);
+	if (!lastVowel) return undefined;
+	return frontVowels.includes(lastVowel) ? `${word}nek` : `${word}nak`;
+}
