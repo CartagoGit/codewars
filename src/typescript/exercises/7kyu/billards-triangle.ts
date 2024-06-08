edb0d1dae5769b44be7d10134055bae58e6a72ac
Cartago <cartago.relaxingcup@gmail.com>
2024-06-08 17:56:13 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/billards-triangle.ts
+++ src/typescript/exercises/7kyu/billards-triangle.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/5bb3e299484fcd5dbb002912/train/typescript
 
 export function pyramid(balls: number): number {
-    return 0
+    let neededBalls = 1;
+    let level = 1;
+    while (balls >= neededBalls) {
+      level++;
+      neededBalls += level;
+    }
+    return level - 1
   }
