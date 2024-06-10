a8e773c83a8ba3a1ca31e5112fcad46cff2d8084
Cartago <cartago.relaxingcup@gmail.com>
2024-06-10 07:34:20 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/growth-population.ts
@@ -0,0 +1,10 @@
+//* https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/typescript
+
+export const nbYear = (
+	p0: number,
+	percent: number,
+	aug: number,
+	p: number
+): number => {
+	// your code
+};
