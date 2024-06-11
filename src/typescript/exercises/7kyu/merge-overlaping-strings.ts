05847ed6d971edf1638fa9ffb7758b5ed4099767
Cartago <cartago.relaxingcup@gmail.com>
2024-06-11 07:42:01 +0200

    Avanzamos en la kata

--- src/typescript/exercises/7kyu/merge-overlaping-strings.ts
+++ src/typescript/exercises/7kyu/merge-overlaping-strings.ts
@@ -1,6 +1,12 @@
 //* https://www.codewars.com/kata/61c78b57ee4be50035d28d42/train/typescript
 
 export const mergeStrings = (first: string, second: string): string => {
-	// Do your thing
-	return '';
+	let result = '';
+	let secondIndex = 0;
+	for (let i = 0; i < first.length; i++) {
+		if (first[i] === second[secondIndex]) secondIndex++;
+		else secondIndex = 0;
+	}
+    result = first + second.slice(secondIndex);
+    return result;
 };
