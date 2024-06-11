bf0f1d6dc27e2a125469ed7d392e3cd71be06c2e
Cartago <cartago.relaxingcup@gmail.com>
2024-06-11 07:27:06 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/merge-overlaping-strings.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/61c78b57ee4be50035d28d42/train/typescript
+
+export const mergeStrings = (first: string, second: string): string => {
+	// Do your thing
+	return '';
+};
