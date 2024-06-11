e766232c674cb17d0585a9fcc477cea87f302c66
Cartago <cartago.relaxingcup@gmail.com>
2024-06-11 07:46:45 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/merge-overlaping-strings.ts
+++ src/typescript/exercises/7kyu/merge-overlaping-strings.ts
@@ -5,6 +5,7 @@ export const mergeStrings = (first: string, second: string): string => {
 	let secondIndex = 0;
 	for (let i = 0; i < first.length; i++) {
 		if (first[i] === second[secondIndex]) secondIndex++;
+        else if(first[i] === second[0]) secondIndex = 1;
 		else secondIndex = 0;
 	}
     result = first + second.slice(secondIndex);
