d71a479bbed7032105c89c65800a622094feb55e
Cartago <cartago.relaxingcup@gmail.com>
2024-06-11 07:47:47 +0200

    Refactor

--- src/typescript/exercises/7kyu/merge-overlaping-strings.ts
+++ src/typescript/exercises/7kyu/merge-overlaping-strings.ts
@@ -1,13 +1,11 @@
 //* https://www.codewars.com/kata/61c78b57ee4be50035d28d42/train/typescript
 
 export const mergeStrings = (first: string, second: string): string => {
-	let result = '';
 	let secondIndex = 0;
 	for (let i = 0; i < first.length; i++) {
 		if (first[i] === second[secondIndex]) secondIndex++;
-        else if(first[i] === second[0]) secondIndex = 1;
+		else if (first[i] === second[0]) secondIndex = 1;
 		else secondIndex = 0;
 	}
-    result = first + second.slice(secondIndex);
-    return result;
+	return first + second.slice(secondIndex);
 };
