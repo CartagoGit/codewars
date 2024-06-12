98409941c5c3485d7fe1f8457715e1aa0c767faf
Cartago <cartago.relaxingcup@gmail.com>
2024-06-12 07:33:48 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/near-square.ts
+++ src/typescript/exercises/8kyu/near-square.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/5a805d8cafa10f8b930005ba/train/typescript
 
-export function nearestSq(n : number) : number {
-    return 0 // your code here
-  }
+export function nearestSq(n: number): number {
+	const sqrt = Math.floor(Math.sqrt(n));
+	
+}
