5d116e9b3eff646a7ca04fe18189b65770d2affd
Cartago <cartago.relaxingcup@gmail.com>
2024-06-13 08:22:29 +0200

    Completamos la kata

--- src/typescript/exercises/8kyu/binary-adition.ts
+++ src/typescript/exercises/8kyu/binary-adition.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/57642a90dee2da8dd3000161/train/typescript
 
 export function arr2bin(arr: any[]): string {
-    // show me the code
-  }
+	return arr
+		.reduce((acc, curr) => acc + (typeof curr === 'number' ? curr : 0), 0)
+		.toString(2);
+}
