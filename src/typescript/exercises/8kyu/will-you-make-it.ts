9f0131ea97cc023dee3ac716409a69bd07ab6d92
Cartago <cartago.relaxingcup@gmail.com>
2024-06-14 07:28:26 +0200

    Preparamos kata

---
+++ src/typescript/exercises/8kyu/will-you-make-it.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/5861d28f124b35723e00005e/train/typescript
+
+export const zeroFuel = (
+	distance: number,
+	mpg: number,
+	fuelLeft: number
+): boolean => {
+	throw new Error('Not implemented');
+};
