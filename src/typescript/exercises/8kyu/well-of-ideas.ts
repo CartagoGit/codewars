612c5094f6323c9aa2dc65e5d86e912f67a4027a
Cartago <cartago.relaxingcup@gmail.com>
2024-06-15 23:46:03 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/well-of-ideas.ts
+++ src/typescript/exercises/8kyu/well-of-ideas.ts
@@ -1,5 +1,10 @@
 //*  https://www.codewars.com/kata/57f222ce69e09c3630000212/train/typescript
 
-export function well(x: string[]): string{
-    return ""
-  }
+export function well(x: string[]): string {
+	const timesGood = x.filter((chain) => chain === 'good').length;
+	return timesGood > 2
+		? 'I smell a series!'
+		: timesGood === 0
+		? 'Fail!'
+		: 'Publish!';
+}
