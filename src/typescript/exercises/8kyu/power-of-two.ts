52e2b3560c4f7d2e033ec1c65a3e5772df49559a
Cartago <cartago.relaxingcup@gmail.com>
2024-06-16 00:25:46 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/power-of-two.ts
+++ src/typescript/exercises/8kyu/power-of-two.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/57a083a57cb1f31db7000028/train/typescript
 
 export function powersOfTwo(n: number): number[] {
-	return [];
+	return Array.from({ length: n + 1 }, (_, i) => 2 ** i);
 }
