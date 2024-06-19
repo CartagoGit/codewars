7f6fe94152858ce21dc464ad704036969f4e2d2b
Cartago <cartago.relaxingcup@gmail.com>
2024-06-19 07:29:27 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/parity-bit.ts
+++ src/typescript/exercises/7kyu/parity-bit.ts
@@ -1,6 +1,12 @@
 //* https://www.codewars.com/kata/5df261342964c80028345a0a/train/typescript
 
-export function checkParity(parity: string, bin: string) : number {
-  // have fun!
-  return 0;
+export function checkParity(parity: string, bin: string): number {
+	let onesCount = bin.split('').filter((bit) => bit === '1').length;
+	if (parity === 'even') {
+		// Si se desea paridad par y hay un número impar de unos, se agrega un uno
+		return onesCount % 2 !== 0 ? 1 : 0;
+	} else {
+		// Si se desea paridad impar y hay un número par de unos, se agrega un uno
+		return onesCount % 2 === 0 ? 1 : 0;
+	}
 }
