681d4ba5a05b0bc532e0a99ee719f39414cbb545
Cartago <cartago.relaxingcup@gmail.com>
2024-06-20 09:06:56 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/all-inclusive.ts
+++ src/typescript/exercises/7kyu/all-inclusive.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/5700c9acc1555755be00027e/train/typescript
 
 export function containAllRots(str: string, arr: string[]): boolean {
-	// your code
+    if(str === '') return true;
+	for(let i = 0; i < str.length; i++) {
+        if(!arr.includes(str.slice(i) + str.slice(0, i))) return false;
+    }
+    return true;
 }
