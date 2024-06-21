a2fb4b7d189259e24b9ed0a9ad1333795ca9383a
Cartago <cartago.relaxingcup@gmail.com>
2024-06-21 07:30:02 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/odd-ones-out.ts
+++ src/typescript/exercises/7kyu/odd-ones-out.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5d376cdc9bcee7001fcb84c0/train/typescript
 
 export function oddOnesOut(nums: number[]) {
-	// your code here
+	let counter: Record<number, number> = {};
+	for (let num of nums) {
+		if (counter[num]) counter[num]++;
+		else counter[num] = 1;
+	}
+	return nums.filter((num) => counter[num] % 2 === 0);
 }
