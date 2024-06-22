63cf157b3ba24c1f0d19a92cfd9f858a96c1c9e6
Cartago <cartago.relaxingcup@gmail.com>
2024-06-22 13:26:34 +0200

    Terminamos la kata

--- src/typescript/exercises/8kyu/check-same-case.ts
+++ src/typescript/exercises/8kyu/check-same-case.ts
@@ -1,5 +1,10 @@
+
 //* https://www.codewars.com/kata/5dd462a573ee6d0014ce715b/train/typescript
 
 export function sameCase(a: string, b: string): number {
-	return 0; // your code here
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const isUpperCase = (char: string) => alphabet.toUpperCase().includes(char);
+    if([a, b].some((char) => !alphabet.includes(char.toLowerCase()))) return -1;
+    else if (isUpperCase(a) === isUpperCase(b)) return 1;
+    else return 0;
 }
