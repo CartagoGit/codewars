fc8b067223396035b51b24014b784fa95fb805c0
Cartago <cartago.relaxingcup@gmail.com>
2024-06-25 08:25:52 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/all-star-code-challenge-23.ts
+++ src/typescript/exercises/7kyu/all-star-code-challenge-23.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/5865cff66b5699883f0001aa/train/typescript
 
 export function toTime(seconds: number): string {
-	return '';
+	const hours = Math.floor(seconds / 3600);
+	const minutes = Math.floor((seconds % 3600) / 60);
+	return `${hours} hour(s) and ${minutes} minute(s)`;
 }
