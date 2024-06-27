b10451942a7addd4456ddf18d4937359fb6531a3
Cartago <cartago.relaxingcup@gmail.com>
2024-06-27 10:41:30 +0200

    Refactor

--- src/typescript/exercises/6kyu/irreducible-sum-rationals.ts
+++ src/typescript/exercises/6kyu/irreducible-sum-rationals.ts
@@ -2,20 +2,17 @@
 
 export function sumFracts(list: number[][]): string | null {
 	if ([...list].flat().length === 0) return null;
-	const lcm = (a: number, b: number) => {
-		return Math.abs(a * b) / gcd(a, b);
-	};
-	const gcd = (a: number, b: number): number => {
-		return b === 0 ? a : gcd(b, a % b);
-	};
-	const sum = list.reduce(
-		(acc, [num, den]) => {
-			const common = acc[1] * den;
-			return [acc[0] * (common / acc[1]) + num * (common / den), common];
+	const gcd = (a: number, b: number): number => (b === 0 ? a : gcd(b, a % b));
+	const [num, den] = list.reduce(
+		([accNum, accDen], [currNum, currDen]) => {
+			const common = accDen * currDen;
+			return [
+				accNum * (common / accDen) + currNum * (common / currDen),
+				common,
+			];
 		},
 		[0, 1]
 	);
-	const [num, den] = sum;
 	const divisor = gcd(num, den);
 	return num % den === 0
 		? (num / den).toString()
