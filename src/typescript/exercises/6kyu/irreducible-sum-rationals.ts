d208fb3941aed926603ffb828274390b784dc8dd
Cartago <cartago.relaxingcup@gmail.com>
2024-06-27 10:37:37 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/irreducible-sum-rationals.ts
+++ src/typescript/exercises/6kyu/irreducible-sum-rationals.ts
@@ -1,6 +1,23 @@
 //* https://www.codewars.com/kata/5517fcb0236c8826940003c9/train/typescript
 
-export function sumFracts(l: number[][]): string | null {
-	// your code
-	return '';
+export function sumFracts(list: number[][]): string | null {
+	if ([...list].flat().length === 0) return null;
+	const lcm = (a: number, b: number) => {
+		return Math.abs(a * b) / gcd(a, b);
+	};
+	const gcd = (a: number, b: number): number => {
+		return b === 0 ? a : gcd(b, a % b);
+	};
+	const sum = list.reduce(
+		(acc, [num, den]) => {
+			const common = acc[1] * den;
+			return [acc[0] * (common / acc[1]) + num * (common / den), common];
+		},
+		[0, 1]
+	);
+	const [num, den] = sum;
+	const divisor = gcd(num, den);
+	return num % den === 0
+		? (num / den).toString()
+		: `[${num / divisor}, ${den / divisor}]`;
 }
