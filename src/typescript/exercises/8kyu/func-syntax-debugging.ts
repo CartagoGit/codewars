8f346efd808fac2c2f7f87f2e4a01c4e7c88e3df
Cartago <cartago.relaxingcup@gmail.com>
2024-06-28 07:28:29 +0200

    Solucionamo la kata

--- src/typescript/exercises/8kyu/func-syntax-debugging.ts
+++ src/typescript/exercises/8kyu/func-syntax-debugging.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/56dae9dc54c0acd29d00109a/train/typescript
 
-export function main [verb: string, noun: string]: string
-  return verb + noun
+export function main(verb: string, noun: string): string {
+	return verb + noun;
 }
