4013b77da6a1bef15af203974f89bea5e8501e3e
Cartago <cartago.relaxingcup@gmail.com>
2024-06-30 15:39:14 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/string-transformer.ts
+++ src/typescript/exercises/6kyu/string-transformer.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/5878520d52628a092f0002d0/train/typescript
 
 export function stringTransformer(str: string) {
-    // Your code here
-  }
+	const reverseWordOrder = str.split(' ').reverse().join(' ');
+	const reverseCase = reverseWordOrder
+		.split('')
+		.map((char) =>
+			char === char.toUpperCase()
+				? char.toLowerCase()
+				: char.toUpperCase()
+		)
+		.join('');
+	return reverseCase;
+}
