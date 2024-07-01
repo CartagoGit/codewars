de173417e63d5d0ca1b8b3c56554cde16c36f902
Cartago <cartago.relaxingcup@gmail.com>
2024-07-01 08:53:04 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/maximum-gap.ts
+++ src/typescript/exercises/7kyu/maximum-gap.ts
@@ -1,6 +1,11 @@
 //* https://www.codewars.com/kata/5a7893ef0025e9eb50000013/train/typescript
 
 export function maxGap(nums: number[]): number {
-    // your code here
-  }
-  
+	let maxDistance = 0;
+    const sortedNums = nums.sort((a, b) => a - b);
+	for (let i = 1; i < sortedNums.length; i++) {
+		const distance = sortedNums[i] - sortedNums[i - 1];
+		if (distance > maxDistance) maxDistance = distance;
+	}
+	return maxDistance;
+}
