ad9ae5770815a0b20c168c5213dc81948dd93661
Cartago <cartago.relaxingcup@gmail.com>
2024-07-03 07:26:49 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/switch-it-up.ts
+++ src/typescript/exercises/8kyu/switch-it-up.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/5808dcb8f0ed42ae34000031/train/typescript
 
 export function switchItUp(intNumber: number): string {
-	return '';
+	const nums = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine']
+    return nums[intNumber];
 }
