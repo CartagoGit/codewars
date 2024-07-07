5e73544a602cee64b88d1f12401a049789920cc1
Cartago <cartago.relaxingcup@gmail.com>
2024-07-07 22:36:26 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/adam-and-eve.ts
+++ src/typescript/exercises/8kyu/adam-and-eve.ts
@@ -4,8 +4,10 @@ export class God{
     /**
      * @returns Human[]
      */
-      static create(){
-        // code
+      static create(): Human[]{
+        return [new Man(), new Woman()]
       }
     }
-    // code
+export class Human {}
+export class Man extends Human{}
+export class Woman extends Human{}
