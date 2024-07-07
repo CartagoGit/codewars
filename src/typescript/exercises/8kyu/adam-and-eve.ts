309f089f0da667d9ef30193b5494085b5601b820
Cartago <cartago.relaxingcup@gmail.com>
2024-07-07 22:33:50 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/8kyu/adam-and-eve.ts
@@ -0,0 +1,11 @@
+//* https://www.codewars.com/kata/547274e24481cfc469000416/train/typescript
+
+export class God{
+    /**
+     * @returns Human[]
+     */
+      static create(){
+        // code
+      }
+    }
+    // code
