40a2cc416bd7a21e3a0aeb5f6702e967ac16575e
Cartago <cartago.relaxingcup@gmail.com>
2024-07-08 07:32:40 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/sabatical.ts
+++ src/typescript/exercises/7kyu/sabatical.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/57fe50d000d05166720000b1/train/typescript
 
-export function sabb(s: string, val: number , happiness: number ): string{  
-    return ""
-  }
+export function sabb(s: string, val: number, happiness: number): string {
+	const sabbatical = 'sabbatical'.split('');
+	const sPoints = s
+		.split('')
+		.filter((char) => sabbatical.includes(char)).length;
+	return sPoints + val + happiness > 22
+		? 'Sabbatical! Boom!'
+		: 'Back to your desk, boy.';
+}
