e2e7760a8e77f37479105e4efc5ec4bfb90fb31c
Cartago <cartago.relaxingcup@gmail.com>
2024-07-09 07:28:19 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/are-smaller-than-me.ts
+++ src/typescript/exercises/7kyu/are-smaller-than-me.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/56a1c074f87bc2201200002e/train/typescript
 
 export function smaller(nums: number[]): number[] {
-    return [];
-  }
+	let result: number[] = [];
+	for (let i = 0; i < nums.length; i++) {
+		const restArr = nums.slice(i + 1);
+		const count = restArr.filter((num) => num < nums[i]).length;
+		result.push(count);
+	}
+	return result;
+}
