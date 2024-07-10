ea267a3806ae8ebf587c482e7ba58f25edb96442
Cartago <cartago.relaxingcup@gmail.com>
2024-07-10 07:41:27 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/maximum-triplet-sum.ts
+++ src/typescript/exercises/7kyu/maximum-triplet-sum.ts
@@ -1,6 +1,7 @@
 //* https://www.codewars.com/kata/5aa1bcda373c2eb596000112/train/typescript
 
-export function maxTriSum(nums: number[]): number  {
-    // your code here
-  }
-  
+export function maxTriSum(nums: number[]): number {
+	const uniqueNums = [...new Set(nums)];
+	const sortedNums = uniqueNums.sort((a, b) => b - a);
+	return sortedNums.slice(0, 3).reduce((acc, num) => acc + num, 0);
+}
