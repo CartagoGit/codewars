27c08679465ad0e62100327b76e4bd3a6c772197
Cartago <cartago.relaxingcup@gmail.com>
2024-07-11 08:37:44 +0200

    Damos solucion a la kata

--- src/typescript/exercises/7kyu/power-of-four.ts
+++ src/typescript/exercises/7kyu/power-of-four.ts
@@ -1,9 +1,7 @@
 //* https://www.codewars.com/kata/544d114f84e41094a9000439/train/typescript
 
 export function powerOf4(n: any): boolean {
-	console.log(n);
-	if (typeof n !== 'number') return false;
+	if (typeof n !== 'number' || n === 0) return false;
 	const sqrt4 = Math.log(n) / Math.log(4);
-	console.log({ sqrt4, log: Math.log(n), log4: Math.log(4) });
-	return sqrt4 === Math.abs(sqrt4);
+	return sqrt4 === Math.floor(sqrt4);
 }
