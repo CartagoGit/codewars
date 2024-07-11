15fa24132b8a346363a787d6afa1bff662a7faf2
Cartago <cartago.relaxingcup@gmail.com>
2024-07-11 08:33:11 +0200

    Seguismo probando la kata

--- src/typescript/exercises/7kyu/power-of-four.ts
+++ src/typescript/exercises/7kyu/power-of-four.ts
@@ -1,6 +1,9 @@
 //* https://www.codewars.com/kata/544d114f84e41094a9000439/train/typescript
 
 export function powerOf4(n: any): boolean {
-  
-	return false;
+	console.log(n);
+	if (typeof n !== 'number') return false;
+	const sqrt4 = Math.log(n) / Math.log(4);
+	console.log({ sqrt4, log: Math.log(n), log4: Math.log(4) });
+	return sqrt4 === Math.abs(sqrt4);
 }
