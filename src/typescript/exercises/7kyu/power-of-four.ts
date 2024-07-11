d1f83ebcb8254587d58750be2a12287ed6899b14
Cartago <cartago.relaxingcup@gmail.com>
2024-07-11 08:39:01 +0200

    Damos una solucion mas elegante

--- src/typescript/exercises/7kyu/power-of-four.ts
+++ src/typescript/exercises/7kyu/power-of-four.ts
@@ -1,7 +1,5 @@
 //* https://www.codewars.com/kata/544d114f84e41094a9000439/train/typescript
 
 export function powerOf4(n: any): boolean {
-	if (typeof n !== 'number' || n === 0) return false;
-	const sqrt4 = Math.log(n) / Math.log(4);
-	return sqrt4 === Math.floor(sqrt4);
+	return typeof n === 'number' && Number.isInteger(Math.log(n) / Math.log(4));
 }
