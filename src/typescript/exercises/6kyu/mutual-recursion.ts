790c1b930352d2176fa3cc4b010ee7125d8224e2
Cartago <cartago.relaxingcup@gmail.com>
2024-07-14 16:55:37 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/mutual-recursion.ts
+++ src/typescript/exercises/6kyu/mutual-recursion.ts
@@ -1,9 +1,11 @@
 //* https://www.codewars.com/kata/53a1eac7e0afd3ad3300008b/train/typescript
 
 export function F(n: number): number {
-	return 0;
+	if (n === 0) return 1;
+	return n - M(F(n - 1));
 }
 
 export function M(n: number): number {
-	return 0;
+	if (n === 0) return 0;
+	return n - F(M(n - 1));
 }
