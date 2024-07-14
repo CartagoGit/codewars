1bea12c37b3a24589e075c6d86c5d1423ce1694a
Cartago <cartago.relaxingcup@gmail.com>
2024-07-14 16:49:03 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/mutual-recursion.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/53a1eac7e0afd3ad3300008b/train/typescript
+
+export function F(n: number): number {
+	return 0;
+}
+
+export function M(n: number): number {
+	return 0;
+}
