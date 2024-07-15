a829d5953d028b751a90601f8f9e21827558670a
Cartago <cartago.relaxingcup@gmail.com>
2024-07-15 16:15:14 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/cat-and-mouse.ts
+++ src/typescript/exercises/7kyu/cat-and-mouse.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/57ee24e17b45eff6d6000164/train/typescript
 
 export function catMouse(x: string): string {
-    return '';
-  }
+	return x.length - 2 > 3 ? 'Escaped!' : 'Caught!';
+}
