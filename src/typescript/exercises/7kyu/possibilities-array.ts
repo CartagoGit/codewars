4df1bb4b86419508583cac6c27aa5e0bdc0db266
Cartago <cartago.relaxingcup@gmail.com>
2024-07-16 07:37:02 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/possibilities-array.ts
+++ src/typescript/exercises/7kyu/possibilities-array.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/59b710ed70a3b7dd8f000027/train/typescript
 
 export function isAllPossibilities(x: number[]): boolean {
-    return false;
-  }
+	const uniques = [...new Set(x)].filter((num) => num >= 0);
+	return uniques.length === Math.max(...x) + 1 && uniques.length === x.length;
+}
