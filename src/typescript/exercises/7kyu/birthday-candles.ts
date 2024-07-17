e75862d9cad1d3c391d75c77b0b028d090acd263
Cartago <cartago.relaxingcup@gmail.com>
2024-07-17 07:23:18 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/birthday-candles.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/6630da20f925eb3007c5a498/train/typescript
+
+export function blowCandles(str: string): number {
+    // Let's see how we gonna blow these candles...
+    return 0;
+  }
