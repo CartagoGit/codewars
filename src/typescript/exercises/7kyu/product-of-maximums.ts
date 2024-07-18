afdc5d52fb66a5e6ec2b9a275cd3958078882095
Cartago <cartago.relaxingcup@gmail.com>
2024-07-18 08:15:13 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/product-of-maximums.ts
+++ src/typescript/exercises/7kyu/product-of-maximums.ts
@@ -1,6 +1,6 @@
 //* https://www.codewars.com/kata/5a63948acadebff56f000018/train/typescript
 
 export function maxProduct(numbers: number[], size: number): number {
-    // your code here
-  }
-  
+	const maximums = numbers.sort((a, b) => b - a).slice(0, size);
+	return maximums.reduce((acc, cur) => acc * cur, 1);
+}
