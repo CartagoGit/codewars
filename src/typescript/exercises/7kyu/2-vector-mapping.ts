fa1e0a478736723592b9e454de203adcd73b035e
Cartago <cartago.relaxingcup@gmail.com>
2024-07-19 07:34:17 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/2-vector-mapping.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/5da995d583326300293ce4cb/train/typescript
+
+export function mapVector(
+	vector: [number, number],
+	circle1: [number, number, number],
+	circle2: [number, number, number]
+): [number, number] {
+	return [0, 0];
+}
