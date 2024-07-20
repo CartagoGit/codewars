fabee01f675b09267c74f4db43f1ed5a8aeb17a6
Cartago <cartago.relaxingcup@gmail.com>
2024-07-20 17:01:35 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/what-is-between.ts
+++ src/typescript/exercises/8kyu/what-is-between.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/typescript
 
 export function between(a: number, b: number): number[] {
-	return [];
+	return Array.from({ length: b - a + 1 }, (_, i) => a + i);
 }
