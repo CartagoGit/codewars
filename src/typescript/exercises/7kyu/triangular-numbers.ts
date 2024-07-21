1126e1bd218d1ab11f71337bfdfb6dcf2bccccf7
Cartago <cartago.relaxingcup@gmail.com>
2024-07-21 17:34:42 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/triangular-numbers.ts
+++ src/typescript/exercises/7kyu/triangular-numbers.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/580878d5d27b84b64c000b51/train/typescript
 
 export function sumTriangularNumbers(n: number): number {
-	return 0;
+	let trinagularNums: number[] = [];
+	for (let i = 0; i <= n; i++) {
+		trinagularNums.push((i * (i + 1)) / 2);
+	}
+	return trinagularNums.reduce((acc, curr) => acc + curr, 0);
 }
