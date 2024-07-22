f3a1fe59eebf9f9b9559dc0bebe5bf9fce21c620
Cartago <cartago.relaxingcup@gmail.com>
2024-07-22 08:27:31 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/ascii-letters.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/589ebcb9926baae92e000001/train/typescript
+
+export function convert(n: string): string {
+	// ...Convert integers given as string into ASCII uppercase letters...
+	return '';
+}
