1c3a9e1f39ab6fc4e431286f71fd7fc4e994a94c
Cartago <cartago.relaxingcup@gmail.com>
2024-07-22 08:49:18 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/ascii-letters.ts
+++ src/typescript/exercises/7kyu/ascii-letters.ts
@@ -1,6 +1,10 @@
 //* https://www.codewars.com/kata/589ebcb9926baae92e000001/train/typescript
 
-export function convert(n: string): string {
-	// ...Convert integers given as string into ASCII uppercase letters...
-	return '';
+export function convert(chain: string): string {
+	const asciiChars: string[] = chain.split(/(.{2})/).map((char) => {
+		if (!char) return '';
+		const num = parseInt(char.trim(), 10);
+		return num >= 32 && num <= 126 ? String.fromCharCode(num) : ' ';
+	});
+	return asciiChars.join('');
 }
