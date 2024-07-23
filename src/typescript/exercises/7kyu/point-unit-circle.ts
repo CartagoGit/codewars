d3b1dc6cb28772d9fd207a4db57ea14ff954188e
Cartago <cartago.relaxingcup@gmail.com>
2024-07-23 07:30:28 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/point-unit-circle.ts
+++ src/typescript/exercises/7kyu/point-unit-circle.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/58da7ae9b340a2440500009c/train/typescript
 
 export function pointInCircle(x: number, y: number): boolean {
-    // Your code
-  }
+	return Math.sqrt(x * x + y * y) < 1;
+}
