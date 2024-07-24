341a48886f3b16932b416114e880579b98e23cc1
Cartago <cartago.relaxingcup@gmail.com>
2024-07-24 07:17:28 +0200

    Damos solucion a la kata

--- src/typescript/exercises/7kyu/ch4113ng3.ts
+++ src/typescript/exercises/7kyu/ch4113ng3.ts
@@ -1,5 +1,17 @@
 //* https://www.codewars.com/kata/59e9f404fc3c49ab24000112/train/typescript
 
 export function nerdify(txt: string): string {
-  return "";
+	return txt
+		.split('')
+		.map((char) => {
+			const dictionary: Record<string, string> = {
+				a: '4',
+                A: '4',
+				e: '3',
+                E: '3',
+				l: '1',
+			};
+			return dictionary[char] ?? char;
+		})
+		.join('');
 }
