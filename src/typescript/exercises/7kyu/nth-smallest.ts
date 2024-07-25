9664c1262022747ef21645af53761d1644024125
Cartago <cartago.relaxingcup@gmail.com>
2024-07-25 10:00:40 +0200

    Modificamos codigo para evitar inconsistencias

--- src/typescript/exercises/7kyu/nth-smallest.ts
+++ src/typescript/exercises/7kyu/nth-smallest.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5a512f6a80eba857280000fc/train/typescript
 
 export function nthSmallest(arr: number[], pos: number) {
-	return arr.sort((a, b) => a - b)[pos - 1];
+	return [...arr].sort((a, b) => a - b)[pos - 1];
 }
