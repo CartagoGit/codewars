5d377656424f0a41886b253ed77b1f4eff1ed8df
Cartago <cartago.relaxingcup@gmail.com>
2024-07-25 10:00:19 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/nth-smallest.ts
+++ src/typescript/exercises/7kyu/nth-smallest.ts
@@ -1,6 +1,5 @@
 //* https://www.codewars.com/kata/5a512f6a80eba857280000fc/train/typescript
 
-
 export function nthSmallest(arr: number[], pos: number) {
-    // your code here
-  }
+	return arr.sort((a, b) => a - b)[pos - 1];
+}
