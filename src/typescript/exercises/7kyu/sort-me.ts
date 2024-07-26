2a87b49dd2588dd8f503a78f8f897d65b8378b30
Cartago <cartago.relaxingcup@gmail.com>
2024-07-27 00:32:36 +0200

    Damos solucion sin mutar el array original

--- src/typescript/exercises/7kyu/sort-me.ts
+++ src/typescript/exercises/7kyu/sort-me.ts
@@ -1,3 +1,3 @@
 //* https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/typescript
 
-export const sortMe = (names: string[]): string[] => names.sort();
+export const sortMe = (names: string[]): string[] => [...names].sort();
