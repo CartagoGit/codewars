17f8970316cd09086181cfcf1da8bf7538e23367
Cartago <cartago.relaxingcup@gmail.com>
2024-07-27 00:31:17 +0200

    DAmos una solucion a la kata

--- src/typescript/exercises/7kyu/sort-me.ts
+++ src/typescript/exercises/7kyu/sort-me.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/typescript
 
 export function sortMe(names: string[]): string[] {
-	// Your code here
+	return names.sort((a, b) => a.localeCompare(b));
 }
