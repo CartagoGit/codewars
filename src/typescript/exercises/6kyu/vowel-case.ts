48b65ad92b71c89a76e22b8b99a43bdb769d1724
Cartago <cartago.relaxingcup@gmail.com>
2024-07-28 15:57:55 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/vowel-case.ts
@@ -0,0 +1,11 @@
+//* https://www.codewars.com/kata/53697be005f803751e0015aa/train/typescript
+
+// turn vowels into numbers
+export function encode(string: string): string {
+    return '';
+  }
+  
+  // turn numbers back into vowels
+  export function decode(string: string): string {
+    return '';
+  }
