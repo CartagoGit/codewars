163e9a56d5fbe7233fe7c2924695e989c4fccd52
Cartago <cartago.relaxingcup@gmail.com>
2024-07-28 15:59:50 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/vowel-case.ts
+++ src/typescript/exercises/6kyu/vowel-case.ts
@@ -1,11 +1,23 @@
 //* https://www.codewars.com/kata/53697be005f803751e0015aa/train/typescript
 
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
 // turn vowels into numbers
 export function encode(string: string): string {
-    return '';
-  }
-  
-  // turn numbers back into vowels
-  export function decode(string: string): string {
-    return '';
-  }
+	return string
+		.split('')
+		.map((char) =>
+			vowels.includes(char) ? vowels.indexOf(char) + 1 : char
+		)
+		.join('');
+}
+
+// turn numbers back into vowels
+export function decode(string: string): string {
+	return string
+        .split('')
+        .map((char) =>
+            parseInt(char) ? vowels[parseInt(char) - 1] : char
+        )
+        .join('');
+}
