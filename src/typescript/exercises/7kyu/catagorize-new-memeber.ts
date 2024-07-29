ef3de8055ce7422f7d1d0c23076561fb1d05a9c4
Cartago <cartago.relaxingcup@gmail.com>
2024-07-29 07:46:33 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/catagorize-new-memeber.ts
+++ src/typescript/exercises/7kyu/catagorize-new-memeber.ts
@@ -1,6 +1,7 @@
 //* https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/typescript
 
-export function openOrSenior(data:number[][]):string[]{
-  return [""]
+export function openOrSenior(data: number[][]): string[] {
+	return data.map(([age, handicap]) =>
+		age >= 55 && handicap > 7 ? 'Senior' : 'Open'
+	);
 }
-
