4a1abdf084b2724c009abac39ae76b571fcc011c
Cartago <cartago.relaxingcup@gmail.com>
2024-07-30 09:26:48 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/find-next-square.ts
+++ src/typescript/exercises/7kyu/find-next-square.ts
@@ -1,6 +1,6 @@
 //* https://www.codewars.com/kata/56269eb78ad2e4ced1000013/train/typescript
 
-export function findNextSquare(sq:number):number {
-  // Return the next square if sq is a perfect square, -1 otherwise
-  return -1;
+export function findNextSquare(sq: number): number {
+	const sqrt = Math.sqrt(sq);
+	return Number.isInteger(sqrt) ? Math.pow(sqrt + 1, 2) : -1;
 }
