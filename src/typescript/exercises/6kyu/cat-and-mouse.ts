87754a6ff07b78aefc95845932b548713935fa66
Cartago <cartago.relaxingcup@gmail.com>
2024-08-01 08:54:56 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/cat-and-mouse.ts
+++ src/typescript/exercises/6kyu/cat-and-mouse.ts
@@ -1,8 +1,16 @@
 //* https://www.codewars.com/kata/57ee2a1b7b45efcf700001bf/train/typescript
 
-import { M } from "./mutual-recursion";
-
 export function catMouse(x: string, jump: number): string {
-  
-  }
-  
+	if (['m', 'C', 'D'].some((animal) => !x.includes(animal)))
+		return 'boring without all three';
+	const cat = x.indexOf('C');
+	const mouse = x.indexOf('m');
+	const dog = x.indexOf('D');
+	if (Math.abs(cat - mouse) > jump) return 'Escaped!';
+	if (
+		(mouse < cat && (cat < dog || dog < mouse)) ||
+		(mouse > cat && (cat > dog || dog > mouse))
+	)
+		return 'Caught!';
+	return 'Protected!';
+}
