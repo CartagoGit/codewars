ddd7892b5c2ee1a6fa8f7ff2a18074d1de12bf5c
Cartago <cartago.relaxingcup@gmail.com>
2024-08-01 08:50:11 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/cat-and-mouse.ts
@@ -0,0 +1,8 @@
+//* https://www.codewars.com/kata/57ee2a1b7b45efcf700001bf/train/typescript
+
+import { M } from "./mutual-recursion";
+
+export function catMouse(x: string, jump: number): string {
+  
+  }
+  
