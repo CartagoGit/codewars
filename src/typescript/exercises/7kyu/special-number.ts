40f1ce727a9d64e6b01fca1c0e3ec5028bb8568f
Cartago <cartago.relaxingcup@gmail.com>
2024-08-02 08:46:02 +0200

    Terminamos la kata

--- src/typescript/exercises/7kyu/special-number.ts
+++ src/typescript/exercises/7kyu/special-number.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5a55f04be6be383a50000187/train/typescript
 
 export function specialNumber(n: number) {
-	
+	return n
+		.toString()
+		.split('')
+		.every((digit) => Number(digit) <= 5)
+		? 'Special!!'
+		: 'NOT!!';
 }
