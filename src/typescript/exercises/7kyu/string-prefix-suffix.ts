f0c605cdd041890f80205d644d8006a5ba74390a
Cartago <cartago.relaxingcup@gmail.com>
2024-08-03 13:42:33 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/string-prefix-suffix.ts
+++ src/typescript/exercises/7kyu/string-prefix-suffix.ts
@@ -1,15 +1,8 @@
 //* https://www.codewars.com/kata/5ce969ab07d4b7002dcaa7a1/train/typescript
 
 export function solve(chain: string) {
-	// const lastIndex = chain.lastIndexOf(chain[0]);
-	// if ([-1, 0].includes(lastIndex)) return 0;
-	// const fromLastChain = chain.slice(lastIndex);
-	// const fromFirstChain = chain.slice(0, fromLastChain.length);
-	// console.log({ chain, lastIndex, fromLastChain, fromFirstChain });
-	// return fromFirstChain === fromLastChain ? fromLastChain.length : 0;
-	for (let i = 1; i < chain.length; i++) {
-        console.log({ i, chain: chain.slice(i), startsWith: chain.startsWith(chain.slice(i)) });
+	for (let i = Math.ceil(chain.length / 2); i < chain.length; i++) {
 		if (chain.startsWith(chain.slice(i))) return chain.slice(i).length;
 	}
-    return 0;
+	return 0;
 }
