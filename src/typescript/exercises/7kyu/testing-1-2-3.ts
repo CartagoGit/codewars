01270fcb8ad9716efd3b20953723bebd94c60415
Cartago <cartago.relaxingcup@gmail.com>
2024-08-04 14:25:30 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/testing-1-2-3.ts
+++ src/typescript/exercises/7kyu/testing-1-2-3.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/typescript
 
 export function number(array: string[]): string[] {
-	return [];
+	return array.map((value, index) => `${index + 1}: ${value}`);
 }
