a160be81e401cfa989ddd7cf0cd5dc5c02700a9a
Cartago <cartago.relaxingcup@gmail.com>
2024-08-05 07:22:25 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/counting-sheep.ts
+++ src/typescript/exercises/8kyu/counting-sheep.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/54edbc7200b811e956000556/train/typescript
 
 export function countSheeps(arrayOfSheep: (boolean | undefined | null)[]) {
-	
+	return arrayOfSheep.filter((sheep) => !!sheep).length;
 }
