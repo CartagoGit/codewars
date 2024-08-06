01968a8cd883431faedc7633db33b38219c0958f
Cartago <cartago.relaxingcup@gmail.com>
2024-08-06 07:34:12 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/jumping-number.ts
+++ src/typescript/exercises/7kyu/jumping-number.ts
@@ -1,6 +1,10 @@
 //* https://www.codewars.com/kata/5a54e796b3bfa8932c0000ed/train/typescript
 
 export function jumpingNumber(n: number) {
-   
-  }
-  
+	if (n < 10) return 'Jumping!!';
+	const num = n.toString().split('').map(Number);
+	for (let i = 1; i < num.length; i++) {
+		if (Math.abs(num[i] - num[i - 1]) !== 1) return 'Not!!';
+	}
+	return 'Jumping!!';
+}
