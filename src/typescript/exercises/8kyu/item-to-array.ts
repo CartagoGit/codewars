91c4a3f3cd2f305f5d50e54ee28f7a12d35edd8b
Cartago <cartago.relaxingcup@gmail.com>
2024-08-07 08:01:16 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/8kyu/item-to-array.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/511f0fe64ae8683297000001/train/typescript
+
+import { websites } from "./preloaded";
+// add the value "codewars" to the `websites` array
+
+
+export { websites };
