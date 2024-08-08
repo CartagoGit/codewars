a612bc5d06a90702f5f47ad0edaf98d83bbb9d77
Cartago <cartago.relaxingcup@gmail.com>
2024-08-08 09:20:50 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/spinning-rings.ts
+++ src/typescript/exercises/7kyu/spinning-rings.ts
@@ -1,12 +1,11 @@
 //* https://www.codewars.com/kata/59afff65f1c8274f270020f5/train/typescript
 
 export function spinningRings(innerMax: number, outerMax: number): number {
-	let numberOfSpinsTillRingsAreEqual = 1;
-	const nextInner = (val: number) => (val-- < 0 ? innerMax : val--);
-	const nextOuter = (val: number) => (val++ > outerMax ? 0 : val++);
+	let numberOfSpinsTillRingsAreEqual = 0;
+	const nextInner = (val: number) => (val - 1 < 0 ? innerMax : val - 1);
+	const nextOuter = (val: number) => (val + 1 > outerMax ? 0 : val + 1);
 	let [inner, outer] = [0, 0];
 	do {
-        console.log({innerMax, outerMax, inner, outer});
 		numberOfSpinsTillRingsAreEqual++;
 		inner = nextInner(inner);
 		outer = nextOuter(outer);
