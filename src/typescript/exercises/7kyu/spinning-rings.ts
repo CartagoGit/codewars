629ce37f830cb2c466a32ce533437c7fcc35d1fb
Cartago <cartago.relaxingcup@gmail.com>
2024-08-08 09:18:24 +0200

    Seguimos con las pruebas logicas de la kata

--- src/typescript/exercises/7kyu/spinning-rings.ts
+++ src/typescript/exercises/7kyu/spinning-rings.ts
@@ -1,7 +1,15 @@
 //* https://www.codewars.com/kata/59afff65f1c8274f270020f5/train/typescript
 
 export function spinningRings(innerMax: number, outerMax: number): number {
-    let numberOfSpinsTillRingsAreEqual = 0;
-    // your code here
-    return numberOfSpinsTillRingsAreEqual;
-  };
+	let numberOfSpinsTillRingsAreEqual = 1;
+	const nextInner = (val: number) => (val-- < 0 ? innerMax : val--);
+	const nextOuter = (val: number) => (val++ > outerMax ? 0 : val++);
+	let [inner, outer] = [0, 0];
+	do {
+        console.log({innerMax, outerMax, inner, outer});
+		numberOfSpinsTillRingsAreEqual++;
+		inner = nextInner(inner);
+		outer = nextOuter(outer);
+	} while (inner !== outer);
+	return numberOfSpinsTillRingsAreEqual;
+}
