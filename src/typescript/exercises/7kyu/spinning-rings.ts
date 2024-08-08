20827cb42d6452e07afbbead6b71f7a9b11fa381
Cartago <cartago.relaxingcup@gmail.com>
2024-08-08 09:05:56 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/spinning-rings.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/59afff65f1c8274f270020f5/train/typescript
+
+export function spinningRings(innerMax: number, outerMax: number): number {
+    let numberOfSpinsTillRingsAreEqual = 0;
+    // your code here
+    return numberOfSpinsTillRingsAreEqual;
+  };
