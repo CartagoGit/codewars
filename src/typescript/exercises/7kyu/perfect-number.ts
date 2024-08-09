99fc9ddeb0dbf5737f52080ca618bbbe49e4d3ce
Cartago <cartago.relaxingcup@gmail.com>
2024-08-10 00:23:01 +0200

    Resolvemos la ktaa

--- src/typescript/exercises/7kyu/perfect-number.ts
+++ src/typescript/exercises/7kyu/perfect-number.ts
@@ -1,6 +1,7 @@
 //* https://www.codewars.com/kata/5a662a02e626c54e87000123/train/typescript
 
 export function extraPerfect(n: number) {
-    // your code here
-  }
-  
+	return Array.from({ length: n }, (_, i) => i + 1).filter(
+		(el) => el % 2 !== 0
+	);
+}
