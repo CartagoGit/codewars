72434b91ae2b45a31b36d1b3503fd85f2bf681f9
Cartago <cartago.relaxingcup@gmail.com>
2024-08-09 08:34:17 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/list-filtering.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/typescript
+
+export function filter_list(l: Array<any>): Array<number> {
+	// Return a new array with the strings filtered out
+}
