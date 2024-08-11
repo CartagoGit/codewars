1cc1603d28a831f0e1e0d7a5f8004486a85f7810
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2024-08-11 15:43:07 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/stacked-balls-2d.ts
+++ src/typescript/exercises/7kyu/stacked-balls-2d.ts
@@ -1,6 +1,11 @@
 // https://www.codewars.com/kata/5bb804397274c772b40000ca/train/typescript
 
 export function stackHeight2d(layers: number) {
-    // your code here
-  }
-  
+	if (!layers) return 0;
+	const heigth =
+		(Math.sqrt(Math.pow(layers, 2) - Math.pow(layers / 2, 2)) *
+			(layers - 1) +
+			layers) /
+		layers;
+	return heigth;
+}
