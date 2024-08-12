9c098c4fe9bd031ac30a2510b6f9418c74f9d45f
Cartago Nova - Portatil <cartago.relaxingcup@gmail.com>
2024-08-12 12:24:00 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/say-hello.ts
+++ src/typescript/exercises/7kyu/say-hello.ts
@@ -1,3 +1,3 @@
 // * https://www.codewars.com/kata/53a20af2e0afd3e2cd000333/train/typescript
 
-export const say = (string1:string) => // your code here
+export const say = (string1:string) => (string2:string) => `${string1} ${string2}`;
