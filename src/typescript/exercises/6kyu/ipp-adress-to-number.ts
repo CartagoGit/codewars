7808cecea84408cfd49b01dd753612da9a4fa292
Cartago <cartago.relaxingcup@gmail.com>
2024-08-18 16:04:59 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/ipp-adress-to-number.ts
@@ -0,0 +1,9 @@
+//* https://www.codewars.com/kata/541a354c39c5efa5fa001372/train/typescript
+
+export function ipToNum(ip: string): number {
+    return 0;
+  }
+  
+  export function numToIp(x: number): string {
+    return '0.0.0.0';
+  }
