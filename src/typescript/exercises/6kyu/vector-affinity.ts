f47165dc0bcf2212d7e9ac6c443d7979ed7802d6
Cartago <cartago.relaxingcup@gmail.com>
2024-08-19 09:03:24 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/vector-affinity.ts
+++ src/typescript/exercises/6kyu/vector-affinity.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/5498505a43e0fd83620010a9/train/typescript
 
-export const vectorAffinity = function vectorAffinity(xs:Array<number|null>,ys:Array<number|null>):number {
-    return 0;
-  }
+export const vectorAffinity = function vectorAffinity(
+	xs: Array<number | null>,
+	ys: Array<number | null>
+): number {
+	const sameResults = xs.filter((x, i) => x === ys[i]);
+	const result = sameResults.length / Math.max(xs.length, ys.length);
+	if (isNaN(result)) {
+		if (xs.length === 0 && ys.length === 0) return 1;
+		return 0;
+	}
+	return result;
+};
