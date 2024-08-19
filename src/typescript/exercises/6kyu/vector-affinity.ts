16d5dee5be68620820012b66271b544d5c1e27f7
Cartago <cartago.relaxingcup@gmail.com>
2024-08-19 08:47:53 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/vector-affinity.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/5498505a43e0fd83620010a9/train/typescript
+
+export const vectorAffinity = function vectorAffinity(xs:Array<number|null>,ys:Array<number|null>):number {
+    return 0;
+  }
