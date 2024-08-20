df06a2c307dc42456b8bdd1d7ca5a621a45667ab
Cartago <cartago.relaxingcup@gmail.com>
2024-08-20 07:39:10 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/friend-or-foe.ts
+++ src/typescript/exercises/7kyu/friend-or-foe.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/55b42574ff091733d900002f/train/typescript
 
-export function friend(friends: string[]): string[] { 
-    //your code here
-  }
+export function friend(friends: string[]): string[] {
+	return friends.filter((friend) => friend.length === 4);
+}
