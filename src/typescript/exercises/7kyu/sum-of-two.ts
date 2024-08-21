b9960570b6a5282abd90e55a42dad3dfff36f0d1
Cartago <cartago.relaxingcup@gmail.com>
2024-08-21 07:43:10 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/sum-of-two.ts
+++ src/typescript/exercises/7kyu/sum-of-two.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/typescript
 
 export function sumTwoSmallestNumbers(numbers:Array<number>):number {  
-    //Code here
-  }
+    const [min1, min2]  = numbers.sort((a, b) => a - b);
+    return min1 + min2;
+}
