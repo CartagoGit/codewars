3380f359b6df930924c113743ef100e2cc39aa1a
Cartago <cartago.relaxingcup@gmail.com>
2024-08-22 07:44:19 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/8kyu/cat-dog-years.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/typescript
+
+export function humanYearsCatYearsDogYears(humanYears: number): [number, number, number] {
+    // Your code here!
+    return [0,0,0];
+  }
+  
