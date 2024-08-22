e11f439c5fb503e6cdb3b8ac5320cb2a86268f80
Cartago <cartago.relaxingcup@gmail.com>
2024-08-22 07:46:53 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/cat-dog-years.ts
+++ src/typescript/exercises/8kyu/cat-dog-years.ts
@@ -1,7 +1,20 @@
 //* https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/typescript
 
-export function humanYearsCatYearsDogYears(humanYears: number): [number, number, number] {
-    // Your code here!
-    return [0,0,0];
-  }
-  
+export function humanYearsCatYearsDogYears(
+	humanYears: number
+): [number, number, number] {
+	let [catYears, dogYears] = [0, 0];
+	for (let i = 0; i < humanYears; i++) {
+		if (i === 0) {
+			catYears += 15;
+			dogYears += 15;
+		} else if (i === 1) {
+			catYears += 9;
+			dogYears += 9;
+		} else {
+			catYears += 4;
+			dogYears += 5;
+		}
+	}
+	return [humanYears, catYears, dogYears];
+}
