a88c2702567e92f2314a64b9aa09b628302e16e6
Cartago <cartago.relaxingcup@gmail.com>
2024-08-23 15:29:14 +0200

    Solventamos la kata

--- src/typescript/exercises/8kyu/array-comma-delimiters.ts
+++ src/typescript/exercises/8kyu/array-comma-delimiters.ts
@@ -1,5 +1,3 @@
 //* https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/typescript
 
-export function printArray(array:Number[]){
-    // show me the code!
-  }
+export const printArray = (array: unknown[]) => array.join(',');
