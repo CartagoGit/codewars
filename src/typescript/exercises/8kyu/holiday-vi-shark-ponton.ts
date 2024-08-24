4cdd044b6eeed7d5b2a0dfc41a062cd411defcd9
Cartago <cartago.relaxingcup@gmail.com>
2024-08-24 15:50:08 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/8kyu/holiday-vi-shark-ponton.ts
@@ -0,0 +1,11 @@
+//* https://www.codewars.com/kata/57e921d8b36340f1fd000059/train/typescript
+
+export function shark(
+	pontoonDistance: number,
+	sharkDistance: number,
+	youSpeed: number,
+	sharkSpeed: number,
+	dolphin: boolean
+): string {
+    
+}
