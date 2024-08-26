789e3b49ad91325766b1b2c5e065d06802702d27
Cartago <cartago.relaxingcup@gmail.com>
2024-08-27 00:04:41 +0200

    Resolvemos la kata

--- src/typescript/exercises/8kyu/cats-in-hats.ts
+++ src/typescript/exercises/8kyu/cats-in-hats.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/57b5907920b104772c00002a/train/typescript
 
 export function height(n: number): string {
-    return '';
-  }
+	let height = 2000000;
+    let lastCatHeight = height;
+	for (let i = n; i > 0; i--) {
+        lastCatHeight = lastCatHeight / 2.5;
+		height = height + lastCatHeight;
+	}
+	return height.toFixed(3);
+}
