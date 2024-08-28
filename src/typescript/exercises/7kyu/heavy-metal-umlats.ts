1468b73cb85744ce1581552608742997e9770425
Cartago <cartago.relaxingcup@gmail.com>
2024-08-28 07:39:26 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/heavy-metal-umlats.ts
+++ src/typescript/exercises/7kyu/heavy-metal-umlats.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/57d4e99bec16701a67000033/train/typescript
 
+const dictionary = { A: 'Ä', E: 'Ë', I: 'Ï', O: 'Ö', U: 'Ü', Y: 'Ÿ', a: 'ä', e: 'ë', i: 'ï', o: 'ö', u: 'ü', y: 'ÿ' };
+
 export function heavyMetalUmlauts(boringText: string): string {
-	return '';
+	return boringText.replace(
+		/[AEIOUYaeiouy]/g,
+		(match) => dictionary[match as keyof typeof dictionary]
+	);
 }
