9fbb3e517df0d32b5d59abadd5c07fa8169dba25
Cartago <cartago.relaxingcup@gmail.com>
2024-08-29 08:28:20 +0200

    Resolvemos la kata

--- src/typescript/exercises/6kyu/framed-reflection.ts
+++ src/typescript/exercises/6kyu/framed-reflection.ts
@@ -1,10 +1,11 @@
-import { reverse } from './../3kyu/reverse';
+
 //* https://www.codewars.com/kata/581331293788bc1702001fa6/train/typescript
 
 export function mirror(text: string): string {
 	const words = text.split(' ');
-	const reversedWords = words.map((word) => reverse(word));
+	const reversedWords = words.map((word) => word.split('').reverse().join(''));
 	const maxLength = Math.max(...words.map((word) => word.length));
+    const spacedReversedWords = reversedWords.map((word) => `* ${word.padEnd(maxLength, ' ')} *`);
 	const frame = '*'.repeat(maxLength + 4);
-    return 'fail'
+    return [frame, ...spacedReversedWords, frame].join('\n');
 }
