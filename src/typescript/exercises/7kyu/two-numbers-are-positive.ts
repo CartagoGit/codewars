061afc6e8b76b78e547e6832ee62704b84da2a6f
Cartago <cartago.relaxingcup@gmail.com>
2024-08-31 13:57:48 +0200

    Resolvemos la kata

--- src/typescript/exercises/7kyu/two-numbers-are-positive.ts
+++ src/typescript/exercises/7kyu/two-numbers-are-positive.ts
@@ -1,6 +1,5 @@
 //* https://www.codewars.com/kata/602db3215c22df000e8544f0/train/typescript
 
 export function twoArePositive(a: number, b: number, c: number): boolean {
-    // your code here
-    return false;
-  }
+	return [a, b, c].filter((num) => num > 0).length === 2;
+}
