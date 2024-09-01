fc1d2f558caf5949cea605b4ac3cd602e6a6a486
Cartago <cartago.relaxingcup@gmail.com>
2024-09-01 16:02:43 +0200

    Refactorizamos las funciones

--- src/typescript/exercises/6kyu/easy-diagonal.ts
+++ src/typescript/exercises/6kyu/easy-diagonal.ts
@@ -2,6 +2,11 @@
 
 export function diagonal(numRows: number, numDiagonal: number): number {
 	//* Triangle of Pascal
+	const triangle = generateTriangle(numRows);
+	return getDiagonalSum({ numRows, triangle, numDiagonal });
+}
+
+const generateTriangle = (numRows: number): number[][] => {
 	// It starts with a 1 at the top, and each subsequent row is constructed by adding two 1s to the ends of the row above it.
 	let triangle: number[][] = [[1]];
 
@@ -18,7 +23,15 @@ export function diagonal(numRows: number, numDiagonal: number): number {
 		row.push(1); // Every row ends with 1
 		triangle.push(row);
 	}
+	return triangle;
+};
 
+const getDiagonalSum = (data: {
+	numRows: number;
+	triangle: number[][];
+	numDiagonal: number;
+}): number => {
+	const { numRows, triangle, numDiagonal } = data;
 	// Get sum of diagonal specified
 	let sum = 0;
 	const diagonalLength = numRows - numDiagonal + 1;
@@ -32,6 +45,6 @@ export function diagonal(numRows: number, numDiagonal: number): number {
 		sum += row[diagonalIndex];
 	}
 	return sum;
-}
+};
 
 console.log('result:', diagonal(7, 2));
