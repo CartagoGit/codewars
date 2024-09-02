ca92b70d6ff783f1d24c17fd2f410335f8696664
Cartago <cartago.relaxingcup@gmail.com>
2024-09-02 07:32:40 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/bob-count-letters-digits.ts
+++ src/typescript/exercises/7kyu/bob-count-letters-digits.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/5738f5ea9545204cec000155/train/typescript
 
 export function countLettersAndDigits(input: string): number {
-    return 0;
-  }
+	const regex = /[a-zA-Z0-9]/g;
+	const matches = input.match(regex);
+	return matches?.length ?? 0;
+}
