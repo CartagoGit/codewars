3192565e66bef67ca541188e0a8ba1a7b3268463
Cartago <cartago.relaxingcup@gmail.com>
2024-09-03 07:40:47 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/minimize-sum-array.ts
+++ src/typescript/exercises/7kyu/minimize-sum-array.ts
@@ -1,5 +1,10 @@
 //* https://www.codewars.com/kata/5a523566b3bfa84c2e00010b/train/typescript
 
 export function minSum(arr: number[]): number {
-    // your code here
-  }
+	const sort = arr.sort((a, b) => a - b);
+	let sum = 0;
+	for (let i = 0; i < arr.length / 2; i++) {
+		sum += arr[i] * arr[arr.length - 1 - i];
+	}
+	return sum;
+}
