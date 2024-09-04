322d508925972a882ee20863d0d0a3ab3093b977
Cartago <cartago.relaxingcup@gmail.com>
2024-09-04 07:46:10 +0200

    Quitamos codigo innecesario

--- src/typescript/exercises/7kyu/minimize-sum-array.ts
+++ src/typescript/exercises/7kyu/minimize-sum-array.ts
@@ -1,7 +1,6 @@
 //* https://www.codewars.com/kata/5a523566b3bfa84c2e00010b/train/typescript
 
 export function minSum(arr: number[]): number {
-	const sort = arr.sort((a, b) => a - b);
 	let sum = 0;
 	for (let i = 0; i < arr.length / 2; i++) {
 		sum += arr[i] * arr[arr.length - 1 - i];
