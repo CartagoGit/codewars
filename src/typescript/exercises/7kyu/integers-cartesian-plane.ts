42c08d50c253f6c9082b634a2ef86ddff330f252
Cartago <cartago.relaxingcup@gmail.com>
2024-09-05 08:22:32 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/integers-cartesian-plane.ts
@@ -0,0 +1,11 @@
+//* https://www.codewars.com/kata/559e3224324a2b6e66000046/train/typescript
+
+export function sumin(n: number): number {
+    throw new Error('TODO')
+  }
+  export function sumax(n: number): number {
+    throw new Error('TODO')
+  }
+  export function sumsum(n: number): number {
+    throw new Error('TODO')
+  }
