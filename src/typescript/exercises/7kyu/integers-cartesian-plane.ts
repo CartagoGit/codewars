eba74872ff5a81219c3c28fa38107ba9d18c75fa
Cartago <cartago.relaxingcup@gmail.com>
2024-09-05 09:10:37 +0200

    Solucion con timeout

--- src/typescript/exercises/7kyu/integers-cartesian-plane.ts
+++ src/typescript/exercises/7kyu/integers-cartesian-plane.ts
@@ -1,11 +1,24 @@
 //* https://www.codewars.com/kata/559e3224324a2b6e66000046/train/typescript
 
 export function sumin(n: number): number {
-    throw new Error('TODO')
-  }
-  export function sumax(n: number): number {
-    throw new Error('TODO')
-  }
-  export function sumsum(n: number): number {
-    throw new Error('TODO')
-  }
+	return createTable(n, 'min')
+		.flat()
+		.reduce((acc, curr) => acc + curr, 0);
+}
+export function sumax(n: number): number {
+	return createTable(n, 'max')
+		.flat()
+		.reduce((acc, curr) => acc + curr, 0);
+}
+export function sumsum(n: number): number {
+	return sumin(n) + sumax(n);
+}
+
+const createTable = (n: number, kind: 'min' | 'max') => {
+	return Array.from({ length: n }, (_, rowIndex) => {
+		return Array.from({ length: n }, (_, columnIndex) => {
+			if (kind === 'max') return Math.max(columnIndex + 1, rowIndex + 1);
+			else return Math.min(columnIndex + 1, rowIndex + 1);
+		});
+	});
+};
