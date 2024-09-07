af260170147635d9ccfb37af6a46e27d689640ed
Cartago <cartago.relaxingcup@gmail.com>
2024-09-08 01:06:17 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/regexp-eight-bit-unsigned.ts
+++ src/typescript/exercises/7kyu/regexp-eight-bit-unsigned.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/567e8f7b4096f2b4b1000005/train/typescript
 
+interface String {
+	eightBitNumber(): boolean;
+}
+
 String.prototype.eightBitNumber = function (): boolean {
-   
+	return /^(25[0-5]|2[0-4]\d|1\d{2}|[1-9]?\d)$/.test(`${this}`);
 };
