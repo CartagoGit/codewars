a50d28eb2e3b72d99221c93ba5aaa9ed59d9bbf6
Cartago <cartago.relaxingcup@gmail.com>
2024-09-07 19:03:46 +0200

    Damos una solucion recursiva

--- src/typescript/exercises/7kyu/seven.ts
+++ src/typescript/exercises/7kyu/seven.ts
@@ -1,11 +1,18 @@
 //* https://www.codewars.com/kata/55e6f5e58f7817808e00002e/train/typescript
 
-export function seven(m: number): number[] {
-	let counter = 0;
-	while (m > 99) {
-		const [lastNum, restNums] = [m % 10, Math.floor(m / 10)];
-		m = restNums - lastNum * 2;
-		counter++;
-	}
-	return [m, counter];
-}
+export const seven = (num: number, counter = 0): number[] => {
+	if (num < 100) return [num, counter];
+	const [lastNum, restNums] = [num % 10, Math.floor(num / 10)];
+	return seven(restNums - lastNum * 2, counter + 1);
+};
+
+//* Solucion 1
+// export function seven(m: number): number[] {
+// 	let counter = 0;
+// 	while (m > 99) {
+// 		const [lastNum, restNums] = [m % 10, Math.floor(m / 10)];
+// 		m = restNums - lastNum * 2;
+// 		counter++;
+// 	}
+// 	return [m, counter];
+// }
