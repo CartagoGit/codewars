431e114cf7bb07ad9312963799a4a590d3818337
Cartago <cartago.relaxingcup@gmail.com>
2024-09-07 18:57:24 +0200

    Avanzamos en la kata

--- src/typescript/exercises/7kyu/seven.ts
+++ src/typescript/exercises/7kyu/seven.ts
@@ -1,5 +1,14 @@
 //* https://www.codewars.com/kata/55e6f5e58f7817808e00002e/train/typescript
 
 export function seven(m: number): number[] {
-    // your code
-  }
+	let counter = 0;
+	if (m < 100) return [m, counter];
+	do {
+		const [lastValue, ...restValues] = m.toString().split('').reverse();
+		const lastNum = parseInt(lastValue);
+		const restNum = parseInt(restValues.reverse().join(''));
+		m = restNum - lastNum * 2;
+		counter++;
+	} while (m > 99);
+	return [m, counter];
+}
