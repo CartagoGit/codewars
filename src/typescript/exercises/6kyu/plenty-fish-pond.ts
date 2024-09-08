cee2b90a2aa23c94606f7800b47720eedb7f7e7d
Cartago <cartago.relaxingcup@gmail.com>
2024-09-08 14:23:08 +0200

    Quitamos  logs

--- src/typescript/exercises/6kyu/plenty-fish-pond.ts
+++ src/typescript/exercises/6kyu/plenty-fish-pond.ts
@@ -4,18 +4,15 @@ export function fish(shoal: String): number {
 	let size = 1;
 	let amountToGrow = 0;
 	const funcAmountToGrow = (size: number) => size * 4;
-
 	const fishes = shoal
 		.split('')
 		.sort()
 		.map(Number)
 		.filter((fish) => !!fish);
 	for (let fish of fishes) {
-		console.log('fish:', fish, { size, amountToGrow });
 		if (fish > size) break;
 		else amountToGrow += fish;
 		const needToGrow = funcAmountToGrow(size);
-		console.log({ needToGrow, amountToGrow });
 		if (amountToGrow >= needToGrow) {
 			size++;
 			amountToGrow = amountToGrow - needToGrow;
