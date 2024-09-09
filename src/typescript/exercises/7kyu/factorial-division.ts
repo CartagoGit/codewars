d3d7e3287db411ab2eb47fd936260610659932ba
Cartago <cartago.relaxingcup@gmail.com>
2024-09-09 07:39:30 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/factorial-division.ts
+++ src/typescript/exercises/7kyu/factorial-division.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/52f3a8e1f85fadcdf7001e31/train/typescript
 
-export function factorialDivision(n: bigint, d:bigint){
-  
+export function factorialDivision(n: bigint, d: bigint) {
+	let result = 1n;
+	for (let i = n; i > d; i--) {
+		result = i * result;
+	}
+	return result;
 }
