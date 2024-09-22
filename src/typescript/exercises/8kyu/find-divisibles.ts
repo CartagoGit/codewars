07f365effda3c0ab65f997c5c1a084e68dd556b8
Cartago <cartago.relaxingcup@gmail.com>
2024-09-22 22:00:39 +0200

    Resolvemos la kata

--- src/typescript/exercises/8kyu/find-divisibles.ts
+++ src/typescript/exercises/8kyu/find-divisibles.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/55edaba99da3a9c84000003b/train/typescript
 
 export function divisibleBy(numbers: number[], divisor: number): number[] {
-	return [];
+	return numbers.filter((num) => num % divisor === 0);
 }
