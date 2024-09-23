dc247c9c36865623ee030edc8aa7bfd90ce2978d
Cartago <cartago.relaxingcup@gmail.com>
2024-09-23 23:17:36 +0200

    Pasamos linter

--- src/typescript/exercises/7kyu/wheat-rice-chessboard.ts
+++ src/typescript/exercises/7kyu/wheat-rice-chessboard.ts
@@ -1,3 +1,4 @@
 //* https://www.codewars.com/kata/5b0d67c1cb35dfa10b0022c7/train/typescript
 
-export const squaresNeeded = (grains: number) =>   Math.ceil(Math.log2(grains + 1));
+export const squaresNeeded = (grains: number) =>
+	Math.ceil(Math.log2(grains + 1));
