d6af10a2d7a2180d652dea63fe1f1905e68553c4
Cartago <cartago.relaxingcup@gmail.com>
2024-09-25 00:09:13 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/clousure-counter.ts
+++ src/typescript/exercises/7kyu/clousure-counter.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/60edafd71dad1800563cf933/train/typescript
 
 export function counter(): Function {
-    return;
-  };
+	let counter = 1;
+	return () => counter++;
+}
