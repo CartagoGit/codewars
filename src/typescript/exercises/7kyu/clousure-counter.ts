e6d459531ef955359e72778d5a705a37c8e901ef
Cartago <cartago.relaxingcup@gmail.com>
2024-09-25 00:09:59 +0200

    cambiamos el nombre a la variable

--- src/typescript/exercises/7kyu/clousure-counter.ts
+++ src/typescript/exercises/7kyu/clousure-counter.ts
@@ -1,6 +1,6 @@
 //* https://www.codewars.com/kata/60edafd71dad1800563cf933/train/typescript
 
 export function counter(): Function {
-	let counter = 1;
-	return () => counter++;
+	let count = 1;
+	return () => count++;
 }
