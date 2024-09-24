52458a6b11b2e2327fa14fb0be09b6bd2ffb2b5f
Cartago <cartago.relaxingcup@gmail.com>
2024-09-25 00:11:26 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/binary-addition.ts
+++ src/typescript/exercises/7kyu/binary-addition.ts
@@ -1,3 +1,3 @@
 //* https://www.codewars.com/kata/551f37452ff852b7bd000139/train/typescript
 
-export function addBinary(a: number, b: number): string {}
+export const addBinary = (a: number, b: number): string => (a + b).toString(2);
