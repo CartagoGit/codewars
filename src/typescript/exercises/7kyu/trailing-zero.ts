a587e60cc0ae6fd78a24dbcdaa659511e043beb4
Cartago <cartago.relaxingcup@gmail.com>
2024-09-24 07:50:11 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/trailing-zero.ts
+++ src/typescript/exercises/7kyu/trailing-zero.ts
@@ -1,5 +1,6 @@
 //* https://www.codewars.com/kata/66e793bba4b1a6f2e8f890e5/train/typescript
 
 export function trailingZeros(n: number): number {
-	return 0;
+	const matched = n.toString(2).match(/0+$/);
+	return matched ? matched[0].length : 0;
 }
