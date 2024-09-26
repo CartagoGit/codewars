bcf9ae52553cee4b86230af7abe49cc47a68aeb2
Cartago <cartago.relaxingcup@gmail.com>
2024-09-26 08:43:13 +0200

    Damos una solucion aproximada a la kata

--- src/typescript/exercises/6kyu/most-frequent-weekdays.ts
+++ src/typescript/exercises/6kyu/most-frequent-weekdays.ts
@@ -1,16 +1,13 @@
 //* https://www.codewars.com/kata/56eb16655250549e4b0013f4/train/typescript
 
 const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', "November", "December"];
 
 export const mostFrequentDays = (year: number): string[] => {
   const mapDays = new Map<string, number>();
   for(let month = 1; month <= 12; month++) {
-    const daysInMonth = new Date(year, month - 1 , 0).getDate();
-    console.log(months[month - 1],daysInMonth);
+    const daysInMonth = new Date(year, month , 0).getDate();
     for(let day = 1; day <= daysInMonth; day++) {
-      const date = new Date(year, month - 1, day);
-      const dayOfWeek = date.getDay();
+      const dayOfWeek = new Date(year, month - 1, day - 1).getDay();
       const dayName = days[dayOfWeek];
       mapDays.set(dayName, (mapDays.get(dayName) ?? 0) + 1);
     }
@@ -19,5 +16,3 @@ export const mostFrequentDays = (year: number): string[] => {
     return days.filter(day => mapDays.get(day) === max);
 };
 
-
-console.log(mostFrequentDays(1770)); // ["Monday"]
