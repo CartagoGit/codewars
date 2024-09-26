4bca8bca733faff2df4b3a14bb46b7e82549b603
Cartago <cartago.relaxingcup@gmail.com>
2024-09-27 00:11:04 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/vaporcode.ts
+++ src/typescript/exercises/7kyu/vaporcode.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5966eeb31b229e44eb00007a/train/typescript
 
 export function vaporcode(str: string): string {
-	return '';
+	return str.toUpperCase().replace(/\s/g, '').split('').join(' '.repeat(2));
 }
