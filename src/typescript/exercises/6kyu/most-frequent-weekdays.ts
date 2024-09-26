de7997bb63187da5f59939a0cf2fc2c9e2582b6e
Cartago <cartago.relaxingcup@gmail.com>
2024-09-26 08:27:06 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/most-frequent-weekdays.ts
@@ -0,0 +1,8 @@
+//* https://www.codewars.com/kata/56eb16655250549e4b0013f4/train/typescript
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+export const mostFrequentDays = (year: number): string[] => {
+
+};
+
