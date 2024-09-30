c10edcc4044ebde4f3c86f6fe15e331c204be4e9
Cartago <cartago.relaxingcup@gmail.com>
2024-09-30 14:50:15 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/remove-empty-items.ts
+++ src/typescript/exercises/7kyu/remove-empty-items.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5c857bab20089d4fe65a7f5b/train/typescript
 
 export function clean(array: any[]): any[] {
-  
+	return array.filter((_item, index) => array.hasOwnProperty(index));
 }
