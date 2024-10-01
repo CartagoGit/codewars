36cae9bf44b6a1136dfdc1a738353aac279e8382
Cartago <cartago.relaxingcup@gmail.com>
2024-10-01 07:50:53 +0200

    Avanzamos en la kata

--- src/typescript/exercises/7kyu/switch-gravity.ts
+++ src/typescript/exercises/7kyu/switch-gravity.ts
@@ -1,5 +1,24 @@
 //* https://www.codewars.com/kata/64c743cb0a2a00002856ff73/train/typescript
 
-export function switchGravity(arr: ('-' | '#')[][]): ('-' | '#')[][] {
-	return arr;
+type Gravity = '-' | '#';
+
+export function switchGravity(arr: Gravity[][]): Gravity[][] {
+	let forceGravity: Gravity[][] = [];
+	for (let row of arr) {
+		for (let [indexCell, cell] of row.entries()) {
+			if (cell !== '#') continue;
+			let innerRow = 0;
+			while (forceGravity?.[innerRow]?.[indexCell] === '#') {
+				innerRow++;
+			}
+			if (!forceGravity[innerRow]) forceGravity[innerRow] = [];
+			forceGravity[innerRow][indexCell] = '#';
+		}
+	}
+	forceGravity = [...forceGravity].map((row) =>
+		row.map((cell) => (cell === '#' ? cell : '-'))
+	);
+	console.log(forceGravity);
+	return forceGravity;
+    // REVIEW SIN TERMINAR
 }
