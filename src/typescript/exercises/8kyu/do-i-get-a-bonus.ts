0403197d74eb5ebb02eff70b0feb822e20baae8f
Cartago <cartago.relaxingcup@gmail.com>
2024-10-02 07:45:21 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/do-i-get-a-bonus.ts
+++ src/typescript/exercises/8kyu/do-i-get-a-bonus.ts
@@ -1,7 +1,7 @@
 //* https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/typescript
 
 export class Kata {
-    public static bonusTime(salary:number, bonus:boolean):string {
-      // show me the code!
-    }
+	public static bonusTime(salary: number, bonus: boolean): string {
+		return bonus ? `£${salary * 10}` : `£${salary}`;
+	}
 }
