576771d6a836e7a76aceda72bcc61dc11a08a3f8
Cartago <cartago.relaxingcup@gmail.com>
2024-10-02 07:44:19 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/8kyu/do-i-get-a-bonus.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/typescript
+
+export class Kata {
+    public static bonusTime(salary:number, bonus:boolean):string {
+      // show me the code!
+    }
+}
