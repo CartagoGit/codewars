a57d629684f504b83334daf20b59c10246fd5488
Cartago <cartago.relaxingcup@gmail.com>
2024-10-03 08:03:52 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/file-path-operation.ts
@@ -0,0 +1,19 @@
+//* https://www.codewars.com/kata/5844e0890d3bedc5c5000e54/train/typescript
+
+export class FileMaster {
+    constructor(filepath:string) {
+    
+    }
+    
+    extension() {
+      return '';
+    }
+    
+    filename() {
+      return '';
+    }
+    
+    dirpath() {
+      return '';
+    }
+  }
