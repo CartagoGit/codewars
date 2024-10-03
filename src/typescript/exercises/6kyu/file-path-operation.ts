a1d887a83d920deba174f3b5a7b9c4f522cde3b4
Cartago <cartago.relaxingcup@gmail.com>
2024-10-03 08:07:32 +0200

    Damos solucion a la kata

--- src/typescript/exercises/6kyu/file-path-operation.ts
+++ src/typescript/exercises/6kyu/file-path-operation.ts
@@ -1,19 +1,21 @@
 //* https://www.codewars.com/kata/5844e0890d3bedc5c5000e54/train/typescript
 
 export class FileMaster {
-    constructor(filepath:string) {
-    
-    }
-    
-    extension() {
-      return '';
-    }
-    
-    filename() {
-      return '';
-    }
-    
-    dirpath() {
-      return '';
-    }
-  }
+	public filepath: string;
+
+	constructor(filepath: string) {
+		this.filepath = filepath;
+	}
+
+	extension() {
+		return this.filepath.split('.').pop();
+	}
+
+	filename() {
+		return this.filepath.split('/').pop()?.split('.')[0];
+	}
+
+	dirpath() {
+		return this.filepath.split('/').slice(0, -1).join('/') + '/';
+	}
+}
