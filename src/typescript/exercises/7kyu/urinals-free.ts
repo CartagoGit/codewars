f71f5082f5435754d4ccf5c808aef5dd63acfa37
Cartago <cartago.relaxingcup@gmail.com>
2024-10-04 07:39:11 +0200

    Comnezamos la kata

--- src/typescript/exercises/7kyu/urinals-free.ts
+++ src/typescript/exercises/7kyu/urinals-free.ts
@@ -1,5 +1,8 @@
 //* https://www.codewars.com/kata/5e2733f0e7432a000fb5ecc4/train/typescript
 
 export function getFreeUrinals(urinals: string): number {
-  
+	let counter = 0;
+	for (let i = 0; i < urinals.length; i++) {
+		
+	}
 }
