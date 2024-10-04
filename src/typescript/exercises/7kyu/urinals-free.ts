d5ef96fc26749aab1a49131d7dc0ee9dc551f386
Cartago <cartago.relaxingcup@gmail.com>
2024-10-04 07:48:28 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/urinals-free.ts
+++ src/typescript/exercises/7kyu/urinals-free.ts
@@ -1,8 +1,13 @@
 //* https://www.codewars.com/kata/5e2733f0e7432a000fb5ecc4/train/typescript
 
-export function getFreeUrinals(urinals: string): number {
+export function getFreeUrinals(ur: string): number {
 	let counter = 0;
-	for (let i = 0; i < urinals.length; i++) {
-		
+	for (let i = 0; i < ur.length; i++) {
+		const [prev, current, next] = [ur[i - 1], ur[i], ur[i + 1]];
+		if ([current, next].every((pos) => pos === '1')) return -1;
+		if ([prev, current, next].some((pos) => pos === '1')) continue;
+		counter++;
+		i++;
 	}
+	return counter;
 }
