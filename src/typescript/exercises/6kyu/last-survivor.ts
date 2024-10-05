15dd2d732fa909516b65a3606d27dfbda2ac6ab6
Cartago <cartago.relaxingcup@gmail.com>
2024-10-05 19:58:35 +0200

    Comenzamos a desarrollar el ejercicio

--- src/typescript/exercises/6kyu/last-survivor.ts
+++ src/typescript/exercises/6kyu/last-survivor.ts
@@ -1,5 +1,17 @@
 //* https://www.codewars.com/kata/60a1aac7d5a5fc0046c89651/train/typescript
 
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
 export function lastSurvivors(str: string): string {
-  
+	let map = new Map<string, number>();
+	str.split('').forEach((char) => map.set(char, (map.get(char) || 0) + 1));
+    while([...map.values()].some(val => val > 1)) {
+        for(let [key, value] of map) {
+            
+        }
+    }
+	return '';
 }
+
+const test = 'xsdlafqpcmjytoikojsecamgdkehrqqgfknlhoudqygkbxftivfbpxhxtqgpkvsrfflpgrlhkbfnyftwkdebwfidmpauoteahyh'
+console.log(lastSurvivors(test)); // 'abcde'
