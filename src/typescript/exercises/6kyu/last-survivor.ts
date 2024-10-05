4e379230ca107074fce411c85b61e823c6fb7d08
Cartago <cartago.relaxingcup@gmail.com>
2024-10-05 20:03:47 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/last-survivor.ts
+++ src/typescript/exercises/6kyu/last-survivor.ts
@@ -3,15 +3,28 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 export function lastSurvivors(str: string): string {
+	str = str.toLowerCase();
 	let map = new Map<string, number>();
 	str.split('').forEach((char) => map.set(char, (map.get(char) || 0) + 1));
-    while([...map.values()].some(val => val > 1)) {
-        for(let [key, value] of map) {
-            
-        }
-    }
-	return '';
+	while ([...map.values()].some((val) => val > 1)) {
+		for (let [key, value] of map) {
+			if (value <= 1) continue;
+			const index = alphabet.indexOf(key);
+			const nextIndex = index + 1 === alphabet.length ? 0 : index + 1;
+			const newValue = value % 2;
+			const timesNewKey = Math.floor(value / 2);
+			map.set(key, newValue);
+			map.set(
+				alphabet[nextIndex],
+				(map.get(alphabet[nextIndex]) || 0) + timesNewKey
+			);
+		}
+	}
+	return [...map.entries()]
+		.map(([key, value]) => (value ? key : ''))
+		.join('');
 }
 
-const test = 'xsdlafqpcmjytoikojsecamgdkehrqqgfknlhoudqygkbxftivfbpxhxtqgpkvsrfflpgrlhkbfnyftwkdebwfidmpauoteahyh'
+const test =
+	'xsdlafqpcmjytoikojsecamgdkehrqqgfknlhoudqygkbxftivfbpxhxtqgpkvsrfflpgrlhkbfnyftwkdebwfidmpauoteahyh';
 console.log(lastSurvivors(test)); // 'abcde'
