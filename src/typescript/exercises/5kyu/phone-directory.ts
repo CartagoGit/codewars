3d2ca0fe6eaeffaa75911f01640e4358653b373e
Cartago <cartago.relaxingcup@gmail.com>
2024-10-06 09:29:09 +0200

    Continuamos desarrollando la kata

--- src/typescript/exercises/5kyu/phone-directory.ts
+++ src/typescript/exercises/5kyu/phone-directory.ts
@@ -2,9 +2,16 @@
 
 export const phone = (strng: string, num: string): string => {
 	const errorMsgs = {
-		toMany: 'Error => Too many people: num',
-		notFound: 'Error => Not found: num',
+		toMany: (num: string) => `Error => Too many people: ${num}`,
+		notFound: (num: string) => `Error => Not found: ${num}`,
 	};
+	const linesFound = strng.split('\n').filter((line) => line.includes(num));
+	if (linesFound.length === 0) return errorMsgs.notFound(num);
+	if (linesFound.length > 1) return errorMsgs.toMany(num);
+	const line = linesFound[0];
+	console.timeLog('line => ', line, linesFound);
+	const pattern = /\+(\d{1,2})-(\d{3})-(\d{3})-(\d{4})\s+<([^>]+)>\s+(.*)/;
+	console.log(line.match(pattern));
 	// your code
 	return '...';
 };
