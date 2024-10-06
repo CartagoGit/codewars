371bb9c48e055f5a2550451f78d53e84fa1448b4
Cartago <cartago.relaxingcup@gmail.com>
2024-10-06 08:58:44 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/hollow-array.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/59b72376460387017e000062/train/typescript
+
+export function isHollow(x: number[]): boolean {
+	throw new Error('The method or operation is not implemented.');
+}
