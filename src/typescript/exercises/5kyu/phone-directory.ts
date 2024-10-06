e21048a9ef139649514bbcb2ac88ba301c63e51d
Cartago <cartago.relaxingcup@gmail.com>
2024-10-06 09:55:17 +0200

    Llegamos cerca de la solucion

--- src/typescript/exercises/5kyu/phone-directory.ts
+++ src/typescript/exercises/5kyu/phone-directory.ts
@@ -5,27 +5,37 @@ export const phone = (strng: string, num: string): string => {
 		toMany: (text: string) => `Error => Too many people: ${text}`,
 		notFound: (text: string) => `Error => Not found: ${text}`,
 	};
+	const getInfoFormated = (data: {
+		num: string;
+		name: string;
+		address: string;
+	}) => {
+		const { num, name, address } = data;
+		return `Phone => ${num}, Name => ${name}, Address => ${address}`;
+	};
 	const linesFound = strng.split('\n').filter((line) => line.includes(num));
 	if (linesFound.length === 0) return errorMsgs.notFound(num);
 	if (linesFound.length > 1) return errorMsgs.toMany(num);
 	const line = linesFound[0];
-	console.timeLog('line => ', line, linesFound);
-	// const pattern = /\+\d{1,2}-\d{3}-\d{3}-\d{4}|<([^>]+)>|([A-Za-z0-9\s]+(?:\s[A-Za-z0-9\s]+)*)/;
-	// const matches = line.match(pattern);
-	// console.log(matches);
 	const numPattern = /\+\d{1,2}-\d{3}-\d{3}-\d{4}/;
 	const namePattern = /<([^>]+)>/;
-	const addressPattern =
-		/(?:\+\d{1,2}-\d{3}-\d{3}-\d{4}\s+)?(?:<[^>]+>\s+)?(.+)/;
-
-	const numMatch = line.match(numPattern)?.[0] ?? return errorMsgs.notFound(num);
+	const numMatch = line.match(numPattern)?.[0].replace('+', '');
 	const nameMatch = line.match(namePattern)?.[1];
-	const addressMatch = line.match(addressPattern);
 	console.log({
-		numMatch,
-		nameMatch,
-		addressMatch,
+		num: line.match(numPattern),
+		name: line.match(namePattern),
 	});
-	// your code
-	return '...';
+	if (!numMatch || !nameMatch) return errorMsgs.notFound(num);
+	const addressMatch = line
+		.replace(numMatch, '')
+		.replace(nameMatch, '')
+		.replace(/<|>/g, '')
+		.replace(/[^a-zA-Z0-9\s-]/g, '')
+		.trim();
+	console.log({ numMatch, nameMatch, addressMatch });
+	return getInfoFormated({
+		num: numMatch,
+		name: nameMatch,
+		address: addressMatch,
+	});
 };
