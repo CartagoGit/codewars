b5e7a92c8bc7077e222dfce8b114360311135c7b
Cartago <cartago.relaxingcup@gmail.com>
2024-10-06 09:12:23 +0200

    Damos solucion a la kata

--- src/typescript/exercises/6kyu/hollow-array.ts
+++ src/typescript/exercises/6kyu/hollow-array.ts
@@ -1,5 +1,18 @@
 //* https://www.codewars.com/kata/59b72376460387017e000062/train/typescript
 
 export function isHollow(x: number[]): boolean {
-	throw new Error('The method or operation is not implemented.');
+	const [firstZero, lastZero] = [x.indexOf(0), x.lastIndexOf(0)];
+	const finalDistance = x.length - lastZero - 1;
+	const middle = x.slice(firstZero, lastZero + 1);
+	return (
+		middle.length >= 3 &&
+		middle.every((num) => !num) &&
+		finalDistance === firstZero
+	);
 }
+
+const test = [1, 2, 0, 0, 0, 3, 4];
+const test2 = [1, 0, 0, 0, 0, 3, 4];
+console.log(isHollow(test)); // true
+
+console.log(isHollow(test2)); // false
