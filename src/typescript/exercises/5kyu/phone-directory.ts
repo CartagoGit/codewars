a5c640a191334fab3323b5a0e50af0e5e1f91df1
Cartago <cartago.relaxingcup@gmail.com>
2024-10-06 09:19:49 +0200

    Comenzamos la kata guardando mensajes posibbles de error

--- src/typescript/exercises/5kyu/phone-directory.ts
+++ src/typescript/exercises/5kyu/phone-directory.ts
@@ -1,6 +1,10 @@
 //* https://www.codewars.com/kata/56baeae7022c16dd7400086e/train/typescript
 
 export const phone = (strng: string, num: string): string => {
+	const errorMsgs = {
+		toMany: 'Error => Too many people: num',
+		notFound: 'Error => Not found: num',
+	};
 	// your code
 	return '...';
 };
