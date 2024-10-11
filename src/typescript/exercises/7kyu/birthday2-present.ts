df3c2b4829941e979ad87718013976dd0b95a506
Cartago <cartago.relaxingcup@gmail.com>
2024-10-11 07:39:36 +0200

    Comenzamos a preparar la kata

--- src/typescript/exercises/7kyu/birthday2-present.ts
+++ src/typescript/exercises/7kyu/birthday2-present.ts
@@ -1,5 +1,13 @@
 //* https://www.codewars.com/kata/5805f0663f1f9c49be00011f/train/typescript
 
-export function present(x: string, y: number): string{
-    return ""
-  }
+export function present(x: string, y: number): string {
+	const present = {
+		badpresent: () => 'Take this back!',
+		goodpresent: () => {},
+		crap: () => {},
+		empty: () => {},
+		bang: () => {},
+		dog: () => `pass out from excitement ${y} times`,
+	};
+	return '';
+}
