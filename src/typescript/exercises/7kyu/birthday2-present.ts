b790f3a00ad564d6b723a956fbda229de307dfa9
Cartago <cartago.relaxingcup@gmail.com>
2024-10-11 07:44:25 +0200

    Damos solucion a la kata

--- src/typescript/exercises/7kyu/birthday2-present.ts
+++ src/typescript/exercises/7kyu/birthday2-present.ts
@@ -1,13 +1,13 @@
 //* https://www.codewars.com/kata/5805f0663f1f9c49be00011f/train/typescript
 
 export function present(x: string, y: number): string {
-	const present = {
+	const present: Record<string, () => string> = {
 		badpresent: () => 'Take this back!',
-		goodpresent: () => {},
-		crap: () => {},
-		empty: () => {},
-		bang: () => {},
+		goodpresent: () => x.split('').map((letter) => String.fromCharCode(letter.charCodeAt(0) + y)).join(''),
+		crap: () => x.split('').sort().join(''),
+		empty: () => x.split('').sort().join(''),
+		bang: () => x.split('').map(char => char.charCodeAt(0) - y).reduce((acc, curr) => acc + curr, 0).toString(),
 		dog: () => `pass out from excitement ${y} times`,
 	};
-	return '';
+	return present[x]();
 }
