4f634fea6eaf5214faa94a40cfc5099076a89ead
Cartago <cartago.relaxingcup@gmail.com>
2024-10-17 14:33:15 +0200

    Solucionamos la kata

--- src/typescript/exercises/6kyu/dashatize-it.ts
+++ src/typescript/exercises/6kyu/dashatize-it.ts
@@ -1,6 +1,13 @@
 //* https://www.codewars.com/kata/58223370aef9fc03fd000071/train/typescript
 
-export function dashatize(num: number) : string {
-    return ""
-  };
-  
+export function dashatize(num: number): string {
+	const abdNum = Math.abs(num);
+	let chainNum = abdNum.toString().split('');
+	for (const [index, digit] of chainNum.entries()) {
+		if (parseInt(digit) % 2 === 0) continue;
+		const prevChar = index === 0 ? '' : '-';
+		const lastChar = index === chainNum.length - 1 ? '' : '-';
+		chainNum[index] = `${prevChar}${digit}${lastChar}`;
+	}
+	return chainNum.join('').replaceAll('--', '-');
+}
