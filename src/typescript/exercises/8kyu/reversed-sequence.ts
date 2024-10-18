e1ef8ce393d3962173c5ad9f4c4f3b09b1092e10
Cartago <cartago.relaxingcup@gmail.com>
2024-10-18 07:32:38 +0200

    Solucionamos la kata

--- src/typescript/exercises/8kyu/reversed-sequence.ts
+++ src/typescript/exercises/8kyu/reversed-sequence.ts
@@ -1,5 +1,5 @@
 //* https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/typescript
 
 export const reverseSeq = (n: number): number[] => {
-    return [];
-  };
+	return Array.from({ length: n }, (_, i) => n - i);
+};
