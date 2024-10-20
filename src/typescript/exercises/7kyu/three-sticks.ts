425154fdab6bf7cdfaf53a82ce95144716a4ea9e
Cartago <cartago.relaxingcup@gmail.com>
2024-10-20 18:41:42 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/three-sticks.ts
+++ src/typescript/exercises/7kyu/three-sticks.ts
@@ -1,6 +1,8 @@
 //* https://www.codewars.com/kata/57c1f22d8fbb9fd88700009b/train/typescript
 
 export function maxlen(l1: number, l2: number): number {
-    // return maximum length
-    return 0;
-  }
+	const [min, max] = [Math.min(l1, l2), Math.max(l1, l2)];
+	if (max / 3 > min) return max / 3;
+	else if (max / 2 >= min) return min;
+	else return max / 2;
+}
