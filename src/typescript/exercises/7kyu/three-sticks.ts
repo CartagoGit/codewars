d24259e9806d193f91f3c37364ac804c453ff8bb
Cartago <cartago.relaxingcup@gmail.com>
2024-10-20 18:17:35 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/three-sticks.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/57c1f22d8fbb9fd88700009b/train/typescript
+
+export function maxlen(l1: number, l2: number): number {
+    // return maximum length
+    return 0;
+  }
