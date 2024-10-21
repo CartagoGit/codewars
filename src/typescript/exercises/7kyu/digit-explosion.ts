bb20b301a72b9d87ba2203280dfe649341c46768
Cartago <cartago.relaxingcup@gmail.com>
2024-10-21 07:15:43 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/digit-explosion.ts
+++ src/typescript/exercises/7kyu/digit-explosion.ts
@@ -1,7 +1,8 @@
 //* https://www.codewars.com/kata/585b1fafe08bae9988000314/train/typescript
 
-
 export function explode(s: string): string {
-    // your code here
-    return '';
-  }
+	return s
+		.split('')
+		.map((digit, index) => digit.repeat(parseInt(digit)))
+		.join('');
+}
