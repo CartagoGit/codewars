18855d5cc2220e8c7c22c2d0275c260bb827049b
Cartago <cartago.relaxingcup@gmail.com>
2024-10-22 07:40:06 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/hex-word-sum.ts
+++ src/typescript/exercises/7kyu/hex-word-sum.ts
@@ -1,5 +1,20 @@
 //* https://www.codewars.com/kata/5c46ea433dd41b19af1ca3b3/train/typescript
 
 export function hexWordSum(string: string): number {
-    // ...
-  }
+	return (string || '0')
+		.toUpperCase()
+		.split(' ')
+		.map((word) => {
+			const dictionary: Record<string, number> = {
+				O: 0,
+				S: 5,
+			};
+			const parsedWord = word
+				.split('')
+				.map((letter) => dictionary[letter] ?? letter)
+				.join('');
+			const isHex = /^[0-9A-F]+$/i.test(parsedWord);
+			return isHex ? parseInt(parsedWord, 16) : 0;
+		})
+		.reduce((a, b) => a + b, 0);
+}
