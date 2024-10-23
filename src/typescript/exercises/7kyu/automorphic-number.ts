bfb6f5f153ff6f5760e4231d71e36970cca8c926
Cartago <cartago.relaxingcup@gmail.com>
2024-10-23 07:32:02 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/automorphic-number.ts
+++ src/typescript/exercises/7kyu/automorphic-number.ts
@@ -1,6 +1,9 @@
 //* https://www.codewars.com/kata/5a58d889880385c2f40000aa/train/typescript
 
 export function automorphic(n: number): string {
-    // your code here
-  }
-  
+	const square = Math.pow(n, 2);
+	const squareStr = square.toString();
+	const nStr = n.toString();
+	const isAutomorphic = squareStr.endsWith(nStr);
+	return isAutomorphic ? 'Automorphic' : 'Not!!';
+}
