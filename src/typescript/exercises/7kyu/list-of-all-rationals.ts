b4a63c017d67254312be93b40b994993ecc91ae4
Cartago <cartago.relaxingcup@gmail.com>
2024-10-24 20:03:42 +0200

    Solucionamos la kata

--- src/typescript/exercises/7kyu/list-of-all-rationals.ts
+++ src/typescript/exercises/7kyu/list-of-all-rationals.ts
@@ -1,5 +1,11 @@
 //* https://www.codewars.com/kata/5e4e8f5a72d9550032953717/train/typescript
 
 export function* allRationals(): IterableIterator<[number, number]> {
-
+	const queue: [number, number][] = [[1, 1]];
+	while (true) {
+		const [a, b] = queue.shift()!;
+		yield [a, b];
+		queue.push([a, a + b]);
+		queue.push([a + b, b]);
+	}
 }
