073da407efdffcbbc5cabd7e48758e9658ec7a26
Cartago <cartago.relaxingcup@gmail.com>
2024-10-25 07:25:32 +0200

    Solucionamos la primera parte de la kata

--- src/typescript/exercises/7kyu/decimal-time-conversion.ts
+++ src/typescript/exercises/7kyu/decimal-time-conversion.ts
@@ -1,10 +1,14 @@
 //* https://www.codewars.com/kata/6397b0d461067e0030d1315e/train/typescript
 
-export function toIndustrial(time: number|string): number {
-    return ;
-  }
-  
-  export function toNormal(time: number): string {
-    return ;
-  }
-  
+export function toIndustrial(time: number | string): number {
+	let totalHours = 0;
+	if (typeof time === 'string') {
+		const [hours, minutes] = time.split(':').map(Number);
+		totalHours = hours + minutes / 60;
+	} else if (typeof time === 'number') totalHours = time / 60;
+	return Math.round(totalHours * 100) / 100;
+}
+
+export function toNormal(time: number): string {
+	
+}
