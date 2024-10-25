36ce93e3bef939f4cf288ca7b22be38ab5a635f2
Cartago <cartago.relaxingcup@gmail.com>
2024-10-25 07:16:13 +0200

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/decimal-time-conversion.ts
@@ -0,0 +1,10 @@
+//* https://www.codewars.com/kata/6397b0d461067e0030d1315e/train/typescript
+
+export function toIndustrial(time: number|string): number {
+    return ;
+  }
+  
+  export function toNormal(time: number): string {
+    return ;
+  }
+  
