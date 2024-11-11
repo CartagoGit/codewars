06335e1c481c5feadad146c5c31beabf311af01d
Cartago <cartago.relaxingcup@gmail.com>
2024-11-11 07:40:03 +0100

    Solucionamos el primer reto

--- src/typescript/halloween-dev/reto-1.ts
+++ src/typescript/halloween-dev/reto-1.ts
@@ -1,6 +1,13 @@
 //* https://www.halloween.dev/es/retos/2024/1
 
-function createMagicPotion(potions: number[], target: number) {
-	// Code here
-	return undefined;
+function createMagicPotion(
+	potions: number[],
+	target: number
+): number[] | undefined {
+	let maped = new Map();
+	for (let [index, potion] of potions.entries()) {
+		let diff = target - potion;
+		if (maped.has(diff)) return [maped.get(diff), index];
+		maped.set(potion, index);
+	}
 }
