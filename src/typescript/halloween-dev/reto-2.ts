c2b68f959fb705db7ecaea16dd12d961ece42bc6
Cartago <cartago.relaxingcup@gmail.com>
2024-11-12 07:32:07 +0100

    Resolvemos el segundo reto

--- src/typescript/halloween-dev/reto-2.ts
+++ src/typescript/halloween-dev/reto-2.ts
@@ -1,6 +1,20 @@
 //* https://www.halloween.dev/es/retos/2024/2
 
-function battleHorde(zombies: number, humans: number) {
-	// Code here
+function battleHorde(zombies: string, humans: string) {
+	const results = {
+		zombies: 0,
+		humans: 0,
+	};
+	for (let i = 0; i < zombies.length; i++) {
+		const zombie = Number(zombies[i]);
+		const human = Number(humans[i]);
+		const diff = Math.abs(zombie - human);
+		if (zombie > human) results.zombies += diff;
+		else if (zombie < human) results.humans += diff;
+	}
+	const { zombies: totalZombies, humans: totalHumans } = results;
+	const diff = Math.abs(totalZombies - totalHumans);
+	if (totalZombies > totalHumans) return diff + 'z';
+	if (totalZombies < totalHumans) return diff + 'h';
 	return 'x';
 }
