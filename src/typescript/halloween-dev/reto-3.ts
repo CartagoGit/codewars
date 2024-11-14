1200617e70fd32b70c172010fe7554592c84f305
Cartago <cartago.relaxingcup@gmail.com>
2024-11-14 21:49:58 +0100

    Damos una solucion mas eficaz

--- src/typescript/halloween-dev/reto-3.ts
+++ src/typescript/halloween-dev/reto-3.ts
@@ -1,29 +1,34 @@
 //* https://www.halloween.dev/es/retos/2024/3
 
+interface ICollected {
+    results: number[];
+}
+
 function findSafestPath(dream: number[][]): number {
-    const rows = dream.length;
-    const cols = dream[0].length;
-
-    const goalX = rows - 1;
-    const goalY = cols - 1;
-
-    const dfs = (x: number, y: number, collected: number): number => {
-        // Si estamos fuera de los límites, retornamos un valor alto para descartar este camino
-        if (x >= rows || y >= cols) return Infinity;
-        // Acumular la peligrosidad de la celda actual
-        collected += dream[x][y];
-
-        // Si hemos llegado al objetivo, retornamos el valor acumulado
-        if (x === goalX && y === goalY) return collected;
-
-        // Exploramos hacia abajo y hacia la derecha y elegimos el camino con menor peligrosidad
-        const moveDown = dfs(x + 1, y, collected); // Abajo
-        const moveRight = dfs(x, y + 1, collected); // Derecha
-        return Math.min(moveDown, moveRight);
+    let collected: ICollected[][] = 
+        dream.map(row => row.map(() => ({ results: [] })));
+    for (let [rowIndex, row] of dream.entries()) {
+        for (let [cellIndex, cellValue] of row.entries()) {
+            const [nextX, nextY] = [rowIndex + 1, cellIndex + 1];
+            const rightValue = dream[nextX]?.[cellIndex];
+            const downValue = dream[rowIndex]?.[nextY];
+            const currentCell = collected[rowIndex][cellIndex];
+            const hasCollected = !!currentCell.results.length;
+            const currentCellValue = hasCollected
+                ? Math.min(...currentCell.results)
+                : cellValue;
+            if (rightValue !== undefined) {
+                const value = currentCellValue + rightValue;
+                collected[nextX][cellIndex].results.push(value);
+            }
+            if (downValue !== undefined) {
+                const value = currentCellValue + downValue;
+                collected[rowIndex][nextY].results.push(value);
+            }
+        }
     }
-
-    // Iniciamos la búsqueda desde la celda (0, 0)
-    return dfs(0, 0, 0);
+    const { results } = collected[dream.length - 1][dream[0].length - 1];
+    return Math.min(...results);
 }
 
 
