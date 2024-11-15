61cb8a551f84d6c7b02d29f12fa30151fd2d8cc8
Cartago <cartago.relaxingcup@gmail.com>
2024-11-15 19:39:40 +0100

    Comenzamos a resolver el reto

--- src/typescript/halloween-dev/reto-4.ts
+++ src/typescript/halloween-dev/reto-4.ts
@@ -1,6 +1,29 @@
 //* https://www.halloween.dev/es/retos/2024/4
 
 function findTheKiller(whisper: string, suspects: string[]): string {
-    // Code here
-    return ''
-  }
+    const [...lowerSuspects] = suspects.map(suspect => suspect.toLowerCase());
+    const maxNameLength = Math.max(...suspects.map(suspect => suspect.length));
+    const isCompleteName = whisper.at(-1) === '$';
+    const filteredWhisper =
+        isCompleteName ?
+            whisper.slice(0, -1) :
+            whisper;
+    const filteredSuspects =
+        isCompleteName ?
+            lowerSuspects.filter(susp => susp.length === whisper.length - 1) :
+            lowerSuspects.filter(susp => susp.length >= whisper.length);
+    let finalSuspects: string[] = [];
+    for (const suspect of filteredSuspects) {
+        let isValidSuspect = true;
+        for (const [index, char] of [...filteredWhisper].entries()) {
+            if (char !== '~' || suspect[index] === char) continue;
+            isValidSuspect = false;
+        }
+        if (isValidSuspect) finalSuspects.push(suspect);
+    }
+    return finalSuspects.join(',');
+}
+
+const result = findTheKiller('d~~~~~a', ['Dracula', 'Freddy Krueger', 'Jason Voorhees', 'Michael Myers'])
+
+console.log(result); // Esperamos Dracula
