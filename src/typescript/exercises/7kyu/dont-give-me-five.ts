6d2a543ee77f40e63a844704a9f135329eca9433
Cartago <cartago.relaxingcup@gmail.com>
2024-11-16 10:16:40 +0100

    Solucionamos la kata

--- src/typescript/exercises/7kyu/dont-give-me-five.ts
+++ src/typescript/exercises/7kyu/dont-give-me-five.ts
@@ -1,5 +1,9 @@
 //* https://www.codewars.com/kata/5813d19765d81c592200001a/train/typescript
 
 export function dontGiveMeFive(start: number, end: number): number {
-    return 0;
+    let counter = 0;
+    for (let i = start; i <= end; i++) {
+        if (!i.toString().includes('5')) counter++;
+    }
+    return counter;
 }
