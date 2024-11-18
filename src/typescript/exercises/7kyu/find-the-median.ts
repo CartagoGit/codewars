56daec17dc2a0c1ddd69ad6707422238e4a757d0
Cartago <cartago.relaxingcup@gmail.com>
2024-11-18 07:34:07 +0100

    Solucionamos la kata

--- src/typescript/exercises/7kyu/find-the-median.ts
+++ src/typescript/exercises/7kyu/find-the-median.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/5864eb8039c5ab9cd400005c/train/typescript
 
 export function median(n: number[]): number {
-    //Work your magic here. Make sure you're calculating the median, NOT the mean!
+    n.sort((a, b) => a - b);
+    const middle = Math.floor(n.length / 2);
+    return n.length % 2 !== 0 ? n[middle] : (n[middle - 1] + n[middle]) / 2;
 }
