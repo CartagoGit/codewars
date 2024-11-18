adcd1f70ab98c59b951eefd76ecfe3d5a6e0f681
Cartago <cartago.relaxingcup@gmail.com>
2024-11-18 07:32:50 +0100

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/find-the-median.ts
@@ -0,0 +1,5 @@
+//* https://www.codewars.com/kata/5864eb8039c5ab9cd400005c/train/typescript
+
+export function median(n: number[]): number {
+    //Work your magic here. Make sure you're calculating the median, NOT the mean!
+}
