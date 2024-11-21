27071c9490316dfac43b1b880389b0dd51314f03
Cartago <cartago.relaxingcup@gmail.com>
2024-11-21 01:04:37 +0100

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/over-the-road.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/typescript
+
+export function overTheRoad(address: number, n: number):number {
+    // Good luck!
+    
+    return 0;
+  }
