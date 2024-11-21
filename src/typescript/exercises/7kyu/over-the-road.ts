1c2b62434a4603a180ae6a45f3541611ba0319a8
Cartago <cartago.relaxingcup@gmail.com>
2024-11-21 01:19:25 +0100

    Solucionamos la kata

--- src/typescript/exercises/7kyu/over-the-road.ts
+++ src/typescript/exercises/7kyu/over-the-road.ts
@@ -4,16 +4,11 @@ export function overTheRoad(address: number, n: number): number {
     const isEven = address % 2 === 0;
     const position = isEven ? address / 2 : (address + 1) / 2;
     const opositePosition = n + 1 - position;
-    console.log({
-        isEven,
-        n,
-        position,
-        opositePosition
-    })
-    return opositePosition * 2;
+    return opositePosition * 2 - (isEven ? 1 : 0);
 }
 
 
+console.log(overTheRoad(1, 3)) // 6
 console.log(overTheRoad(3, 3)) // 4
 console.log(overTheRoad(2, 3)) // 5
 console.log(overTheRoad(3, 5)) // 8
