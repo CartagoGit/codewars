eaae4798d81fb4c4f1370b033472a3981f1f5315
Cartago <cartago.relaxingcup@gmail.com>
2024-11-24 23:28:20 +0100

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/is-this-my-tail.ts
@@ -0,0 +1,11 @@
+//* https://www.codewars.com/kata/56f695399400f5d9ef000af5/train/typescript
+
+export function correctTail(body: string, tail: string): boolean{
+    sub = body.substr(bodylength-(tail.length)
+    
+    if sub = tail) ;
+      return true
+    }
+    else 
+      return false
+  }
