b5181c451238c642c9f6b55fd1f594e495561027
Cartago <cartago.relaxingcup@gmail.com>
2024-11-24 23:30:44 +0100

    Resolvemos la kata

--- src/typescript/exercises/8kyu/is-this-my-tail.ts
+++ src/typescript/exercises/8kyu/is-this-my-tail.ts
@@ -1,11 +1,3 @@
 //* https://www.codewars.com/kata/56f695399400f5d9ef000af5/train/typescript
 
-export function correctTail(body: string, tail: string): boolean{
-    sub = body.substr(bodylength-(tail.length)
-    
-    if sub = tail) ;
-      return true
-    }
-    else 
-      return false
-  }
+export const correctTail = (body: string, tail: string): boolean => body.endsWith(tail);
