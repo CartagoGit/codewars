1c29b5cf42e90c9f6d37742372657f4767ade210
Cartago <cartago.relaxingcup@gmail.com>
2024-11-24 23:31:44 +0100

    Pasamos prettier

--- src/typescript/exercises/8kyu/is-this-my-tail.ts
+++ src/typescript/exercises/8kyu/is-this-my-tail.ts
@@ -1,3 +1,4 @@
 //* https://www.codewars.com/kata/56f695399400f5d9ef000af5/train/typescript
 
-export const correctTail = (body: string, tail: string): boolean => body.endsWith(tail);
+export const correctTail = (body: string, tail: string): boolean =>
+  body.endsWith(tail);
