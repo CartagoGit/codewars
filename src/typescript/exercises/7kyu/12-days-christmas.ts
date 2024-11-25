5ac2cbb5d3ce41edf5829eaf81e79288da0c348a
Cartago <cartago.relaxingcup@gmail.com>
2024-11-25 07:30:17 +0100

    Preparamos la kata

---
+++ src/typescript/exercises/7kyu/12-days-christmas.ts
@@ -0,0 +1,7 @@
+//* https://www.codewars.com/kata/57dd3a06eb0537b899000a64/train/typescript
+
+export function comparator(a: string, b: string): number {
+    // Your code here
+    return 0
+  }
+  
