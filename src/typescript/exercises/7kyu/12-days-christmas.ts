d7428f88ff35a5e7a8b8de205666eb44fbaf825d
Cartago <cartago.relaxingcup@gmail.com>
2024-11-25 07:33:08 +0100

    Añadimos la letra de la cancion

--- src/typescript/exercises/7kyu/12-days-christmas.ts
+++ src/typescript/exercises/7kyu/12-days-christmas.ts
@@ -1,7 +1,22 @@
 //* https://www.codewars.com/kata/57dd3a06eb0537b899000a64/train/typescript
 
+const song = [
+    "On the 12th day of Christmas my true love gave to me",
+    "12 drummers drumming,",
+    "11 pipers piping,",
+    "10 lords a leaping,",
+    "9 ladies dancing,",
+    "8 maids a milking,",
+    "7 swans a swimming,",
+    "6 geese a laying,",
+    "5 golden rings,",
+    "4 calling birds,",
+    "3 French hens,",
+    "2 turtle doves and ",
+    "a partridge in a pear tree.",
+]
 export function comparator(a: string, b: string): number {
+    console.log(a, b)
     // Your code here
     return 0
-  }
-  
+}
