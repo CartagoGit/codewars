834ce98a6c8c1306183d11ed1826b15f80899463
Cartago <cartago.relaxingcup@gmail.com>
2024-11-26 16:07:14 +0100

    Tratamos de seguir resolviendo el problema de los 12 dias de navidad

--- src/typescript/exercises/7kyu/12-days-christmas.ts
+++ src/typescript/exercises/7kyu/12-days-christmas.ts
@@ -1,23 +1,23 @@
 //* https://www.codewars.com/kata/57dd3a06eb0537b899000a64/train/typescript
 
 const song = [
-    "On the 12th day of Christmas my true love gave to me",
-    "12 drummers drumming,",
-    "11 pipers piping,",
-    "10 lords a leaping,",
-    "9 ladies dancing,",
-    "8 maids a milking,",
-    "7 swans a swimming,",
-    "6 geese a laying,",
-    "5 golden rings,",
-    "4 calling birds,",
-    "3 French hens,",
-    "2 turtle doves and ",
-    "a partridge in a pear tree.",
-]
+	'On the 12th day of Christmas my true love gave to me',
+	'12 drummers drumming,',
+	'11 pipers piping,',
+	'10 lords a leaping,',
+	'9 ladies dancing,',
+	'8 maids a milking,',
+	'7 swans a swimming,',
+	'6 geese a laying,',
+	'5 golden rings,',
+	'4 calling birds,',
+	'3 French hens,',
+	'2 turtle doves and ',
+	'a partridge in a pear tree.',
+];
 export function comparator(a: string, b: string): number {
-    return song.indexOf(a) - song.indexOf(b)
-
+	const diff = song.indexOf(a) - song.indexOf(b);
+	return diff > 0 ? 1 : diff < 0 ? -1 : 0;
 }
 
 // REVIEW Sin resolver por falta de entendimiento del enunciado
