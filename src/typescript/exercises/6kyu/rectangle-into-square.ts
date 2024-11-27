5720f38b4c193f04e2230a88680cc8880f2981dc
Cartago <cartago.relaxingcup@gmail.com>
2024-11-27 22:43:04 +0100

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/rectangle-into-square.ts
@@ -0,0 +1,6 @@
+//* https://www.codewars.com/kata/55466989aeecab5aac00003e/train/typescript
+
+export const sqInRect=(l:number, w:number): null|number[] =>{
+    // Your code here...
+    return null
+}
