4d97356a12e26316728b1681b30ef78b7f5921c9
Cartago <cartago.relaxingcup@gmail.com>
2024-11-27 23:05:38 +0100

    Solucionamos la kata

--- src/typescript/exercises/6kyu/rectangle-into-square.ts
+++ src/typescript/exercises/6kyu/rectangle-into-square.ts
@@ -1,6 +1,17 @@
 //* https://www.codewars.com/kata/55466989aeecab5aac00003e/train/typescript
 
-export const sqInRect=(l:number, w:number): null|number[] =>{
-    // Your code here...
-    return null
-}
+export const sqInRect = (l: number, w: number): null | number[] => {
+	if (l === w) return null;
+	const recursive = (size1: number, size2: number): number[] => {
+		const [min, max] = size1 < size2 ? [size1, size2] : [size2, size1];
+		if (min === 0) return [];
+		return [min, ...recursive(min, max - min)];
+	};
+	return recursive(l, w);
+};
+
+console.log(sqInRect(5, 5)); //, null);
+console.log(sqInRect(5, 3)); //[3, 2, 1, 1]);
+console.log(sqInRect(3, 5)); //[3, 2, 1, 1]);
+console.log(sqInRect(20, 14)); //[14, 6, 6, 2, 2, 2]);
+console.log(sqInRect(14, 20)); //[14, 6, 6, 2, 2, 2]);
