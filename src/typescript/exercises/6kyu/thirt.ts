46a6620b7ae8d07408debb8a78e903b4f7ff47c8
Cartago <cartago.relaxingcup@gmail.com>
2024-11-28 08:26:07 +0100

    Damos con una posible solucion

--- src/typescript/exercises/6kyu/thirt.ts
+++ src/typescript/exercises/6kyu/thirt.ts
@@ -1,5 +1,17 @@
 //* https://www.codewars.com/kata/564057bc348c7200bd0000ff/train/typescript
 
 export function thirt(n: number): number {
-	
+	const sequence = [1, 10, 9, 12, 3, 4];
+	const recursive = (n: number): number => {
+		const newN = n
+			.toString()
+			.split('')
+			.reverse()
+			.reduce((acc, digit, index) => {
+				return acc + Number(digit) * sequence[index % sequence.length];
+			}, 0);
+		if (newN === n) return n;
+		return recursive(newN);
+	};
+	return recursive(n);
 }
