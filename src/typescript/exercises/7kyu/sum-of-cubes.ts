262598fbb16639b907a026f336990e009a907531
Cartago <cartago.relaxingcup@gmail.com>
2024-11-29 18:08:02 +0100

    Solucionamos la kata

--- src/typescript/exercises/7kyu/sum-of-cubes.ts
+++ src/typescript/exercises/7kyu/sum-of-cubes.ts
@@ -1,5 +1,8 @@
 //* https://www.codewars.com/kata/59a8570b570190d313000037/train/typescript
 
 export function sumCubes(n: number): number {
-    // your code here
-  }
+	return Array.from({ length: n }, (_, i) => i + 1).reduce(
+		(acc, num) => acc + num ** 3,
+		0
+	);
+}
