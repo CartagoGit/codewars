d7dcded2f7c288c04c8f498c90d508d127bf4b1b
Cartago <cartago.relaxingcup@gmail.com>
2024-12-02 04:38:18 +0100

    Enfocamos la kata

--- src/typescript/exercises/6kyu/rainfall.ts
+++ src/typescript/exercises/6kyu/rainfall.ts
@@ -1,11 +1,28 @@
 //* https://www.codewars.com/kata/56a32dd6e4f4748cc3000006/train/typescript
 
-export const mean = (town:string, s:string): number => {
-    // your code
-    return 0
-  }
-  
-  export const variance = (town:string, strng:string): number => {
-    // your code
-    return 0
-  }
+export const mean = (town: string, chain: string): number => {
+	const townData = getParsedTownData(town, chain);
+	if (!townData.length) return -1;
+	return townData.reduce((acc, curr) => acc + curr, 0) / townData.length;
+};
+
+export const variance = (town: string, chain: string): number => {
+	const townData = getParsedTownData(town, chain);
+	if (!townData.length) return -1;
+	return (
+		townData.reduce(
+			(acc, curr) => acc + Math.pow(curr - mean(town, chain), 2),
+			0
+		) / townData.length
+	);
+};
+
+const getParsedTownData = (town: string, chain: string): number[] => {
+	const townData = chain.split('\n').find((data) => data.startsWith(town));
+	if (!townData) return [];
+	const monthsData = townData
+		.split(':')[1]
+		.split(',')
+		.map((data) => parseFloat(data.split(' ')[1]));
+	return monthsData;
+};
