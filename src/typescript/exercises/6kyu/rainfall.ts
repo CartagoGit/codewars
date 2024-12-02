0df845dff1f272545d8a21f97daecb134518f80c
Cartago <cartago.relaxingcup@gmail.com>
2024-12-02 04:23:09 +0100

    Preparamos la kata

---
+++ src/typescript/exercises/6kyu/rainfall.ts
@@ -0,0 +1,11 @@
+//* https://www.codewars.com/kata/56a32dd6e4f4748cc3000006/train/typescript
+
+export const mean = (town:string, s:string): number => {
+    // your code
+    return 0
+  }
+  
+  export const variance = (town:string, strng:string): number => {
+    // your code
+    return 0
+  }
