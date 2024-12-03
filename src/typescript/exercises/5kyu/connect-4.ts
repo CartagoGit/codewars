2c32f72de75f46802ec8c40f16690a7316ba9fab
Cartago <cartago.relaxingcup@gmail.com>
2024-12-03 11:42:01 +0100

    Comenzamos a enfocar la kata

--- src/typescript/exercises/5kyu/connect-4.ts
+++ src/typescript/exercises/5kyu/connect-4.ts
@@ -1,12 +1,44 @@
 //* https://www.codewars.com/kata/586c0909c1923fdb89002031/train/typescript
 
-export class Connect4 {
+type IPlayer = '1' | '2';
 
-    constructor() {
-      // Good luck
-    }
-  
-    play(col: number): string{
-      // Good luck
-    }
-  }
+export class Connect4 {
+    // ANCHOR : Properties
+	public board = Array.from({ length: 6 }, () =>
+		Array.from({ length: 7 }, () => 0)
+	);
+
+	private _playerTurn: IPlayer = '1';
+	get playerTurn(): IPlayer {
+		return this._playerTurn;
+	}
+	set playerTurn(_player: IPlayer) {
+		this._is1Turn = _player === '1';
+		this._playerTurn = _player;
+	}
+
+	private _is1Turn: boolean = true;
+	get is1Turn(): boolean {
+		return this._is1Turn;
+	}
+
+    // ANCHOR: Constructor
+
+	constructor() {}
+
+    // ANCHOR: Methods
+
+	play(col: number): string {
+		// Good luck
+
+        let result: string;
+
+        result = `Player ${this.playerTurn} has a turn`;
+        this._changePlayerTurn();
+        return result;
+	}
+
+	private _changePlayerTurn(): void {
+		this.playerTurn = this.is1Turn ? '2' : '1';
+	}
+}
