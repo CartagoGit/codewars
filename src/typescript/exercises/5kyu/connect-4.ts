ab5782733221f069564d20d11220f729533d71ba
Cartago <cartago.relaxingcup@gmail.com>
2024-12-03 11:32:10 +0100

    Preparamos la kata

---
+++ src/typescript/exercises/5kyu/connect-4.ts
@@ -0,0 +1,12 @@
+//* https://www.codewars.com/kata/586c0909c1923fdb89002031/train/typescript
+
+export class Connect4 {
+
+    constructor() {
+      // Good luck
+    }
+  
+    play(col: number): string{
+      // Good luck
+    }
+  }
