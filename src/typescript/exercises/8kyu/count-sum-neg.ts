6dbbbf41ded19f8109e18142b5c126ebb6629834
Cartago <cartago.relaxingcup@gmail.com>
2024-12-05 19:39:55 +0100

    Añadimos tipado

--- src/typescript/exercises/8kyu/count-sum-neg.ts
+++ src/typescript/exercises/8kyu/count-sum-neg.ts
@@ -1,5 +1,5 @@
 // ** https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/typescript
 
-export function countPositivesSumNegatives(input: any) {
-    throw new Error('The method or operation is not implemented.');
+export function countPositivesSumNegatives(input: number[]): [number, number] {
+    
   }
