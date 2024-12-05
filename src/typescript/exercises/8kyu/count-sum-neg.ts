a18ed216893853384becc31a6544f704364b66f9
Cartago <cartago.relaxingcup@gmail.com>
2024-12-05 19:46:22 +0100

    Solucionamos la kata

--- src/typescript/exercises/8kyu/count-sum-neg.ts
+++ src/typescript/exercises/8kyu/count-sum-neg.ts
@@ -1,5 +1,13 @@
 // ** https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/typescript
 
-export function countPositivesSumNegatives(input: number[]): [number, number] {
-    
-  }
+export function countPositivesSumNegatives(input: number[] | null): number[] {
+    if(!input?.length) return []
+	return input.reduce(
+		(acc, curr) => {
+			if (curr > 0) acc[0]++;
+			if (curr < 0) acc[1] += curr;
+			return acc;
+		},
+		[0, 0]
+	);
+}
