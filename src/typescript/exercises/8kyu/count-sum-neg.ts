e39628b9e0302932de5259d7dfbf4e6aaa9c58dc
Cartago <cartago.relaxingcup@gmail.com>
2024-12-05 19:39:18 +0100

    Perparamos la kata

---
+++ src/typescript/exercises/8kyu/count-sum-neg.ts
@@ -0,0 +1,5 @@
+// ** https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/typescript
+
+export function countPositivesSumNegatives(input: any) {
+    throw new Error('The method or operation is not implemented.');
+  }
