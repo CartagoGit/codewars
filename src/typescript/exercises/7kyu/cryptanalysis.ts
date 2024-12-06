2fcc3ae62e6f201b2cc03279d2d32459432b88e2
Cartago <cartago.relaxingcup@gmail.com>
2024-12-06 16:54:40 +0100

    Solucionamos la kata

--- src/typescript/exercises/7kyu/cryptanalysis.ts
+++ src/typescript/exercises/7kyu/cryptanalysis.ts
@@ -1,7 +1,13 @@
 //* https://www.codewars.com/kata/5f3142b3a28d9b002ef58f5e/train/typescript
 
 export function wordPattern(word: string): string {
-	const letters = word.split('');
-	const uniqueLetters = [...new Set(letters)];
-	return letters.map((letter) => uniqueLetters.indexOf(letter)).join('.');
+	const letters = word.split('').map((letter) => letter.toLowerCase());
+	const mapLetters = [...new Set(letters)].reduce((acc, curr, index) => {
+		if (!acc[curr]) acc[curr] = index;
+		return acc;
+	}, {} as Record<string, number>);
+	return letters.map((letter) => mapLetters[letter]).join('.');
 }
+
+
+console.log(wordPattern('hello')); 
