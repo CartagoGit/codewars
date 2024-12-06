9eab26403c4a33eb9c03c540798cd4b5e297785b
Cartago <cartago.relaxingcup@gmail.com>
2024-12-06 16:55:49 +0100

    Realizamos una correccion en el ejercicio

--- src/typescript/exercises/7kyu/cryptanalysis.ts
+++ src/typescript/exercises/7kyu/cryptanalysis.ts
@@ -1,7 +1,7 @@
 //* https://www.codewars.com/kata/5f3142b3a28d9b002ef58f5e/train/typescript
 
 export function wordPattern(word: string): string {
-	const letters = word.split('').map((letter) => letter.toLowerCase());
+	const letters = word.toLowerCase().split('');
 	const mapLetters = [...new Set(letters)].reduce((acc, curr, index) => {
 		if (!acc[curr]) acc[curr] = index;
 		return acc;
@@ -9,5 +9,4 @@ export function wordPattern(word: string): string {
 	return letters.map((letter) => mapLetters[letter]).join('.');
 }
 
-
-console.log(wordPattern('hello')); 
+console.log(wordPattern('hello'));
