ace1038dcb7b3ca46955195cfbabe84273cb0049
Cartago <cartago.relaxingcup@gmail.com>
2024-12-06 15:41:54 +0100

    Enfocamos el ejercicio

--- src/typescript/exercises/7kyu/cryptanalysis.ts
+++ src/typescript/exercises/7kyu/cryptanalysis.ts
@@ -1,5 +1,7 @@
 //* https://www.codewars.com/kata/5f3142b3a28d9b002ef58f5e/train/typescript
 
 export function wordPattern(word: string): string {
-    return ""; 
-  }
+	const letters = word.split('');
+	const uniqueLetters = [...new Set(letters)];
+	return letters.map((letter) => uniqueLetters.indexOf(letter)).join('.');
+}
